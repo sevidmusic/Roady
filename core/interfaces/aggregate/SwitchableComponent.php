18b3622780f6d10a94a602761669c2954db9fe8c
sevidmusic <sevidmusic@gmail.com>
2019-10-17 01:30:22 -0400

    interfaces|aggregate: Defined new aggregate interface SwitchableComponent which Defines the basic contract of a storable component that can be switched on or off.

---
+++ core/interfaces/aggregate/SwitchableComponent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DarlingCms\interfaces\aggregate;
+
+use DarlingCms\interfaces\primary\Switchable;
+
+/**
+ * Interface SwitchableComponent. Defines the basic contract of a
+ * storable component that can be switched on or off.
+ * @package DarlingCms\interfaces\aggregate
+ */
+interface SwitchableComponent extends StorableComponent, Switchable
+{
+
+}
