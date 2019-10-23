dd200fd79280b04afd379bc3169fac1eba4a1811
sevidmusic <sevidmusic@gmail.com>
2019-10-23 12:20:54 -0400

    unitTests|aggregate: Created unit test for the DarlingCms\abstractions\aggregate\Component abstract class.

---
+++ Tests/Unit/abstractions/aggregate/ComponentTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use DarlingCms\abstractions\aggregate\Component;
+use PHPUnit\Framework\MockObject\MockObject;
+
+/**
+ * Class ComponentTest. Defines tests for the DarlingCms\abstractions\aggregate\Component abstract class.
+ */
+class ComponentTest extends PHPUnit\Framework\TestCase
+{
+    /**
+     * @var Component|MockObject PhpUnit mock object instance that represents
+     *                           the DarlingCms\abstractions\aggregate\Component
+     *                           abstract class that will be used for testing.
+     */
+    protected $component;
+
+    /**
+     * Setup the mock object instance of the DarlingCms\abstractions\aggregate\Component
+     * abstract class that will be used for testing.
+     */
+    public function setUp(): void
+    {
+        $constructorArgs = ['ComponentName', 'ComponentType'];
+        $this->component = $this->getMockForAbstractClass('\DarlingCms\abstractions\aggregate\Component', $constructorArgs);
+    }
+
+    /**
+     * Test that the getName() method returns a non empty string.
+     */
+    public function testGetName()
+    {
+        $this->assertIsString($this->component->getName());
+        $this->assertNotEmpty($this->component->getName());
+    }
+
+    /**
+     * Test that the getUniqueId() method returns a non empty string.
+     */
+    public function testGetUniqueId()
+    {
+        $this->assertIsString($this->component->getUniqueId());
+        $this->assertNotEmpty($this->component->getUniqueId());
+    }
+
+    /**
+     * Test that the getType() method returns a non empty string.
+     */
+    public function testGetType()
+    {
+        $this->assertIsString($this->component->getType());
+        $this->assertNotEmpty($this->component->getType());
+    }
+}
