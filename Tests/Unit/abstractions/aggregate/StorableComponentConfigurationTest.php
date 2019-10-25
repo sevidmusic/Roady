5ff5b5bdfb5729ddf28da213f39ad37be8dbb369
sevidmusic <sdmwebsdm@gmail.com>
2019-10-25 00:42:20 +0000

    unitTests: Revised doc comments in StorableComponentConfigurationTest.php

--- Tests/Unit/abstractions/aggregate/StorableComponentConfigurationTest.php
+++ Tests/Unit/abstractions/aggregate/StorableComponentConfigurationTest.php
@@ -5,17 +5,41 @@ namespace UnitTests\abstractions\aggregate;
 use DarlingCms\abstractions\aggregate\StorableComponentConfiguration;
 use PHPUnit\Framework\MockObject\MockObject;
 
+/**
+ * Class StorableComponentConfigurationTest. Defines tests for the
+ * DarlingCms\abstractions\aggregate\StorableComponentConfiguration
+ * abstract class.
+ * @package UnitTests\abstractions\aggregate
+ * @see StorableComponentConfigurationTest::setUp()
+ * @see StorableComponentConfigurationTest::testGetNameReturnsNonEmptyString()
+ * @see StorableComponentConfigurationTest::testGetUniqueIdReturnsNonEmptyString()
+ * @see StorableComponentConfigurationTest::testGetTypeReturnsNonEmptyString()
+ * @see StorableComponentConfigurationTest::testGetLocationReturnsNonEmptyString()
+ * @see StorableComponentConfigurationTest::testGetContainerReturnsNonEmptyString()
+ * @see StorableComponentConfigurationTest::
+ * @see StorableComponentConfigurationTest::
+ * @see StorableComponentConfigurationTest::
+ * @see StorableComponentConfigurationTest::
+ */
 class StorableComponentConfigurationTest extends StorableComponentTest
 {
     /**
-     * @var StorableComponentConfiguration|MockObject PhpUnit mock object instance that represents
-     *                                     the DarlingCms\abstractions\aggregate\StorableComponentConfiguration
-     *                                     abstract class that will be used for testing.
+     * @var StorableComponentConfiguration|MockObject PhpUnit mock object
+     *                                                instance that
+     *                                                represents the
+     *                                                DarlingCms\
+     *                                                abstractions\
+     *                                                aggregate\
+     *                                                StorableComponentConfiguration
+     *                                                abstract class that
+     *                                                will be used for
+     *                                                testing.
      */
     protected $component;
 
     /**
-     * Setup the mock object instance of the DarlingCms\abstractions\aggregate\StorableComponentConfiguration
+     * Setup the mock object instance of the
+     * DarlingCms\abstractions\aggregate\StorableComponentConfiguration
      * abstract class that will be used for testing.
      */
     public function setUp(): void
