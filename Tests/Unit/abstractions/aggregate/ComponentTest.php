866648ed9a13c76c388ff9bbc4cfa8a5c1be7a9d
sevidmusic <sevidmusic@gmail.com>
2019-11-02 13:05:59 -0400

    unitTests|aggregate: Added new tests to ComponentTest, testGetExpectedConstructorArgumentsReturnsArrayWhoseValuesAreValidDefaultConstructorArguments, and testGetExpectedConstructorArgumentsReturnsArrayWhoseKeysAreExpectedArgumentNames. Also revised doc comments in ComponentTest.php

--- Tests/Unit/abstractions/aggregate/ComponentTest.php
+++ Tests/Unit/abstractions/aggregate/ComponentTest.php
@@ -5,21 +5,27 @@ namespace UnitTests\abstractions\aggregate;
 use DarlingCms\abstractions\aggregate\Component;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
+use ReflectionMethod;
 
 /**
- * Class ComponentTest. Defines tests for the DarlingCms\abstractions\aggregate\Component abstract class.
+ * Class ComponentTest. Defines tests for the
+ * DarlingCms\abstractions\aggregate\Component
+ * abstract class.
  */
 class ComponentTest extends TestCase
 {
     /**
-     * @var Component|MockObject PhpUnit mock object instance that represents
+     * @var Component|MockObject PhpUnit mock object
+     *                           instance that represents
      *                           the DarlingCms\abstractions\aggregate\Component
-     *                           abstract class that will be used for testing.
+     *                           abstract class that will
+     *                           be used for testing.
      */
     protected $component;
 
     /**
-     * Setup the mock object instance of the DarlingCms\abstractions\aggregate\Component
+     * Setup the mock object instance of the
+     * DarlingCms\abstractions\aggregate\Component
      * abstract class that will be used for testing.
      */
     public function setUp(): void
@@ -29,7 +35,8 @@ class ComponentTest extends TestCase
     }
 
     /**
-     * Test that the getName() method returns a non empty string.
+     * Test that the getName() method returns a non
+     * empty string.
      */
     public function testGetNameReturnsNonEmptyString()
     {
@@ -37,7 +44,8 @@ class ComponentTest extends TestCase
     }
 
     /**
-     * Test that the getUniqueId() method returns a non empty string.
+     * Test that the getUniqueId() method returns a non
+     * empty string.
      */
     public function testGetUniqueIdReturnsNonEmptyString()
     {
@@ -45,13 +53,58 @@ class ComponentTest extends TestCase
     }
 
     /**
-     * Test that the getType() method returns a non empty string.
+     * Test that the getType() method returns a non
+     * empty string.
      */
     public function testGetTypeReturnsNonEmptyString()
     {
         $this->isNonEmptyString($this->component->getType());
     }
 
+    /**
+     * Test the the getExpectedConstructorArguments()
+     * method returns a non empty array.
+     */
+    public function testGetExpectedConstructorArgumentsReturnsNonEmptyArray() {
+        $this->assertNotEmpty($this->getExpectedConstructorArguments());
+    }
+
+    /**
+     * Test that the getExpectedConstructorArguments()
+     * method returns an array whose keys match the
+     * expected constructor argument names.
+     */
+    public function testGetExpectedConstructorArgumentsReturnsArrayWhoseKeysAreExpectedArgumentNames() {
+        $reflection = new ReflectionMethod(get_class($this->component), '__construct');
+        $expectedArgumentNames = array();
+        foreach($reflection->getParameters() as $reflectionParameter) {
+            array_push($expectedArgumentNames, $reflectionParameter->name);
+        }
+        $this->assertEquals(array_keys($this->component->getExpectedConstructorArguments()), $expectedArgumentNames);
+
+    }
+
+    /**
+     * Test the the getExpectedConstrutorArguments()
+     * method returns a array whose values are valid
+     * default constructor arguments.
+     */
+    public function testGetExpectedConstructorArgumentsReturnsArrayWhoseValuesAreValidDefaultConstructorArgumentValues() {
+        $reflection = new ReflectionMethod(get_class($this->component), '__construct');
+        $expectedArgumentTypes = array();
+        foreach($reflection->getParameters() as $reflectionParameter) {
+            array_push($expectedArgumentTypes, $reflectionParameter->getType());
+        }
+        $argumentTypes = array();
+        foreach($this->component->getExpectedConstructorArguments() as $argument) {
+            array_push($argumentTypes, getType($argument));
+        }
+        $this->assertEquals($expectedArgumentTypes, $argumentTypes);
+    }
+
+    /**
+     * Assert that a value is a non empty string.
+     */
     protected function isNonEmptyString(string $value) {
         $this->assertIsString($value);
         $this->assertNotEmpty($value);
