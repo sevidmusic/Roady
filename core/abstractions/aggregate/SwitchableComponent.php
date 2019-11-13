de21e3e416c0eefc18fbc1a0a464284ece6afcc7
sevidmusic <sevidmusic@gmail.com>
2019-11-13 17:42:11 -0500

    abstractions|aggregate: Revised doc comments in SwitchableComponent.php

--- core/abstractions/aggregate/SwitchableComponent.php
+++ core/abstractions/aggregate/SwitchableComponent.php
@@ -7,7 +7,7 @@ use \ReflectionMethod;
 /**
  * Class SwitchableComponent. Defines an abstract implementation of the
  * SwitchableComponent interface that can be implemented to define
- * niche storable components.
+ * niche switchable components.
  * @package DarlingCms\abstractions\aggregate
  * @see SwitchableComponent
  * @see SwitchableComponent::getType()
@@ -17,14 +17,19 @@ use \ReflectionMethod;
 abstract class SwitchableComponent extends StorableComponent implements SwitchableComponentInterface
 {
 
+    /**
+     * @var bool $state Boolean that indicates that component's state,
+     *                  true for on, false for off.
+     */
     private $state;
 
     /**
-     * SwitchableComponent constructor. Assigns the name,
-     * location, and container.
+     * SwitchableComponent constructor. Assigns the name, location, container,
+     * and initial state.
      * @param string $name The name to assign.
      * @param string $location The location to assign.
      * @param string $container The container to assign.
+     * @param bool $initialState The initial state, true for on, false for off.
      */
     public function __construct(string $name, string $location, string $container, bool $initialState)
     {
@@ -32,10 +37,23 @@ abstract class SwitchableComponent extends StorableComponent implements Switchab
         $this->state = $initialState;
     }
 
+     /**
+     * Returns the state of the switch represented as a boolean value,
+     * true for on, false for off.
+     * @return bool The state of the switch represented as a boolean value,
+     *              true for on, false for off.
+     */
     public function getState(): bool {
         return $this->state;
     }
 
+    /**
+     * Switches the state, either from true to false, or false to
+     * true. This method will return true if state was switched,
+     * false otherwise.
+     *
+     * @return bool True if state was switched, false otherwise.
+     */
     public function switchState():bool {
         $initialState = $this->getState();
         $this->state = ($initialState === true ? false : true);
