273a6a7e2d5144b38b88543a694948cb571f6c24
sevidmusic <sevidmusic@gmail.com>
2019-11-13 15:59:35 -0500

    interfaces|primary: Revised doc comments in SwitchableComponent.php

--- core/interfaces/aggregate/SwitchableComponent.php
+++ core/interfaces/aggregate/SwitchableComponent.php
@@ -6,8 +6,8 @@ use DarlingCms\interfaces\primary\Switchable;
 
 /**
  * Interface SwitchableComponent. Describes the basic contract of a
- * component that is switchable, i.e., a component that can be turned
- * on or off.
+ * storable component that is switchable, i.e. a storable component
+ * that can be turned on or off.
  * @package DarlingCms\interfaces\aggregate
  * @see SwitchableComponent::getName()
  * @see SwitchableComponent::getUniqueId()
