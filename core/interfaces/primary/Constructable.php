b5facbe3d23faef9f0e7b44968e333de60d392ff
sevidmusic <sevidmusic@gmail.com>
2019-11-13 15:48:23 -0500

    interfaces|primary: Refactored the Constructable primary interface: - Defined new methods getExpectedConstructorArgumentNames(), getExpectedConstructorArgumentTypes(), and getExpectedConstructorDefaults(). - Removed deprecated method getExpectedConstructorArguments(). Revised doc comments.

--- core/interfaces/primary/Constructable.php
+++ core/interfaces/primary/Constructable.php
@@ -6,20 +6,43 @@ namespace DarlingCms\interfaces\primary;
  * Interface Constructable. Defines the basic contract
  * of something that can articulate how it should be
  * constructed.
- *
- * @see Constructable::getExpectedConstructorArguments()
+ * @see Constructable
+ * @see Constructable::getExpectedConstructorArgumentNames()
+ * @see Constructable::getExpectedConstructorArgumentTypes()
+ * @see Constructable::getExpectedConstructorArgumentDefaults()
  */
 
 interface Constructable
 {
     /**
-     * Returns an array whose keys are expected
-     * constructor argument names and values are
-     * appropriate default values.
-     * @return array An array whose keys are expected
-     *               argument names and values are
-     *               appropriate default values.
+     * Returns a numercially indexed array of the names
+     * of the arguments expected by this implementation's
+     * __construct() method.
+     * @return array  A numercially indexed array of the names
+     *                of the arguments expected by this
+     *                implementation' __construct() method.
      */
-    public static function getExpectedConstructorArguments();
+    public function getExpectedConstructorArgumentNames() : array;
+
+    /**
+     * Returns an associative array of the types of the arguments
+     * expected by this implementation's __construct method, indexed
+     * by argument name.
+     * @return  An associative array of the types of the arguments
+     *          expected by this implementation's __construct method,
+     *          indexed by argument name.
+     */
+    public function getExpectedConstructorArgumentTypes() : array;
+
+
+    /**
+     * Returns an associatvie array of values that can be passed
+     * to the implementation's __construct method as default values,
+     * indexed by argument name.
+     * @return  An associatvie array of values that can be passed
+     *          to the implementation's __construct method as
+     *          default values, indexed by argument name.
+     */
+    public function getExpectedConstructorArgumentDefaults() : array;
 }
 
