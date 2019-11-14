8819a72ee94d5a42137ebdebdde6171dd699bbdb
sevidmusic <sevidmusic@gmail.com>
2019-11-13 23:33:21 -0500

    abstractions|aggregate: Refactored the Component abstract aggregate class: - Revised doc comments. - Refactored the getExpectedConstructorArgumentDefaults() method to return an empty array if there are not any expected constructor arguments. - Refactored the getExpectedConstructorArgumentDefaults() method to use the new private generateAppropriateConstructorArgumentDefaultValues() method to generate appropriate constructor argument default values if the implementation's getExpectedConstructorArgumentDefaultValues() method returns an empty array. - Defined new method generateAppropriateConstructorArgumentDefaultValues() which returns a numerically indexed array of internally generated default values that can be assigned as the expected constructor argument defaults. - Defined new abstract protected method getExpectedConstructorArgumentDefaultValues() which returns a numerically indexed array of values that can be passed to the implementation's __construct() method as default values.

--- core/abstractions/aggregate/Component.php
+++ core/abstractions/aggregate/Component.php
@@ -101,10 +101,10 @@ abstract class Component implements ComponentInterface
 
     /**
      * Returns an associative array of the types of the arguments
-     * expected by this implementation's __construct method, indexed
-     * by argument name.
+     * expected by this implementation's __construct() method,
+     * indexed by argument name.
      * @return  An associative array of the types of the arguments
-     *          expected by this implementation's __construct method,
+     *          expected by this implementation's __construct() method,
      *          indexed by argument name.
      */
     public function getExpectedConstructorArgumentTypes(): array {
@@ -113,15 +113,98 @@ abstract class Component implements ComponentInterface
 
     /**
      * Returns an associatvie array of values that can be passed
-     * to the implementation's __construct method as default values,
+     * to the implementation's __construct() method as default values,
      * indexed by argument name.
      * @return  An associatvie array of values that can be passed
-     *          to the implementation's __construct method as
+     *          to the implementation's __construct() method as
      *          default values, indexed by argument name.
      */
     public function getExpectedConstructorArgumentDefaults(): array {
-        return array_combine($this->getComponentConstructorParamerterInfo('n'), array('DefaultName'));
+        // If there are no constructor agruments, return an empty array
+        if(empty($this->getComponentConstructorParamerterInfo('n'))) {
+            return array();
+        }
+        return array_combine($this->getComponentConstructorParamerterInfo('n') , (empty($this->getExpectedConstructorArgumentDefaultValues()) === true ? $this->generateAppropriateConstructorArgumentDefaultValues() : $this->getExpectedConstructorArgumentDefaultValues()));
     }
+
+    /**
+     * Returns a numerically indexed array of internally generated default
+     * values that can be assigned as the expected constructor argument
+     * defaults. This method is able to generate values for arguments that
+     * expect the following types of values: boolean, integer, double,
+     * string, and array.
+     *
+     * WARNING: Arguments that expect an object instance will be assigned
+     *          an instance of stdClass() since there is no way to
+     *          know ahead of time what types of objects will be expected
+     *          or how they should be constructed.
+     */
+    final private function generateAppropriateConstructorArgumentDefaultValues() : array {
+        $defaults = array();
+        foreach($this->getComponentConstructorParamerterInfo('t') as $type) {
+            switch($type) {
+                case 'boolean':
+                    array_push($defaults, false);
+                    break;
+                case 'integer':
+                    array_push($defaults, 1);
+                    break;
+               /**
+                * Double is same as float.
+                * @see https://www.php.net/manual/en/function.gettype.php for more information.
+                */
+                case 'double':
+                    array_push($defaults, 1.2345);
+                    break;
+                case 'string':
+                    array_push($defaults, 'Default_') . random_bytes(12);
+                    break;
+               case 'array':
+                   array_push($defaults, array());
+                   break;
+               case 'object':
+                   error_log('Darling Cms | Component Implementation Error: Generation of default argument for object types is not supported by the Component::generateAppropriateConstructorArgumentDefaultValues() method. Implementations of the Component abstract aggregate class that inject objects via their __construct() method MUST implement the Component::getExpectedConstructorArgumentDefaultValues() abstract method and explicitly define appropriate default values for the implementation. An instance of StdClass was used in place of the expected object. This will most likely cause bugs!!!');
+                    array_push($defaults, new stdClass());
+                    break;
+                case 'NULL':
+                    array_push($defaults, null);
+                    break;
+                default:
+                    error_log('Darling Cms | Component Implementation Error: Generation of default argument failed, encountered unknown type. If this error continues, try implementing the Component::getExpectedConstructorArgumentDefaultValues() method and explicitly define the appropriate default values. NULL was used as default value, this will most likely cause bugs!');
+                    array_push($defaults, null);
+            }
+        }
+        return $defaults;
+    }
+
+    /**
+     * Returns a numerically indexed array of values that can be passed
+     *  to the implementation's __construct() method as default values.
+     * Note: It is recomended that each implmentation explicitly define
+     *       appropriate default constructor argument values in the
+     *       array returned by this method. However, it is possible
+     *       to have the base Component abstract aggregate class
+     *       generate default arguments internally by returning
+     *       an empty array from the implementation of this method.
+     * WARNING: If the implementation of this method returns an empty
+     *          array to indicate that default values should be generated
+     *          internally, but the implementation's __construct() method
+     *          expects arguments that are object instances, then the
+     *          default values generated internally may not reflect
+     *          the expected argument types since instances of stdClass()
+     *          will be assigned as default values for those arguments that
+     *          expect an object instance.
+     *
+     *          In general, it is best only to return an empty array from
+     *          this method while developing a Component implementation,
+     *          and to explictly define appropriate default values in the
+     *          array returned by this method once development of the
+     *          Component implementation is finished.
+     *
+     * @return A numerically indexed array of values that can be passed to the
+     *         implementation's __construct() method as default values.
+     */
+    abstract protected function getExpectedConstructorArgumentDefaultValues() : array;
 
     /**
      * Request information about the Component's constructor parameters.
@@ -152,6 +235,4 @@ abstract class Component implements ComponentInterface
         }
         return $parameterInfo;
     }
-
-
 }
