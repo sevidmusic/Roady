8f8f97e974b9cc23d96762716ed4cb8ed3b33454
sevidmusic <sevidmusic@gmail.com>
2019-12-19 18:47:09 +0000

    unitTests|primary: Refactored the \UnitTests\abstractions\primary\StorableTest unit test class to extend the \UnitTests\abstractions\primary\IdentifiableTest unit test class to reflect that implementations of the \DarlingCms\interfaces\primary\Storable interface must also implment the methods of the \DarlingCms\interfaces\primary\Identifiable interface since the Storable interface extends the Identifiable interface.

--- Tests/Unit/abstractions/primary/StorableTest.php
+++ Tests/Unit/abstractions/primary/StorableTest.php
@@ -6,14 +6,17 @@ use DarlingCms\abstractions\primary\Storable;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
 use UnitTests\interfaces\primary\TestTraits\StorableTestTrait;
+use UnitTests\abstractions\primary\IdentifiableTest;
 
-class StorableTest extends TestCase {
+class StorableTest extends IdentifiableTest {
     use StorableTestTrait;
-   protected $storable;
+    protected $storable;
+    protected $identifiable;
 
     public function setUp():void {
         $constructorArguments = ['MockName', 'MockLocation', 'MockContainer'];
         $this->storable = $this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Storable', $constructorArguments);
+        $this->identifiable = $this->storable;
     }
 
 }
