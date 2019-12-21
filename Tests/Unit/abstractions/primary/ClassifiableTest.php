5a828d5ed134481c9a9c4f7fe8bf8556c7f08ed9
sevidmusic <sevidmusic@gmail.com>
2019-12-21 12:47:31 -0500

    unitTests: Reformatted code in \UnitTest\abstractions\primary\ClassifiableTest, and added doc comment indicating the $classifiable property's type.

--- Tests/Unit/abstractions/primary/ClassifiableTest.php
+++ Tests/Unit/abstractions/primary/ClassifiableTest.php
@@ -3,16 +3,22 @@
 namespace UnitTests\abstractions\primary;
 
 use DarlingCms\abstractions\primary\Classifiable;
-use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 use UnitTests\interfaces\primary\TestTraits\ClassifiableTestTrait;
 
-class ClassifiableTest extends TestCase {
+class ClassifiableTest extends TestCase
+{
     use ClassifiableTestTrait;
-   protected $classifiable;
 
-    public function setUp():void {
-        $constructorArguments = ['MockType'];
+    /**
+     * @var Classifiable|MockObject
+     */
+    protected $classifiable;
+
+    public function setUp(): void
+    {
+        $constructorArguments = [];
         $this->classifiable = $this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Classifiable', $constructorArguments);
     }
 
