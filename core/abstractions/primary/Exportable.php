bc78f18fce0833b160a8ca5876edd33e75e94da0
sevidmusic <sevidmusic@gmail.com>
2019-12-21 14:09:49 -0500

    unitTests|primary: Refactored the \DarlingCms\abstractions\primary\Exportable class: - Reformatted code - Added PhpSpecific annotations to ignore weak warnings. - Defined new method getReflection() which can be used to get a reflection of "this" instance of the Exportable class. - Defined new method getReflectedProperty() which can be used to get a reflection of one of "this" instance's properties. All tests are passing.

--- core/abstractions/primary/Exportable.php
+++ core/abstractions/primary/Exportable.php
@@ -2,21 +2,49 @@
 
 namespace DarlingCms\abstractions\primary;
 
+use DarlingCms\classes\utility\ReflectionUtility;
 use DarlingCms\interfaces\primary\Exportable as ExportableInterface;
+use ReflectionClass;
+use ReflectionException;
 
-abstract class Exportable implements ExportableInterface  {
-    public function export():array {
-        $r = new \DarlingCms\classes\utility\ReflectionUtility();
+abstract class Exportable extends Classifiable implements ExportableInterface
+{
+    /** @noinspection PhpUnused */
+    public function export(): array
+    {
+        $r = new ReflectionUtility();
         return $r->getClassPropertyValues($this);
     }
 
-    public function import(array $export):bool {
-        $reflection = new \ReflectionClass($this);
-        foreach($export as $propertyName => $propertyValue) {
-            $property = $reflection->getProperty($propertyName);
+    /** @noinspection PhpUnused */
+    public function import(array $export): bool
+    {
+        $reflection = $this->getReflection();
+        foreach ($export as $propertyName => $propertyValue) {
+            $property = $this->getReflectedProperty($reflection, $propertyName);
             $property->setAccessible(true);
             $property->setValue($this, $propertyValue);
         }
         return true;
+    }
+
+    private function getReflection()
+    {
+        try {
+            return new ReflectionClass($this);
+        } catch (ReflectionException $e) {
+            error_log(printf('Exportable error: Exportable type "%s" was unable to reflect itself in preparation for import().', $this->getType()));
+            return false;
+        }
+    }
+
+    private function getReflectedProperty(ReflectionClass $reflection, string $propertyName)
+    {
+        try {
+            return $reflection->getProperty($propertyName);
+        } catch (ReflectionException $e) {
+            error_log(printf('Exportable error: Exportable type "%s" was unable to get property  "%s" in preparation for import().', $this->getType(), $propertyName));
+            return false;
+        }
     }
 }
