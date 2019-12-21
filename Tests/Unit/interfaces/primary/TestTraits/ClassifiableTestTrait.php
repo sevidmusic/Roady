5e623c0e3d7afd329d33057c625e81173ea9eada
sevidmusic <sevidmusic@gmail.com>
2019-12-21 12:44:39 -0500

    unitTests|primary: Added doc comments indicating property types to /Tests/Unit/interfaces/primary/TestTraits/ClassifiableTestTrait. Added PhpStorm specific annotation to /Tests/Unit/interfaces/primary/TestTraits/ClassifiableTestTrait to turn of the PhpUnused inspection, the methods defined are not used within the scope of the ClassifiableTestTrait because they are intended to be used by classes that "use" the ClassifiableTestTrait.

--- Tests/Unit/interfaces/primary/TestTraits/ClassifiableTestTrait.php
+++ Tests/Unit/interfaces/primary/TestTraits/ClassifiableTestTrait.php
@@ -1,21 +1,35 @@
 <?php
 
+/** @noinspection PhpUnused */
+
 namespace UnitTests\interfaces\primary\TestTraits;
 
+use DarlingCms\abstractions\primary\Classifiable;
 use UnitTests\TestUtilities\StringTestUtility;
 
-trait ClassifiableTestTrait {
+trait ClassifiableTestTrait
+{
 
+    /**
+     * @var StringTestUtility
+     */
     protected static $stringTestUtility;
+
+    /**
+     * @var Classifiable
+     */
+    protected $classifiable;
 
     /**
      * @beforeClass
      */
-    public static function initializeStringTestUtility() {
+    public static function initializeStringTestUtility()
+    {
         self::$stringTestUtility = new StringTestUtility();
     }
 
-    public function testGetTypeReturnsNonEmptyString() {
+    public function testGetTypeReturnsNonEmptyString()
+    {
         self::$stringTestUtility->stringIsNotEmpty($this->classifiable->getType());
     }
 }
