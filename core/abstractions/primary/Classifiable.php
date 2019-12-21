5532f0fffc7acbd7ac2385f66491fc8d4f2cf93d
sevidmusic <sevidmusic@gmail.com>
2019-12-21 13:02:11 -0500

    unitTests|primary: Reformatted code in \core\abstractions\primary\Classifiable.

--- core/abstractions/primary/Classifiable.php
+++ core/abstractions/primary/Classifiable.php
@@ -4,16 +4,11 @@ namespace DarlingCms\abstractions\primary;
 
 use DarlingCms\interfaces\primary\Classifiable as ClassifiableInterface;
 
-abstract class Classifiable implements ClassifiableInterface {
+abstract class Classifiable implements ClassifiableInterface
+{
 
-    private $type;
-
-
-    public function __construct(string $type) {
-        $this->type = $type;
-    }
-
-    public function getType():string {
-        return $this->type;;
+    public function getType(): string
+    {
+        return get_class($this);
     }
 }
