50de86daca6da494f2bddc1bf3032d9c5a758e81
sevidmusic <sevidmusic@gmail.com>
2019-12-22 01:18:40 -0500

    unitTests|primary: Refactored the \UnitTests\abstractions\primary\Identifiable unit test class: - Reformatted code - Added doc comment indicating type of the $identifiable property. All tests are passing.

--- Tests/Unit/abstractions/primary/IdentifiableTest.php
+++ Tests/Unit/abstractions/primary/IdentifiableTest.php
@@ -3,15 +3,20 @@
 namespace UnitTests\abstractions\primary;
 
 use DarlingCms\abstractions\primary\Identifiable;
-use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 use UnitTests\interfaces\primary\TestTraits\IdentifiableTestTrait;
 
-class IdentifiableTest extends TestCase {
+class IdentifiableTest extends TestCase
+{
     use IdentifiableTestTrait;
-   protected $identifiable;
+    /**
+     * @var Identifiable|MockObject
+     */
+    protected $identifiable;
 
-    public function setUp():void {
+    public function setUp(): void
+    {
         $constructorArguments = ['MockName'];
         $this->identifiable = $this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Identifiable', $constructorArguments);
     }
