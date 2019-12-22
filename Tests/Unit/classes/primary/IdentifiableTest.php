8c5e48aa4f54bf49de964b9bfd4b7bcfeb58c78a
sevidmusic <sevidmusic@gmail.com>
2019-12-22 02:21:22 -0500

    unitTests|primary: Refactored the \UnitTests\classes\primary\IdentifiableTest unit test class: - Reformatted code - The \UnitTests\classes\primary\IdentifiableTest now extends the \UnitTests\abstractions\primary\IdentifiableTest unit test class. All tests are passing.

--- Tests/Unit/classes/primary/IdentifiableTest.php
+++ Tests/Unit/classes/primary/IdentifiableTest.php
@@ -3,17 +3,9 @@
 namespace UnitTests\classes\primary;
 
 use DarlingCms\classes\primary\Identifiable;
-use PHPUnit\Framework\TestCase;
-use UnitTests\interfaces\primary\TestTraits\IdentifiableTestTrait;
 
-class IdentifiableTest extends TestCase
+class IdentifiableTest extends \UnitTests\abstractions\primary\IdentifiableTest
 {
-    use IdentifiableTestTrait;
-    /**
-     * @var Identifiable
-     */
-    protected $identifiable;
-
     public function setUp(): void
     {
         $this->identifiable = new Identifiable('MockName');
