f02976ab7763bc2c39a2c41b47e66c0f67ea2a64
sevidmusic <sevidmusic@gmail.com>
2019-12-24 23:07:48 -0500

    unitTests|primary: Refactored the \UnitTests\TestUtilities\StringTestUtility: - Reformatted code. - Added return types to method definitions. All tests are passing.

--- Tests/Unit/TestUtilities/StringTestUtility.php
+++ Tests/Unit/TestUtilities/StringTestUtility.php
@@ -8,25 +8,31 @@ use PHPUnit\Framework\TestCase;
  * Class StringTestUtility. Defines methods that can be used to perform
  * common assertions on strings, such as asserting that a string is not empty.
  */
-class StringTestUtility extends TestCase {
+class StringTestUtility extends TestCase
+{
 
-    public function stringIsNotEmpty(string $string) {
+    public function stringIsNotEmpty(string $string): void
+    {
         $this->assertNotEmpty($string);
     }
 
-    public function stringIsEmpty(string $string) {
+    public function stringIsEmpty(string $string): void
+    {
         $this->assertEmpty($string);
     }
 
-    public function stringsMatch(string ...$strings) {
+    public function stringsMatch(string ...$strings): void
+    {
         $this->assertTrue(count(array_unique($strings)) === 1);
     }
 
-    public function stringLengthIsExpectedStringLength(string $string, int $expectedStringLength) {
+    public function stringLengthIsExpectedStringLength(string $string, int $expectedStringLength): void
+    {
         $this->assertTrue(strlen($string) === $expectedStringLength);
     }
 
-    public function stringIsAlphaNumeric(string $string) {
+    public function stringIsAlphaNumeric(string $string): void
+    {
         $this->assertTrue(ctype_alnum($string));
     }
 }
