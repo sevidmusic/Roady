b542d870836b47a298612f55cebad10f2ac7bb59
sevidmusic <sevidmusic@gmail.com>
2019-12-25 16:38:35 -0500

    unitTests|primary: Refactored the \UnitTests\classes\primary\SwitchableTest: - Refactored the setUp() method to use the new setSwitchable() method to set the \DarlingCms\interfaces\primary\Identifiable implementation instance used for testing. - Reformatted code. - Cleaned up code. All tests are passing.

--- Tests/Unit/classes/primary/SwitchableTest.php
+++ Tests/Unit/classes/primary/SwitchableTest.php
@@ -4,15 +4,15 @@ namespace UnitTests\classes\primary;
 
 use DarlingCms\classes\primary\Switchable;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\MockObject\MockObject;
 use UnitTests\interfaces\primary\TestTraits\SwitchableTestTrait;
 
-class SwitchableTest extends TestCase {
+class SwitchableTest extends TestCase
+{
     use SwitchableTestTrait;
-   protected $switchable;
 
-    public function setUp():void {
-        $this->switchable = new \DarlingCms\classes\primary\Switchable('MockName');
+    public function setUp(): void
+    {
+        $this->setSwitchable(new Switchable());
     }
 
 }
