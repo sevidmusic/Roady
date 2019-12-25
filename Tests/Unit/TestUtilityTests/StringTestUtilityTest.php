304166ed8d3fe938438d48c3787ca20a4834c365
sevidmusic <sevidmusic@gmail.com>
2019-12-25 16:54:03 -0500

    unitTests|primary: Refactored the UnitTests\TestUtilityTests\StringTestUtilityTest: - Reformatted code. - Added PhpStorm specific annotations to ignore spelling errors in the testCanTestStringIsAlphanumeric() method. All tests are passing.

--- Tests/Unit/TestUtilityTests/StringTestUtilityTest.php
+++ Tests/Unit/TestUtilityTests/StringTestUtilityTest.php
@@ -8,31 +8,40 @@ use UnitTests\TestUtilities\StringTestUtility;
 /**
  * Class StringTestUtilityTest. Defines tests for the StringTestUtility class.
  */
-class StringTestUtilityTest extends TestCase {
+class StringTestUtilityTest extends TestCase
+{
 
     private $stringTestUtility;
 
-    public function setup():void {
+    public function setup(): void
+    {
         $this->stringTestUtility = new StringTestUtility();
     }
 
-    public function testCanTestStringIsNotEmpty() {
+    public function testCanTestStringIsNotEmpty()
+    {
         $this->stringTestUtility->stringIsNotEmpty('Non Empty String');
     }
 
-    public function testCanTestStringIsEmpty() {
+    public function testCanTestStringIsEmpty()
+    {
         $this->stringTestUtility->stringIsEmpty('');
     }
 
-    public function testCanTestStringsMatch() {
+    public function testCanTestStringsMatch()
+    {
         $this->stringTestUtility->stringsMatch('Foo', 'Foo');
     }
 
-    public function testCanTestStringLengthIsExpectedStringLength() {
+    public function testCanTestStringLengthIsExpectedStringLength()
+    {
         $this->stringTestUtility->stringLengthIsExpectedStringLength('Bar', 3);
     }
 
-    public function testCanTestStringIsAlphaNumeric() {
+    public function testCanTestStringIsAlphaNumeric()
+    {
+        /** @noinspection SpellCheckingInspection */
+        /** @noinspection SpellCheckingInspection */
         $this->stringTestUtility->stringIsAlphaNumeric('abcdefghijklmnopqrstuvwxyz0123456789ABCD');
     }
 }
