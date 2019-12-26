2d81d29f2c6c1ff4e5f90e40f7d1e07367abc792
sevidmusic <sevidmusic@gmail.com>
2019-12-25 23:16:48 -0500

    unitTests|primary: Refactored the \UnitTests\abstractions\primary\IdentifiableTest:: - Removed $identifiable property declaration, it is now set and accessed via the new setIdentifiable() and getIdentifiable() methods, respectively. - Refactored the setUp() method to use the new setIdentifiable() method to assign the \DarlingCms\interfaces\primary\Identifiable implementation instance that will be tested. - Reformatted code. - Cleaned up code. All tests are passing.

--- Tests/Unit/abstractions/primary/IdentifiableTest.php
+++ Tests/Unit/abstractions/primary/IdentifiableTest.php
@@ -2,23 +2,17 @@
 
 namespace UnitTests\abstractions\primary;
 
-use DarlingCms\abstractions\primary\Identifiable;
-use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use UnitTests\interfaces\primary\TestTraits\IdentifiableTestTrait;
 
 class IdentifiableTest extends TestCase
 {
     use IdentifiableTestTrait;
-    /**
-     * @var Identifiable|MockObject
-     */
-    protected $identifiable;
 
     public function setUp(): void
     {
         $constructorArguments = ['MockName'];
-        $this->identifiable = $this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Identifiable', $constructorArguments);
+        $this->setIdentifiable($this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Identifiable', $constructorArguments));
     }
 
 }
