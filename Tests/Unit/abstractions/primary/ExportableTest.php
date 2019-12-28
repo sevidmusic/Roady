41820f4fb813b82fff4b36b3975124e599517c36
sevidmusic <sevidmusic@gmail.com>
2019-12-28 15:24:04 -0500

    unitTests|primary: Refactored the \UnitTests\abstractions\primary\ExportableTest: - Now extends the \UnitTests\abstractions\primary\Classifiable unit test class. - Refactored the setUp() method to use the new setExportable() method to assign the \DarlingCms\interfaces\primary\Exportable implementation instance to be used for testing. - Refactored the setUp() method to use the new getExportable() and setClassifiable() methods to assign the assigned \DarlingCms\interfaces\primary\Exportable implementation instance as the \DarlingCms\interfaces\primary\Classifiable implementation instance to be used for testing. - Reformatted code. - Cleaned up code. All tests are passing.

--- Tests/Unit/abstractions/primary/ExportableTest.php
+++ Tests/Unit/abstractions/primary/ExportableTest.php
@@ -2,29 +2,17 @@
 
 namespace UnitTests\abstractions\primary;
 
-use DarlingCms\abstractions\primary\Classifiable;
-use DarlingCms\abstractions\primary\Exportable;
-use PHPUnit\Framework\MockObject\MockObject;
 use UnitTests\interfaces\primary\TestTraits\ExportableTestTrait;
 
 class ExportableTest extends ClassifiableTest
 {
     use ExportableTestTrait;
-    /**
-     * @var Exportable|MockObject
-     */
-    protected $exportable;
-
-    /**
-     * @var Classifiable|Exportable|MockObject
-     */
-    protected $classifiable;
 
     public function setUp(): void
     {
         $constructorArguments = [];
-        $this->exportable = $this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Exportable', $constructorArguments);
-        $this->classifiable = $this->exportable;
+        $this->setExportable($this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Exportable', $constructorArguments));
+        $this->setClassifiable($this->getExportable());
     }
 
 }
