8ef655e66ff1e64ed47a98e0b70a9203a87ad910
sevidmusic <sevidmusic@gmail.com>
2019-12-29 14:48:53 -0500

    unitTests|primary: Refactored the \UnitTests\abstractions\utility\ReflectionUtilityTest: - Now using the Now using the \UnitTests\interfaces\utility\TestTraits\ReflectionUtilityTestTrait. - Refactored the setUp() method to use the \UnitTests\interfaces\utility\TestTraits\ReflectionUtilityTestTrait's setReflectionUtility() method to set an appropriate \DarlingCms\interfaces\utility\ReflectionUtility implementation instance for testing. - Reformatted code. - Cleaned up code. All tests are passing.

--- Tests/Unit/abstractions/utility/ReflectionUtilityTest.php
+++ Tests/Unit/abstractions/utility/ReflectionUtilityTest.php
@@ -3,17 +3,20 @@
 namespace UnitTests\abstractions\utility;
 
 use DarlingCms\interfaces\utility\ReflectionUtility as ReflectionUtilityInterface;
-use DarlingCms\abstractions\utility\ReflectionUtility;
 use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\MockObject\MockObject;
 use UnitTests\interfaces\utility\TestTraits\ReflectionUtilityTestTrait;
 
-class ReflectionUtilityTest extends TestCase implements ReflectionUtilityInterface {
+class ReflectionUtilityTest extends TestCase implements ReflectionUtilityInterface
+{
     use ReflectionUtilityTestTrait;
-   protected $reflectionUtility;
 
-    public function setUp():void {
-        $this->reflectionUtility = $this->getMockForAbstractClass('\DarlingCms\abstractions\utility\ReflectionUtility');;
+    public function setUp(): void
+    {
+        $this->setReflectionUtility(
+            $this->getMockForAbstractClass(
+                '\DarlingCms\abstractions\utility\ReflectionUtility'
+            )
+        );
     }
 
 }
