70c2824f5bcd6869d890eda79b0bdf150b50f13d
sevidmusic <sevidmusic@gmail.com>
2019-12-30 16:17:04 -0500

    unitTests|primary: Refactored the \UnitTests\abstractions\primary\StorableTest: - Removed declaration of $storable and $identifiable properties, they are now set and gotten via the setStorable(), getStorable(), setIdentifiable(), and getIdentifiable() methods, respectively. - Reformatted code. - Cleaned up code. All tests are passing.

--- Tests/Unit/abstractions/primary/StorableTest.php
+++ Tests/Unit/abstractions/primary/StorableTest.php
@@ -2,21 +2,17 @@
 
 namespace UnitTests\abstractions\primary;
 
-use DarlingCms\abstractions\primary\Storable;
-use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\MockObject\MockObject;
 use UnitTests\interfaces\primary\TestTraits\StorableTestTrait;
-use UnitTests\abstractions\primary\IdentifiableTest;
 
-class StorableTest extends IdentifiableTest {
+class StorableTest extends IdentifiableTest
+{
     use StorableTestTrait;
-    protected $storable;
-    protected $identifiable;
 
-    public function setUp():void {
+    public function setUp(): void
+    {
         $constructorArguments = ['MockName', 'MockLocation', 'MockContainer'];
-        $this->storable = $this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Storable', $constructorArguments);
-        $this->identifiable = $this->storable;
+        $this->setStorable($this->getMockForAbstractClass('\DarlingCms\abstractions\primary\Storable', $constructorArguments));
+        $this->setIdentifiable($this->getStorable());
     }
 
 }
