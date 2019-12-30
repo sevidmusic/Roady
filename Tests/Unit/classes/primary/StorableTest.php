2fa90f7bc73860a007d50de60e7624f0ed137e22
sevidmusic <sevidmusic@gmail.com>
2019-12-30 16:17:15 -0500

    unitTests|primary: Refactored the \UnitTests\classes\utility\ReflectionUtilityTest: - Now using the \UnitTests\interfaces\utility\TestTraits\ReflectionUtilityTestTrait to obtain an appropriate \DarlingCms\interfaces\utility\ReflectionUtility implementation instance for testing. - Reformatted code. - Cleaned up code. All tests are passing.

--- Tests/Unit/classes/primary/StorableTest.php
+++ Tests/Unit/classes/primary/StorableTest.php
@@ -3,16 +3,16 @@
 namespace UnitTests\classes\primary;
 
 use DarlingCms\classes\primary\Storable;
-use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\MockObject\MockObject;
 use UnitTests\interfaces\primary\TestTraits\StorableTestTrait;
 
-class StorableTest extends TestCase {
+class StorableTest extends IdentifiableTest
+{
     use StorableTestTrait;
-   protected $storable;
 
-    public function setUp():void {
-        $this->storable = new \DarlingCms\classes\primary\Storable('MockName', 'MockLocation', 'MockContainer');
+    public function setUp(): void
+    {
+        $this->setStorable(new Storable('MockName', 'MockLocation', 'MockContainer'));
+        $this->setIdentifiable($this->getStorable());
     }
 
 }
