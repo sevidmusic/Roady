8db4e2c1507835e223ea4daae717f669f93a8d50
sevidmusic <sevidmusic@gmail.com>
2020-01-09 14:02:58 -0500

    unitTests|utility: Refactored the \DarlingCms\abstractions\primary\Exportable abstract class: - Now using the new Logger trait for logging. - Defined new private property $reflectionUtility. - Defined constants for error messages. - Implemented __construct() method. - Defined new setReflectionUtility() and getReflectionUtility() methods which can be used to set and get the internal \DarlingCms\interfaces\utility\ReflectionUtility implementation, respectively. - Refactored methods to use the new setReflectionUtility() and getReflectionUtility() where appropriate. - The ReflectionUtility implementation is now used to perform any logic where reflection is needed. All tests are passing.

--- core/abstractions/primary/Exportable.php
+++ core/abstractions/primary/Exportable.php
@@ -3,48 +3,102 @@
 namespace DarlingCms\abstractions\primary;
 
 use DarlingCms\classes\utility\ReflectionUtility;
+use DarlingCms\dev\traits\Logger;
 use DarlingCms\interfaces\primary\Exportable as ExportableInterface;
+use DarlingCms\interfaces\utility\ReflectionUtility as ReflectionUtilityInterface;
 use ReflectionClass;
 use ReflectionException;
+use ReflectionProperty;
 
 abstract class Exportable extends Classifiable implements ExportableInterface
 {
-    /** @noinspection PhpUnused */
-    public function export(): array
+    use Logger;
+    private $reflectionUtility;
+
+    const PROP_NOT_DEFINED_IN_CLASS = <<<EOD
+Exportable Notice:
+Exportable type "%s" was unable to get property "%s" in preparation
+for import(). Looking for property in parent class.
+EOD;
+
+    const PROP_NOT_DEFINED_IN_PARENT = <<<EOD
+Exportable Error:
+Exportable type "%s" was unable to get property "%s" in preparation
+for import() from parent class. Defaulting to mocking via an stdClass
+instance to attempt to avoid shutdown.
+EOD;
+
+    const MOCK_STD_FAILED = <<<EOD
+Exportable Fatal Error:
+Exportable type "%s" was unable to get property "%s" in preparation
+for import() from mock stdClass instance. Shutting down.
+EOD;
+
+
+    public function __construct()
     {
-        $r = new ReflectionUtility();
-        return $r->getClassPropertyValues($this);
+        $this->setReflectionUtility(new ReflectionUtility());
     }
 
-    /** @noinspection PhpUnused */
+    private function setReflectionUtility(ReflectionUtilityInterface $reflectionUtility)
+    {
+        $this->reflectionUtility = $reflectionUtility;
+    }
+
+    private function getReflectionUtility(): ReflectionUtility
+    {
+        return $this->reflectionUtility;
+    }
+
+    public function export(): array
+    {
+        return $this->getReflectionUtility()->getClassPropertyValues($this);
+    }
+
     public function import(array $export): bool
     {
-        $reflection = $this->getReflection();
         foreach ($export as $propertyName => $propertyValue) {
-            $property = $this->getReflectedProperty($reflection, $propertyName);
+            $property = $this->getReflectedProperty(
+                $this->getReflectionUtility()->getClassReflection($this),
+                $propertyName
+            );
             $property->setAccessible(true);
             $property->setValue($this, $propertyValue);
         }
-        return true;
+        return ($export === $this->export());
     }
 
-    private function getReflection()
-    {
-        try {
-            return new ReflectionClass($this);
-        } catch (ReflectionException $e) {
-            error_log(printf('Exportable error: Exportable type "%s" was unable to reflect itself in preparation for import().', $this->getType()));
-            return false;
-        }
-    }
-
-    private function getReflectedProperty(ReflectionClass $reflection, string $propertyName)
+    private function getReflectedProperty(ReflectionClass $reflection, string $propertyName): ReflectionProperty
     {
         try {
             return $reflection->getProperty($propertyName);
         } catch (ReflectionException $e) {
-            error_log(printf('Exportable error: Exportable type "%s" was unable to get property  "%s" in preparation for import().', $this->getType(), $propertyName));
-            return false;
+            $this->log(
+                $this::PROP_NOT_DEFINED_IN_CLASS,
+                $this->getType(),
+                $propertyName
+            );
+        }
+        try {
+            return $reflection->getParentClass()->getProperty($propertyName);
+        } catch (ReflectionException $e) {
+            $this->log(
+                $this::PROP_NOT_DEFINED_IN_PARENT,
+                $this->getType(),
+                $propertyName
+            );
+        }
+        try {
+            $reflection = $this->getReflectionUtility()->getClassReflection((object)[$propertyName => null]);
+            return $reflection->getParentClass()->getProperty($propertyName);
+        } catch (ReflectionException $e) {
+            $this->log(
+                $this::MOCK_STD_FAILED,
+                $this->getType(),
+                $propertyName
+            );
+            die();
         }
     }
+
 }
