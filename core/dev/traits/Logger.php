4f0275cc99817cf18ee49d2bb2ae0517265a1b0b
sevidmusic <sevidmusic@gmail.com>
2020-01-10 04:17:32 -0500

    unitTests|dev|traits: Refactored the \DarlingCms\dev\traits\Logger trait: - Defined $logging property which determines whether or not logs should be written. - Set $logging to false for now. All tests are passing.

--- core/dev/traits/Logger.php
+++ core/dev/traits/Logger.php
@@ -6,10 +6,14 @@ namespace DarlingCms\dev\traits;
 
 trait Logger
 {
+    private $logging = false;
+
     protected function log($sprintFormattedMessage, string ...$sprints)
     {
-        $msgArr = [$sprintFormattedMessage];
-        $args = array_merge($msgArr, $sprints);
-        error_log(PHP_EOL . call_user_func_array('sprintf', $args));
+        if ($this->logging === true) {
+            $msgArr = [$sprintFormattedMessage];
+            $args = array_merge($msgArr, $sprints);
+            error_log(PHP_EOL . call_user_func_array('sprintf', $args));
+        }
     }
 }
