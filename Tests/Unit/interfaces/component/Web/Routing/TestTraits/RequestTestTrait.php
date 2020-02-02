20b0b880dbd7fd85d9dd37ad13ed6f0a1c046c00
sevidmusic <sevidmusic@gmail.com>
2020-02-02 16:52:50 -0500

    unitTests|primary: Removed left over var_dump() in Tests/Unit/interfaces/component/Web/Routing/TestTraits/RequestTestTrait.php All tests are passing.

--- Tests/Unit/interfaces/component/Web/Routing/TestTraits/RequestTestTrait.php
+++ Tests/Unit/interfaces/component/Web/Routing/TestTraits/RequestTestTrait.php
@@ -9,6 +9,21 @@ trait RequestTestTrait
 
     private $request;
 
+    public function testGetGetReturnsGetArray(): void
+    {
+        $this->getArrayTestUtility()->arraysAreEqual($_GET, $this->getRequest()->getGet());
+    }
+
+    public function testGetPostReturnsPostArray(): void
+    {
+        $this->getArrayTestUtility()->arraysAreEqual($_POST, $this->getRequest()->getPost());
+    }
+
+    public function testCanGetUrl(): void
+    {
+        $this->getStringTestUtility()->stringIsNotEmpty($this->getRequest()->getUrl());
+    }
+
     protected function setRequestParentTestInstances(): void
     {
         $this->setSwitchableComponent($this->getRequest());
@@ -23,22 +38,6 @@ trait RequestTestTrait
     public function setRequest(Request $request): void
     {
         $this->request = $request;
-    }
-
-    public function testGetGetReturnsGetArray(): void
-    {
-        $this->getArrayTestUtility()->arraysAreEqual($_GET, $this->getRequest()->getGet());
-    }
-
-    public function testGetPostReturnsPostArray(): void
-    {
-        $this->getArrayTestUtility()->arraysAreEqual($_POST, $this->getRequest()->getPost());
-    }
-
-    public function testCanGetUrl(): void
-    {
-        $this->getStringTestUtility()->stringIsNotEmpty($this->getRequest()->getUrl());
-        var_dump($this->getRequest()->getUrl());
     }
 
 }
