c0e0b6035183ac02901cf38ac28676c603d710fc
sevidmusic <sevidmusic@gmail.com>
2020-02-05 12:42:19 -0500

    unitTests|component|Driver|Storage: Defined new base interface \DarlingsCms\interfaces\component\Driver\Storage\Base, which defines the basic contract of a Storage Driver. All tests are passing.

---
+++ core/interfaces/component/Driver/Storage/Base.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DarlingCms\interfaces\component\Driver\Storage;
+
+use DarlingCms\interfaces\component\Component;
+use DarlingCms\interfaces\component\SwitchableComponent;
+use DarlingCms\interfaces\primary\Storable;
+
+interface Base extends SwitchableComponent
+{
+
+    public function write(Component $component): bool;
+
+    public function read(Storable $storable): Component;
+
+    public function delete(Storable $storable): bool;
+
+    public function readAll(string $location, string $container): array;
+
+}
