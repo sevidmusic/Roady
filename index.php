3ca52f95f3c4263f52d94d3ae8df1829c2d507e1
sevidmusic <sevidmusic@gmail.com>
2020-02-24 19:49:48 -0500

    unitTests|index.php: Refactored demonstrations of Router / Request / Crud etc. to allow some configuration of each request, and whether or not data should be saved for request.

--- index.php
+++ index.php
@@ -7,10 +7,20 @@ use DarlingCms\classes\component\OutputComponent;
 use DarlingCms\classes\component\Web\Routing\Request;
 use DarlingCms\classes\component\Web\Routing\Response;
 use DarlingCms\classes\component\Web\Routing\Router;
+use DarlingCms\classes\primary\Positionable;
 use DarlingCms\classes\primary\Storable;
 use DarlingCms\classes\primary\Switchable;
 
 require(__DIR__ . '/vendor/autoload.php');
+
+function getCrudStateFromPost(): bool
+{
+    if (isset($_POST['CRUD_STATE']) === true && $_POST['CRUD_STATE'] === 'On') {
+        return true;
+    }
+    return false;
+}
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -56,6 +66,7 @@ require(__DIR__ . '/vendor/autoload.php');
     </style>
 </head>
 <body Id="animate">
+<!-- Request -->
 <?php
 $request = new Request(
     new Storable('Request', 'Web', 'Requests'),
@@ -70,7 +81,7 @@ foreach ($request->getGet() as $key => $value) {
     echo "<li class='blueDark1'><span class='tanDark1'>$key</span> : $value</li>";
 }
 echo '</ul>';
-echo '<p class="blueDark1">$_POST vars:</p>';
+echo '<p class="blueDark1">$_POST vars ' . (getCrudStateFromPost() === true ? "(Note: The CRUD_STATE post variable will be excluded from the stored request. This is intentional)" : "") . ':</p>';
 echo '<ul>';
 foreach ($request->getPost() as $key => $value) {
     echo "<li class='blueDark1'><span class='tanDark1'>$key</span> : $value</li>";
@@ -78,22 +89,33 @@ foreach ($request->getPost() as $key => $value) {
 echo '</ul>';
 
 ?>
+<!-- Output Component -->
 <?php
 $outputComponent = new OutputComponent(
     new Storable('OutputComponent', 'Output', 'PlainText'),
-    new Switchable()
+    new Switchable(),
+    new Positionable()
 );
 $outputComponent->switchState();
 $outputComponent->import(['output' => 'Hello World!' . strval(rand(100, 999))]);
 echo '<p class="tanDark1">OutputComponent Id: <span class="tanLight1">' . $outputComponent->getUniqueId() . ':</span></p>';
 echo '<p class="tanDark1">Output: <span class="tanLight1">' . $outputComponent->getOutput() . '</span></p>';
 ?>
+<!-- Response -->
 <?php
 $response = new Response(
     new Storable('Response', 'Web', 'Responses'),
     new Switchable()
 );
 $response->switchState();
+function removeDevVarsFromRequestPostData(Request $request)
+{
+    $modifiedRequestPostData = $request->export()['post'];
+    unset($modifiedRequestPostData['CRUD_STATE']);
+    $request->import(['post' => $modifiedRequestPostData]);
+}
+
+removeDevVarsFromRequestPostData($request);
 $response->addRequest($request);
 $response->addOutputComponentStorageInfo($outputComponent);
 echo(
@@ -112,6 +134,7 @@ $response->respondsToRequest($request) === true
     '</span></p>'
 );
 ?>
+<!-- Component Crud -->
 <?php
 $crud = new ComponentCrud(
     new Storable('ComponentCrud', 'DataManagement', 'Crud'),
@@ -121,13 +144,20 @@ $crud = new ComponentCrud(
         new Switchable()
     )
 );
-$crud->switchState();
+
+if (getCrudStateFromPost() === true && $crud->getState() === false) {
+    $crud->switchState();
+}
+if ($crud->getState() === false) {
+    echo "<h3 style='color: red'>Warning: The CRUD is turned off, it will not be possible to save any data!</h3>";
+}
 echo '<p class="tanDark1">Using Crud with Id: <span class="tanLight1">' . $crud->getUniqueId() . '</span></p>';
-?>
-<?php
+
 echo($crud->create($response) ? '<p class="tanDark1">Saved OutputComponent with Id <span class="tanLight1">' . $response->getUniqueId() . '</span></p>' : '<p class="redDark1">Failed to save OutputComponent with Id <span class="redLight1">' . $response->getUniqueId() . '</span></p>');
 echo($crud->create($outputComponent) ? '<p class="tanDark1">Saved OutputComponent with Id <span class="tanLight1">' . $outputComponent->getUniqueId() . '</span></p>' : '<p class="redDark1">Failed to save OutputComponent with Id <span class="redLight1">' . $outputComponent->getUniqueId() . '</span></p>');
+
 ?>
+<!-- Router -->
 <?php
 $router = new Router(
     new Storable('Router', 'Web', 'Routers'),
@@ -136,9 +166,7 @@ $router = new Router(
     $crud
 );
 $router->switchState();
-?>
-<h1 class="blueLight1">Output retrieved using Router:</h1>
-<?php
+echo "<h1 class=\"blueLight1\">Output retrieved using Router:</h1>";
 foreach ($router->getResponses($response->getLocation(), $response->getContainer()) as $storedResponse) {
     /**
      * @var \DarlingCms\interfaces\component\Web\Routing\Response $storedResponse
@@ -151,6 +179,27 @@ foreach ($router->getResponses($response->getLocation(), $response->getContainer
 }
 ?>
 <form method='post'>
+    <label>
+        <br><span>Please select a post value to send with the next request:</span><br>
+        <select name="USER_SUBMITTED_POST_VAR">
+            <option>Foo</option>
+            <option>Bar</option>
+            <option>Baz</option>
+        </select>
+    </label>
+
+    <label>
+        <br><span>Select whether CRUD should be on or off for next request:</span>
+        <br><span>(Note: If CRUD is off it will not be possible to demonstrate reading and writing data to storage!):</span><br>
+        <br><span>The CRUD is currently <?php echo(getCrudStateFromPost() === true ? '<span style="color: limegreen">On</span>' : '<span style="color: red">Off</span>'); ?></span><br>
+        <select name="CRUD_STATE">
+            <?php
+            echo(getCrudStateFromPost() === true
+                ? "<option selected>On</option><option>Off</option>"
+                : "<option>On</option><option selected>Off</option>");
+            ?>
+        </select>
+    </label>
     <input type="submit"/>
     <input type="hidden" name="hiddenValue" value="someValue"/>
 </form>
