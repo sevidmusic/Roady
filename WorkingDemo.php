f4e66b16f9229fba0e7ec036159f7915c8d28920
sevidmusic <sevidmusic@gmail.com>
2020-03-05 11:13:16 -0500

    unitTests|WorkingdDemo: Moved demo code to WorkingDemo.php, and moved common demo functions to DemoFunctions.php. All tests are passing, and the Demo is working.

--- WorkingDemo.php
+++ WorkingDemo.php
@@ -18,14 +18,19 @@ echo getHtml();
 
 function getBody(): string
 {
-    return '
+    return (
+    empty(getCurrentRequest()->getGet()) === true
+        ? '
                 <body class="gradientBg">
                     <div id="welcome" class="genericContainer genericContainerLimitedHeight">' . getWelcomeMessage() . '</div>
+                    <div id="formContainer" class="genericContainer genericContainerLimitedHeight">' . getForm() . '</div>
                     <div id="requestMenu" class="genericContainer genericContainerLimitedHeight">' . getCurrentRequestInfo() . '</div>
                     ' . (empty(getStoredRequestMenu(getMockCrud())) ? "" : '<div class="genericContainer">' . getStoredRequestMenu(getMockCrud()) . '</div>') . '
                         ' . getCollectiveOutputFromOutputAssignedToResponsesToCurrentRequest() . '
                 ' . (str_replace([' ', PHP_EOL], '', getScripts()) === '<script></script>' ? '' : getScripts() . PHP_EOL) . '
-                </body>';
+                </body>'
+        : getCollectiveOutputFromOutputAssignedToResponsesToCurrentRequest()
+    );
 }
 
 function getWelcomeMessage(): string
@@ -33,10 +38,19 @@ function getWelcomeMessage(): string
     return <<<'HTML'
     <h1 class="noticeText">Welcome</h1>
     <p class="successText">
-        To see a working demo of some of the Darling Cms's core components go <a href="WorkingDemo.php">here</a>.
+        This is a demonstration the possible relationships/interactions of a<span class="highlightText"> Request</span>,  
+        <span class="highlightText"> Router</span>, <span class="highlightText"> Response</span>,   
+        <span class="highlightText"> Crud</span>,<span class="highlightText"> Template</span>, and 
+        <span class="highlightText"> OutputComponent</span>.
+    </p>
+    <p class="successText">
+        It currently demonstrates how a stored <span class="highlightText"> Response</span> that responds
+        to the current <span class="highlightText"> Request</span> can be used to determine what
+        <span class="highlightText"> OutputComponent(s)</span>
+        is/are used to generate output for the <span class="highlightText"> Request</span>, and which <span class="highlightText"> Template(s)</span> is/are used to 
+        organize that output.
     </p>
 HTML;
 }
-
 
 
