b84eb096b6cc3ddebdb9239f9ab3e8309c078812
sevidmusic <sevidmusic@gmail.com>
2020-03-31 23:46:15 -0400

    unitTests: Started implemnting necessary setup for StandardUI tests

--- Tests/Unit/abstractions/component/UserInterface/StandardUITest.php
+++ Tests/Unit/abstractions/component/UserInterface/StandardUITest.php
@@ -9,9 +9,118 @@ use UnitTests\abstractions\component\OutputComponentTest as CoreOutputComponentT
 use DarlingCms\abstractions\component\UserInterface\StandardUI;
 use UnitTests\interfaces\component\UserInterface\TestTraits\StandardUITestTrait;
 
+use DarlingCms\classes\component\Web\Routing\Request;
+use DarlingCms\classes\component\Web\Routing\Router;
+use DarlingCms\classes\component\Web\Routing\Response;
+use DarlingCms\classes\component\Crud\ComponentCrud;
+use DarlingCms\classes\component\Driver\Storage\Standard as StorageDriver;
+use DarlingCms\classes\component\OutputComponent;
+use DarlingCms\classes\component\Template\UserInterface\StandardUITemplate;
+
 class StandardUITest extends CoreOutputComponentTest
 {
     use StandardUITestTrait;
+
+    private static $crud;
+    private static $router;
+    private static $outputComponents = [];
+    private static $currentRequest;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$crud = new ComponentCrud(
+            new Storable('StandardUI_TestCrud', 'StandardUI_TestComponents', 'StandardUI_TestCruds'),
+            new Switchable(),
+            new StorageDriver(
+                new Storable('StandardUI_TestStorageDriver', 'StandardUI_TestComponents', 'StandardUI_TestStorageDrivers'),
+                new Switchable()
+            )
+        );
+        self::$currentRequest = self::getRandomRequest();
+        self::$router = new Router(
+            new Storable('StandardUI_TestRouter', 'StandardUI_TestComponents', 'StandardUI_TestRouters'),
+            new Switchable(),
+            self::$currentRequest,
+            self::$crud
+        );
+        // Create Responses
+        var_dump(self::getRandomResponse());
+        // Store Responses, Templates, and Output Components
+    }
+
+    private static function getRandomResponse(): Response
+    {
+        $response = new Response(
+            new Storable('StandardUI_TestResponse' . strval(rand(1000,9999)),'StandardUI_TestComponents','StandardUI_TestResponses'),
+            new Switchable()
+        );
+        $response->addRequestStorageInfo(self::$currentRequest);
+        self::$crud->create($response);
+        return $response;
+    }
+
+    private static function getRandomRequest(): Request
+    {
+        // Create Requests
+        $request = new Request(
+            new Storable('StandardUI_TestRequest' . strval(rand(1000, 9999)), 'StandardUI_TestComponents', 'StandardUI_TestRequests'),
+            new Switchable()
+        );
+        switch (rand(0, 3)) {
+            case 3:
+                $request->import(['url' => 'https://foo.bar/']);
+                break;
+            case 2:
+                $request->import(['url' => 'https://foo.bar/baz' . strval(rand(1000, 9999)) . '.php']);
+                break;
+            case 1:
+                $request->import(['url' => 'https://foo.bar/baz' . strval(rand(1000, 9999)) . '.php?bazzer&foo=bar' . strval(rand(1000, 9999))]);
+                break;
+            default:
+                $request->import(['url' => 'https://foo.bar/baz.php?' . strval(rand(1000, 9999))]);
+                break;
+        }
+        self::$crud->create($request);
+        return $request;
+    }
+
+    private static function getRandomOutputComponent(): OutputComponent
+    {
+        // Create Create Output Components
+        $outputComponent = new OutputComponent(
+            new Storable('StandardUI_TestOutputComponent' . strval((rand(1000, 9999))), 'StandardUI_TestComponents', 'StandardUI_TestOutputComponents'),
+            new Switchable(),
+            new Positionable(self::getRandomPosition())
+        );
+        $outputComponent->import(['output' => 'Output ' . strval(rand(1000, 9999))]);
+        self::$crud->create($output);
+        return $outputComponent;
+    }
+
+    private static function getRandomPosition(): float
+    {
+        /**
+         * We want some Positionable Components to be assigned same position
+         * so we can test that positions are incremented appropriately when
+         * two Positionable components used by the StandardUI have same
+         * position, so rand() is assigned a small range to insure some
+         * positions are repeated. If you change the range later, keep
+         * it small, i.e. less or equal to number of Positionable components
+         * being used in tests.
+         */
+        return (rand(0, 5) / 100);
+    }
+
+    private static function getRandomTemplate(): StandardUITemplate
+    {
+        $template = new StandardUITemplate(
+            new Storable('StandardUI_TestTemplate' . strval(rand(1000, 9999)), 'StandardUI_TestComponents', 'StandardUI_TestTemplates'),
+            new Switchable(),
+            new Positionable(self::getRandomPosition())
+        );
+        self::$crud->create($template);
+        return $template;
+    }
 
     public function setUp(): void
     {
