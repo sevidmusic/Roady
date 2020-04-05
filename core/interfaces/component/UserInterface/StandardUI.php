eadb69880c9b128bad20106e81ab816f8b912eb2
sevidmusic <sevidmusic@gmail.com>
2020-04-05 19:35:28 -0400

    unitTests: Continued development of StandardUI

--- core/interfaces/component/UserInterface/StandardUI.php
+++ core/interfaces/component/UserInterface/StandardUI.php
@@ -7,11 +7,10 @@ use DarlingCms\interfaces\component\OutputComponent as CoreOutputComponent;
 interface StandardUI extends CoreOutputComponent
 {
 
-    // getTemplatesForCurrentRequest(): array | index by position
     public function getTemplatesAssignedToResponses(string $location, string $container): array;
-    //
-    // getOutputComponentsForCurrentRequest():array | index by type, then position
-    //
-    // getOutput() : | string constrcuted from aggregate of OCs organized by Templates
-    // getActionsForCurrentRequest() | index by position
+    /**
+     * @todo
+     * getOutputComponentsAssignedToResponses()
+     * getOutput()
+     */
 }
