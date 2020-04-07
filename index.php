54c32a51da174bdf61280a47dfeb55f35068e219
sevidmusic <sevidmusic@gmail.com>
2020-04-07 03:10:37 -0400

    unitTests|MAJOR STEPPING STONE: Implemented StandardUI in index.php. Everything is working as expected, and all PhpUnit tests are passing. Used WorkingDemo.php to enerate OutputComponent, StandardUITemplate, and Response for index.php, i.e., request to LOCALDOMAIN/index.php. Output should up in response to request to index.php as expected! : ) Lot a hard work paying off. This commit includes the stored components created for index.php by WorkingDemo.php to mark this awesome stepping stone. : )

--- index.php
+++ index.php
@@ -1,7 +1,62 @@
 <?php
 
 ini_set('display_errors', true);
-require(__DIR__ . '/DemoConstants.php');
-require(__DIR__ . '/DemoFunctions.php');
+require_once __DIR__ . DIRECTORY_SEPARATOR . "DemoConstants.php";
+require __DIR__ . DIRECTORY_SEPARATOR . "vendor/autoload.php";
 
-echo getHtml();
+use DarlingCms\classes\component\Crud\ComponentCrud;
+use DarlingCms\classes\component\Driver\Storage\Standard;
+use DarlingCms\classes\primary\Storable;
+use DarlingCms\classes\primary\Switchable;
+
+$crud = new ComponentCrud(
+    new Storable(
+        DEMO_SITE_NAME . "Crud",
+        DEMO_SITE_NAME,
+        DEMO_SITE_NAME . 'Cruds'
+    ),
+    new Switchable(),
+    new Standard(
+        new Storable(
+            DEMO_SITE_NAME . "StandardStorageDriver",
+            DEMO_SITE_NAME,
+            DEMO_SITE_NAME . "StorageDrivers"
+        ),
+        new Switchable()
+    )
+);
+
+$currentRequest = new \DarlingCms\classes\component\Web\Routing\Request(
+    new Storable(
+        DEMO_SITE_NAME . 'CurrentRequest',
+        DEMO_SITE_NAME,
+        DEMO_SITE_NAME . 'Requests'
+    ),
+    new Switchable()
+);
+
+$router = new \DarlingCms\classes\component\Web\Routing\Router(
+    new Storable(
+        DEMO_SITE_NAME . 'Router',
+        DEMO_SITE_NAME,
+        DEMO_SITE_NAME . 'Routers'
+    ),
+    new Switchable(),
+    $currentRequest,
+    $crud
+);
+
+$userInterface = new \DarlingCms\classes\component\UserInterface\StandardUI(
+    new Storable(
+        DEMO_SITE_NAME . 'UserInterface',
+        DEMO_SITE_NAME,
+        DEMO_SITE_NAME . 'UserInterfaces'
+    ),
+    new Switchable(),
+    new \DarlingCms\classes\primary\Positionable(),
+    $router,
+    DEMO_SITE_NAME,
+    DEMO_SITE_RESPONSE_CONTAINER
+);
+
+echo $userInterface->getOutput();
