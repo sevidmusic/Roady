74f4c1e597809f4604c5d783b009feb5ccc0af47
sevidmusic <sevidmusic@gmail.com>
2020-05-11 20:21:15 -0400

    unitTests: Began preparing for development of App component. Revised old Development Template from earlier draft. Nothing has been implemented yet.

--- devInfo/core/intefaces/component/App/DevelopmentTemplate.php
+++ devInfo/core/intefaces/component/App/DevelopmentTemplate.php
@@ -1,23 +1,3 @@
-*************************************************************************
-************************ StoredComponentRegistry ************************
-*************************************************************************
-
-Note: All code in this Component develeopment template is psuedo code,
-and is meant to serve as a guide, actual implementation will be
-different.
-
-*************************************************************************
-************************ Notes about this file **************************
-*************************************************************************
-
-  Hint: Hints provide psuedo code that is meant to serve as an
-        implementation guide.
-
-        Note: Hint numbers correspond pseudo code steps, NOT line numbers.
-              They are merely a formatting feature so the following command
-              can be run to get a summary of the DevelopmentTemplate:
-
-        grep -E 'tes[t]|[0-9][.]' /path/to/DevelopmentTemplate.txt | sed -e 's/tes[t]/\ntest/g;'
 
         In general it is important that the formatting of this document
         be consistent so tools like grep|awk|sed can be used to review
@@ -27,52 +7,67 @@ different.
 *************************************************************************
 *************************************************************************
 
-General Notes:
+*******************
+TestTrait propertys
+*******************
 
-  The following pseudeo definition is a rough outline of what an App component's definition might look like:
+$mockRequest
 
-<?php
-abstract class App extends SwitchableComponent
+*****************
+TestTrait methods
+*****************
+
+getRandomUrl() : string // produce a random url, urls should be both well formed and malformed
+
+getMockRequest(): Request
 {
-    // Important: like Responses, the container of App components MUST be predictable, so a constant MUST be defined for this value.
-    public const APP_CONTAINER = "APP"
-    private $appDomain;
-    __construct(Request*, Switchable) {
-        $this->appDomain = self::deriveAppDomainFromRequest(Request);
-        $storable = new Storable(
-                        self::deriveAppNameFromRequest(Request), // ***make a public static method, though tempting to keep private this could be useful say for dunamicaly determining an App's name, which may also be necessary outside of this class.
-                        self::deriveAppLocationFromRequest(Request), // ***make a public static method, though tempting to keep private this could be useful say for dunamicaly determining an App's name, which may also be necessary outside of this class.
-                        self::APP_CONTAINER
-                    );
-        parent::__construct($storable, Switchable)
+    if(!isset($mockRequest))
+    {
+        $this->mockRequest = new Request(...);
+        $this->mockRequest->import(['url' => $this->getRandomUrl());
     }
-
+    return $this->mockRequest;
 }
-    public static function deriveAppDomainFromRequest(Request $request): string;   // url http://www.foo-bar.baz.123.com/index.php?foo=bar&baz should produce value: http://www.foo-bar.baz.123.com/
-    public static function deriveAppNameFromRequest(Request $request): string;     // url http://www.foo-bar.baz.123.com/index.php?foo=bar&baz should produce value: foobarbaz123
-    public static function deriveAppLocationFromRequest(Request $request): string; // url http://www.foo-bar.baz.123.com/index.php?foo=bar&baz should produce value: wwwfoobarbaz123com
-
-?>
-*Note: An App component is a SwitchableComponent, the SwitchableComponent A.P.I. is
-       new SwitchableComponent(Storable, Switchable)
-       An App component changes the constructors signature to expect a Request as the
-       first argument, this does not conflict with the SwitchableComponent's A.P.I.
-       because all Components are Storables, i.e., Requests are Storables.
-       So all an App is doing is limiting the type of Storable it's __construct method
-       accepts to DarlingCms\interfaces\component\Web\Routing\Response.
-
-*************************
-******* Constants *******
-*************************
-
-APP_CONTAINER
-
-testAppContainerConstantIsSetToStringAPP(): void
-
-**************************
-******* Properties *******
-**************************
 
 *********
-appDomain
+Constants
 *********
+
+APP_CONTAINER = "APP"
+
+testAPP_CONTAINERIsSetToStringAPP(): void
+   Hint:
+       1. assertEquals("APP", DarlingCms\abstractions\component\App\App::APP_CONTAINER) // test direct call from abstraction
+       2. assertEquals("APP", DarlingCms\classes\component\App\App::APP_CONTAINER) // test direct call from class
+       3. assertEquals("APP", TestTrait::getApp()::APP_CONTAINER) // test instance call
+
+*******
+Methods
+*******
+
+testGetContainerReturnsValueOfAPP_CONTAINERConstant(): void
+   Hint:
+       1. assertEquals(App::APP_CONTAINER, App->getContainer())
+       2. assertEquals(App::APP_CONTAINER, App->export()['storable']->getContainer())
+
+testDeriveAppNameLocationReturnsAlphaNumericStringFormOfValueReturnedByParsingSpecifiedRequestsUrlToGetHost(): void
+   Hint:
+       1. assertEquals(
+       1.                 preg_replace("/[^A-Ba-b0-9]/", '', parse_url(TestTrait::getMockRequest()->getUrl(), PHP_URL_HOST)),
+       1.                 App::deriveAppNameLocationFromRequest(TestTrait::getMockRequest())
+       1.             )
+
+testGetNameAndGetLocationReturnSameValue()
+   Hint:
+       1. assertEquals(App->getName(), App->getLocation());
+       2. assertEquals(App->export()['storable']->getName(), App->export()['storable']->getLocation());
+
+testNameAndLocationWereSetUsingDeriveAppNameLocationFromRequestMethod()
+   Hint:
+       1. $expectedNameLocation = App::deriveNameLocationFromRequest(TestTrait::getMockRequest())
+       2. assertEquals($expectedNameLocation, App->getName());
+       2. assertEquals($expectedNameLocation, App->getLocation());
+       2. assertEquals($expectedNameLocation, App->export()['storable']->getName());
+       2. assertEquals($expectedNameLocation, App->export()['storable']->getLocation());
+
+
