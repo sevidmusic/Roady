99f3a0b659bcba269ff9d5f527068e9606d5f024
sevidmusic <sevidmusic@gmail.com>
2020-05-12 02:46:41 +0000

    unitTests: Continued development of App component. Implemented TestTrait methods: getRandomUrl() and getMockRequest() which are used to setup the mock Request used for testing. All tests are NOT passing, this commit marks a stepping stone, it SHOULD NOT be considered stable.

--- Tests/Unit/interfaces/component/Web/TestTraits/AppTestTrait.php
+++ Tests/Unit/interfaces/component/Web/TestTraits/AppTestTrait.php
@@ -3,11 +3,17 @@
 namespace UnitTests\interfaces\component\Web\TestTraits;
 
 use DarlingCms\interfaces\component\Web\App;
+use DarlingCms\interfaces\component\Web\Routing\Request;
+use DarlingCms\classes\component\Web\Routing\Request as CoreRequest;
+
+use DarlingCms\classes\primary\Storable;
+use DarlingCms\classes\primary\Switchable;
 
 trait AppTestTrait
 {
 
     private $app;
+    private $mockRequest;
 
     protected function setAppParentTestInstances(): void
     {
@@ -25,4 +31,41 @@ trait AppTestTrait
         $this->app = $app;
     }
 
+    private function getRandomUrl(): string
+    {
+        $urls = [
+            // Well formed urls
+            'http://192.168.33.10:80',
+            'http://192.168.33.10:80/index.php',
+            'http://192.168.33.10:80/index.php?foo=bar&baz=bazzer',
+            'http://dcms.dev',
+            'http://dcms.dev/index.php',
+            'http://dcms.dev/index.php?foo=bar',
+            // Malformed urls
+            '//192.168.33.10:80',
+            'http:192.168.33.10:80/index.php',
+            '/index.php?foo=bar&baz=bazzer',
+            '/',
+            './',
+            '../',
+            'dcms.dev/index.php',
+            'dcms.dev',
+            'dcms',
+            ];
+        return $urls[array_rand($urls)];
+    }
+
+    private function getMockRequest(): Request
+    {
+        if(!isset($this->mockRequest)) {
+            $this->mockRequest = new CoreRequest(new Storable("MockRequest", "Temp", "Temp"), new Switchable());
+            $this->mockRequest->import(['url' => $this->getRandomUrl()]);
+        }
+        return $this->mockRequest;
+    }
+
+    public function testTest(): void
+    {
+        var_dump($this->getMockRequest()->getUrl());
+    }
 }
