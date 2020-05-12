adb4cce67af957941189feda50e86b1f9ffa7e0a
sevidmusic <sevidmusic@gmail.com>
2020-05-11 20:27:23 -0400

    unitTests: Continued prep for developent of App component. Revised development template.

--- devInfo/core/intefaces/component/App/DevelopmentTemplate.php
+++ devInfo/core/intefaces/component/App/DevelopmentTemplate.php
@@ -1,3 +1,23 @@
+*************************************************************************
+****************************** App **************************************
+*************************************************************************
+
+Note: All code in this Component develeopment template is psuedo code,
+and is meant to serve as a guide, actual implementation will be
+different.
+
+*************************************************************************
+************************ Notes about this file **************************
+*************************************************************************
+
+  Hint: Hints provide psuedo code that is meant to serve as an
+        implementation guide.
+
+        Note: Hint numbers correspond pseudo code steps, NOT line numbers.
+              They are merely a formatting feature so the following command
+              can be run to get a summary of the DevelopmentTemplate:
+
+        grep -E 'tes[t]|[0-9][.]' /path/to/DevelopmentTemplate.txt | sed -e 's/tes[t]/\ntest/g;'
 
         In general it is important that the formatting of this document
         be consistent so tools like grep|awk|sed can be used to review
