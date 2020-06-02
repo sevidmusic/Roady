15d4c1eac052d11ed68dad1b954de9f41477f7ac
sevidmusic <sevidmusic@gmail.com>
2020-06-02 12:44:16 -0400

    Removed code templates to prep for update from DSH repo

--- templates/Primary/Abstraction.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DarlingCms\abstractions\primary;
-
-use DarlingCms\interfaces\primary\DS_COMPONENT_NAME as DS_COMPONENT_NAMEInterface;
-
-abstract class DS_COMPONENT_NAME implements DS_COMPONENT_NAMEInterface
-{
-}
--- templates/Primary/Interface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace DarlingCms\interfaces\primary;
-
-interface DS_COMPONENT_NAME
-{
-}
