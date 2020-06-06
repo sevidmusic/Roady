b124cb6086903697bb406e0788d0138b1273d55e
sevidmusic <sevidmusic@gmail.com>
2020-06-06 23:29:54 +0000

    Extensions|Contests: Continued development of CreateSubmission component. Began implementing testDoCreatesAndStoresSubmissionFromExpectedPostDataWhoseDataMatchesExpectedSubmissionExceptForUniqueIds(). So far the following data is being explicitly tested: pathToSubmittedFile, dateTimeOfSubmission timestamps, submitterName, submitterEmail, and submissionMetaData. All tests are passing.

--- Extensions/Contests/Tests/Unit/interfaces/component/Actions/TestTraits/CreateSubmissionTestTrait.php
+++ Extensions/Contests/Tests/Unit/interfaces/component/Actions/TestTraits/CreateSubmissionTestTrait.php
@@ -4,10 +4,16 @@ namespace Extensions\Contests\Tests\Unit\interfaces\component\Actions\TestTraits
 
 use DarlingCms\classes\component\Crud\ComponentCrud as CoreComponentCrud;
 use DarlingCms\classes\component\Driver\Storage\Standard as CoreStandardStorageDriver;
+use DarlingCms\classes\component\Web\App;
+use DarlingCms\classes\primary\Positionable;
 use DarlingCms\classes\primary\Storable;
 use DarlingCms\classes\primary\Switchable;
 use DarlingCms\interfaces\component\Crud\ComponentCrud;
+use DarlingCms\interfaces\component\Web\Routing\Request;
+use Extensions\Contests\core\classes\component\Contest\Submission as ContestSubmission;
+use Extensions\Contests\core\classes\component\Contest\Submitter;
 use Extensions\Contests\core\interfaces\component\Actions\CreateSubmission;
+use Extensions\Contests\core\interfaces\component\Contest\Submission;
 use RuntimeException;
 
 trait CreateSubmissionTestTrait
@@ -82,6 +88,148 @@ trait CreateSubmissionTestTrait
         );
     }
 
+    private function prepareCurrentRequestToMockExpectedPostRequest(): void
+    {
+        $this->getCreateSubmission()->getCurrentRequest()->import(
+            [
+                'post' => $this->mockPostArrayForRequestExpectedByDo()
+            ]
+        );
+        $this->verifyMockPostArrayAssignedToCurrentRequestIsConfiguredAsExpectedByDoMethod();
+
+    }
+
+    private function mockPostAndGetExpectedSubmission(): Submission
+    {
+        $this->prepareCurrentRequestToMockExpectedPostRequest();
+        return $this->mockCreateSubmissionFromPostData(
+            $this->getCreateSubmission()->getCurrentRequest()
+        );
+
+    }
+
+    public function testDoCreatesAndStoresSubmissionFromExpectedPostDataWhoseDataMatchesExpectedSubmissionExceptForUniqueIds(): void
+    {
+        $mockExpectedSubmission = $this->mockPostAndGetExpectedSubmission();
+        $this->getCreateSubmission()->do();
+        foreach($this->getStoredSubmissions($mockExpectedSubmission) as $storedSubmission) {
+            if($mockExpectedSubmission->getName() === $storedSubmission->getName()) {
+                $this->pathToSubmittedFilesMatch($mockExpectedSubmission, $storedSubmission);
+                $this->dateTimeOfSubmissionTimestampsMatch($mockExpectedSubmission, $storedSubmission);
+                $this->submitterNamesMatch($mockExpectedSubmission, $storedSubmission);
+                $this->submitterEmailsMatch($mockExpectedSubmission, $storedSubmission);
+                $this->submissionMetaDataMatches($mockExpectedSubmission, $storedSubmission);
+            }
+        }
+    }
+
+    private function submissionMetaDataMatches(Submission $expectedSubmission, Submission $actualSubmission): void {
+        $this->assertEquals(
+            $expectedSubmission->export()['metaData'],
+            $actualSubmission->export()['metaData']
+        );
+    }
+
+    private function submitterNamesMatch(Submission $expectedSubmission, Submission $actualSubmission): void {
+        $this->assertEquals(
+            $expectedSubmission->export()['submitter']->getName(),
+            $actualSubmission->export()['submitter']->getName()
+        );
+    }
+
+    private function submitterEmailsMatch(Submission $expectedSubmission, Submission $actualSubmission): void {
+        $this->assertEquals(
+            $expectedSubmission->export()['submitter']->getEmail(),
+            $actualSubmission->export()['submitter']->getEmail()
+        );
+    }
+
+    private function pathToSubmittedFilesMatch(Submission $expectedSubmission, Submission $actualSubmission): void {
+        $this->assertEquals(
+            $expectedSubmission->getPathToSubmittedFile(),
+            $actualSubmission->getPathToSubmittedFile()
+        );
+    }
+
+    private function dateTimeOfSubmissionTimestampsMatch(Submission $expectedSubmission, Submission $actualSubmission): void {
+        $this->assertEquals(
+            $expectedSubmission->export()['dateTimeOfSubmission']->getTimestamp(),
+            $actualSubmission->export()['dateTimeOfSubmission']->getTimestamp()
+        );
+    }
+
+    private function getStoredSubmissions(Submission $expectedSubmission): array
+    {
+        return $this->getMockCrud()->readAll(
+            $expectedSubmission->getLocation(),
+            $expectedSubmission->getContainer()
+        );
+    }
+
+    private function mockPostArrayForRequestExpectedByDo(): array
+    {
+        return [
+            'submitterName' => 'SubmitterName',
+            'submitterContainer' => 'SubmitterContainer',
+            'submitterEmail' => 'submitteremail@example.com',
+            'pathToSubmittedFile' => str_replace(
+                'Extensions/Contests/Tests/Unit/interfaces/component/Actions/TestTraits',
+                'devData/devImage.jpeg',
+                __DIR__
+            ),
+            'CreateSubmissionUniqueId' => $this->getCreateSubmission()->getUniqueId(),
+            'submissionName' => 'SubmissionName',
+            'submissionContainer' => 'SubmissionContainer'
+        ];
+    }
+
+    private function verifyMockPostArrayAssignedToCurrentRequestIsConfiguredAsExpectedByDoMethod(): void
+    {
+        $postDataKeys = array_keys($this->getCreateSubmission()->getCurrentRequest()->getPost());
+        if (
+            !in_array($this->getCreateSubmission()->getUniqueId(), $this->getCreateSubmission()->getCurrentRequest()->getPost())
+            ||
+            !in_array('CreateSubmissionUniqueId', $postDataKeys)
+            ||
+            !in_array('submitterName', $postDataKeys)
+            ||
+            !in_array('submitterContainer', $postDataKeys)
+            ||
+            !in_array('submitterEmail', $postDataKeys)
+            ||
+            !in_array('pathToSubmittedFile', $postDataKeys)
+            ||
+            !in_array('submissionName', $postDataKeys)
+            ||
+            !in_array('submissionContainer', $postDataKeys)
+        ) {
+            throw new RuntimeException('Mock Post Array for Request Expected by CreateSubmission::do() method is not configured correctly for tests, tests will fail till this is fixed.');
+        }
+    }
+
+    private function mockCreateSubmissionFromPostData(Request $request): Submission
+    {
+        //var_dump($request->getPost()['submitterEmail']);
+        return new ContestSubmission(
+            new Storable(
+                $request->getPost()['submissionName'],
+                App::deriveNameLocationFromRequest($request),
+                $request->getPost()['submissionContainer']
+            ),
+            new Switchable(),
+            new Positionable(),
+            new Submitter(
+                new Storable(
+                    $request->getPost()['submitterName'],
+                    App::deriveNameLocationFromRequest($request),
+                    $request->getPost()['submitterContainer']
+                ),
+                $request->getPost()['submitterEmail']
+            ),
+            $request->getPost()['pathToSubmittedFile']
+        );
+    }
+
     public function getMockCrud(): ComponentCrud
     {
         return new CoreComponentCrud(
@@ -99,94 +247,4 @@ trait CreateSubmissionTestTrait
         $this->setAction($this->getCreateSubmission());
         $this->setActionParentTestInstances();
     }
-    /*
-        public function testDoCreatesSubmissionFromPostDataIfCreateSubmissionInstancesUniqueIdIsSetInCurrentRequestsPostDataAndSubmitterNameAndSubmitterEmailAndPathToSubmittedFileKeysExistInRequestsPostData(): void
-        {
-            $this->getCreateSubmission()->getCurrentRequest()->import(
-                [
-                    'post' => $this->mockPostArrayForRequestExpectedByDo()
-                ]
-            );
-            $this->verifyMockPostArrayAssignedToCurrentRequestIsConfiguredAsExpectedByDoMethod();
-            $mockExpectedSubmission = $this->mockCreateSubmissionFromPostData(
-                $this->getCreateSubmission()->getCurrentRequest()
-            );
-            $this->getCreateSubmission()->do();
-            $this->getMockCrud()->create($mockExpectedSubmission);
-            $this->assertFalse(
-                empty(
-                    $this->getMockCrud()->readAll(
-                        $mockExpectedSubmission->getLocation(),
-                        $mockExpectedSubmission->getContainer()
-                    )
-                )
-            );
-            // check if submission was stored and that values match expected...
-        }
-        */
-    /*
-        private function mockPostArrayForRequestExpectedByDo(): array
-        {
-            return [
-                'submitterName' => 'SubmitterName',
-                'submitterContainer' => 'SubmitterContainer',
-                'submitterEmail' => 'submitteremail@example.com',
-                'pathToSubmittedFile' => str_replace(
-                    'Extensions/Contests/Tests/Unit/interfaces/component/Actions/TestTraits',
-                    'devData/devImage.jpeg',
-                    __DIR__
-                ),
-                'CreateSubmissionUniqueId' => $this->getCreateSubmission()->getUniqueId(),
-                'submissionName' => 'SubmissionName',
-                'submissionContainer' => 'SubmissionContainer'
-            ];
-        }
-
-        private function verifyMockPostArrayAssignedToCurrentRequestIsConfiguredAsExpectedByDoMethod(): void
-        {
-            $postDataKeys = array_keys($this->getCreateSubmission()->getCurrentRequest()->getPost());
-            if (
-                !in_array($this->getCreateSubmission()->getUniqueId(), $this->getCreateSubmission()->getCurrentRequest()->getPost())
-                ||
-                !in_array('CreateSubmissionUniqueId', $postDataKeys)
-                ||
-                !in_array('submitterName', $postDataKeys)
-                ||
-                !in_array('submitterContainer', $postDataKeys)
-                ||
-                !in_array('submitterEmail', $postDataKeys)
-                ||
-                !in_array('pathToSubmittedFile', $postDataKeys)
-                ||
-                !in_array('submissionName', $postDataKeys)
-                ||
-                !in_array('submissionContainer', $postDataKeys)
-            ) {
-                throw new RuntimeException('Mock Post Array for Request Expected by CreateSubmission::do() method is not configured correctly for tests, tests will fail till this is fixed.');
-            }
-        }
-
-        private function mockCreateSubmissionFromPostData(Request $request): Submission
-        {
-            var_dump($request->getPost()['submitterEmail']);
-            return new ContestSubmission(
-                new Storable(
-                    $request->getPost()['submissionName'],
-                    App::deriveNameLocationFromRequest($request),
-                    $request->getPost()['submissionContainer']
-                ),
-                new Switchable(),
-                new Positionable(),
-                new Submitter(
-                    new Storable(
-                        $request->getPost()['submitterName'],
-                        App::deriveNameLocationFromRequest($request),
-                        $request->getPost()['submitterContainer']
-                    ),
-                    $request->getPost()['submitterEmail']
-                ),
-                $request->getPost()['pathToSubmittedFile']
-            );
-        }
-    */
 }
--- Extensions/Contests/core/abstractions/component/Actions/CreateSubmission.php
+++ Extensions/Contests/core/abstractions/component/Actions/CreateSubmission.php
@@ -3,10 +3,13 @@
 namespace Extensions\Contests\core\abstractions\component\Actions;
 
 use DarlingCms\abstractions\component\Action as CoreAction;
+use DarlingCms\classes\component\Web\App;
 use DarlingCms\interfaces\component\Crud\ComponentCrud;
 use DarlingCms\interfaces\primary\Positionable;
 use DarlingCms\interfaces\primary\Storable;
 use DarlingCms\interfaces\primary\Switchable;
+use Extensions\Contests\core\classes\component\Contest\Submission;
+use Extensions\Contests\core\classes\component\Contest\Submitter;
 use Extensions\Contests\core\interfaces\component\Actions\CreateSubmission as CreateSubmissionInterface;
 use RuntimeException;
 
@@ -38,6 +41,53 @@ abstract class CreateSubmission extends CoreAction implements CreateSubmissionIn
     public function do(): bool
     {
         $this->import(['wasDone' => true]);
+        if ($this->postDataIsAsExpected() === true) {
+            $this->componentCrud->create(
+                new Submission(
+                    new \DarlingCms\classes\primary\Storable(
+                        $this->getCurrentRequest()->getPost()['submissionName'],
+                        App::deriveNameLocationFromRequest($this->getCurrentRequest()),
+                        $this->getCurrentRequest()->getPost()['submissionContainer']
+                    ),
+                    new \DarlingCms\classes\primary\Switchable(),
+                    new \DarlingCms\classes\primary\Positionable(),
+                    new Submitter(
+                        new \DarlingCms\classes\primary\Storable(
+                            $this->getCurrentRequest()->getPost()['submitterName'],
+                            App::deriveNameLocationFromRequest($this->getCurrentRequest()),
+                            $this->getCurrentRequest()->getPost()['submitterContainer']
+                        ),
+                        $this->getCurrentRequest()->getPost()['submitterEmail']
+                    ),
+                    $this->getCurrentRequest()->getPost()['pathToSubmittedFile']
+                )
+            );
+        }
+        return $this->wasDone();
+    }
+
+    private function postDataIsAsExpected(): bool
+    {
+        $postDataKeys = array_keys($this->getCurrentRequest()->getPost());
+        if (
+            !in_array($this->getUniqueId(), $this->getCurrentRequest()->getPost())
+            ||
+            !in_array('CreateSubmissionUniqueId', $postDataKeys)
+            ||
+            !in_array('submitterName', $postDataKeys)
+            ||
+            !in_array('submitterContainer', $postDataKeys)
+            ||
+            !in_array('submitterEmail', $postDataKeys)
+            ||
+            !in_array('pathToSubmittedFile', $postDataKeys)
+            ||
+            !in_array('submissionName', $postDataKeys)
+            ||
+            !in_array('submissionContainer', $postDataKeys)
+        ) {
+            return false;
+        }
         return true;
     }
 
