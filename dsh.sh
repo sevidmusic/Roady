be04f47724e77254e86d3f5f2863acc7f95d15bc
sevidmusic <sdmwebsdm@gmail.com>
2020-09-30 05:36:08 -0400

    The --help flag now will show flag info for the --start-app-server flag if any of the following terms are specified, or if no terms are specified:
    app, apps
    
    For example, all of the following uses of the --help flag
    will display flag info about the --start-app-server flag:
    
    dsh --help
    dsh --help app
    dsh --help apps
    
    Note for future flags:
    1. Must provide -N and --NNN flags types
    2. Must have corresponding help term(s)
       implemented for -h and --help flags.
    3. Help terms must trigger flag info to be shown for relevant flags when a term
       is specified. For example:
           dsh --help term | Show flag info for flags related to term
    4. If no terms are specified all flag info must be shown
           dsh --help | Show all flag info

--- dsh.sh
+++ dsh.sh
@@ -14,13 +14,17 @@ while test $# -gt 0; do
     -h|--help)
       showBanner
       shift
-      if [[ "${1}" == 'app' ]]
+      if [[ -z "${1}" || "${1}" == 'app' || "${1}" == 'apps' ]]
       then
           notifyUser "dsh provides various flags for working with apps." 0 'dontClear'
           notifyUser "The -s, or --start-app-server will start a local server for you to use while in development." 0 'dontClear'
           exit 0
       fi
-      notifyUser "-h was not supplied any arguments" && exit 0
+      notifyUser "${ERRORCOLOR}Invalid option supplied to -help flag." 0 'dontClear'
+      notifyUser "Options:" 0 'dontClear'
+      notifyUser "${HIGHLIGHTCOLOR}dsh --help${CLEAR_ALL_TEXT_STYLES}  ${HIGHLIGHTCOLOR}dsh --help app${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
+      notifyUser "${HIGHLIGHTCOLOR}dsh --help apps${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
+      exit 0
       ;;
     -s|--start-app-server*)
       shift
