fb30243dd187f56371e60439cebff6eda3dd780e
sevidmusic <sdmwebsdm@gmail.com>
2020-10-07 22:25:53 -0400

    dsh: Cleaned up dsh code. Also, added a message about licensing to phpunit.sh, as well as a copy of the PhpUnit license. Good to give credit where credit is due. : ) The message is shown the first time the -t flag is used, and not again unless the relevant cache file is removed.

--- dsh/phpunit.sh
+++ dsh/phpunit.sh
@@ -15,6 +15,37 @@ PATH_TO_DDMS="${PATH_TO_DSH_DIR//dsh/}"
 
 # shellcheck source=src/util.sh
 . "${PATH_TO_DSHUI}"
-showBanner
+
+if [ ! -f "${PATH_TO_DSH_DIR}/.dsh_license_notice_already_shown" ]
+then
+  clear && showBanner
+  notifyUser "PhpUnit will start in a moment. Please note, PhpUnit is not apart of" 0 'dontClear'
+  notifyUser "the Darling Data Managent System, it is a third party library developed by" 0 'dontClear'
+  notifyUser "Sebastian Bergmann." 0 'dontClear'
+  notifyUser "PhpUnit is a unit testing framework for Php applications." 0 'dontClear'
+  notifyUser "The official PhpUnit source can be found at:" 0 'dontClear'
+  notifyUser "${HIGHLIGHTCOLOR}https://github.com/sebastianbergmann/phpunit" 0 'dontClear'
+  sleep 4
+  clear && showBanner
+  notifyUser "A copy of the LICENSE associated with PhpUnit can be found at:" 0 'dontClear'
+  notifyUser "${HIGHLIGHTCOLOR}${PATH_TO_DSH_DIR}/PHP_UNIT_LICENSE" 0 'dontClear'
+  sleep 4
+  clear && showBanner
+  notifyUser "Note: The PHP_UNIT_LICENSE is not associated with the Darling Data" 0 'dontClear'
+  notifyUser "Management System, or dsh, which both are licensed under the MIT" 0 'dontClear'
+  notifyUser "license." 0 'dontClear'
+  notifyUser "The Darling Data Managemet System's license can be found at:" 0 'dontClear'
+  notifyUser "${HIGHLIGHTCOLOR}${PATH_TO_DDMS}LICENSE" 0 'dontClear'
+  notifyUser "dsh is part of the Darling Data Management system, and therfore uses the same license:" 0 'dontClear'
+  notifyUser "${HIGHLIGHTCOLOR}${PATH_TO_DDMS}LICENSE" 0 'dontClear'
+  sleep 4
+  clear && showBanner
+  notifyUser "This message will not show again unless the relevant .dsh_* cache file is deleted." 0 'dontClear'
+  sleep 4
+  showLoadingBar "Starting PhpUnit"
+fi
 
 "${PATH_TO_DDMS}vendor/phpunit/phpunit/phpunit" -c "${PATH_TO_DDMS}php.xml"
+
+echo "License message already shown" > "${PATH_TO_DSH_DIR}/.dsh_license_notice_already_shown"
+
