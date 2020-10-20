850bfbc59d5a17a181340df9888d43220a643ba3
sevidmusic <sdmwwebsdm@gmail.com>
2020-10-20 13:17:49 -0400

    dsh: Implemented the following functions: runPhpUnit, showHelpMsg, startAppServer. All tests are passing.

--- dsh/dshfunctions.sh
+++ dsh/dshfunctions.sh
@@ -4,6 +4,66 @@ set -o posix
 
 clear
 
+runPhpUnit() {
+    if [ ! -f "${PATH_TO_DSH_DIR}/.dsh_license_notice_already_shown" ]; then
+      clear && showBanner
+      notifyUser "PhpUnit will start in a moment. Please note, PhpUnit is not apart of" 0 'dontClear'
+      notifyUser "the Darling Data Managent System, it is a third party library developed by" 0 'dontClear'
+      notifyUser "Sebastian Bergmann." 0 'dontClear'
+      notifyUser "PhpUnit is a unit testing framework for Php applications." 0 'dontClear'
+      notifyUser "The official PhpUnit source can be found at:" 0 'dontClear'
+      notifyUser "${HIGHLIGHTCOLOR}https://github.com/sebastianbergmann/phpunit" 0 'dontClear'
+      sleep 4
+      clear && showBanner
+      notifyUser "A copy of the LICENSE associated with PhpUnit can be found at:" 0 'dontClear'
+      notifyUser "${HIGHLIGHTCOLOR}${PATH_TO_DSH_DIR}/PHP_UNIT_LICENSE" 0 'dontClear'
+      sleep 4
+      clear && showBanner
+      notifyUser "Note: The PHP_UNIT_LICENSE is not associated with the Darling Data" 0 'dontClear'
+      notifyUser "Management System, or dsh, which both are licensed under the MIT" 0 'dontClear'
+      notifyUser "license." 0 'dontClear'
+      notifyUser "The Darling Data Managemet System's license can be found at:" 0 'dontClear'
+      notifyUser "${HIGHLIGHTCOLOR}${PATH_TO_DDMS}LICENSE" 0 'dontClear'
+      notifyUser "dsh is part of the Darling Data Management system, and therfore uses the same license:" 0 'dontClear'
+      notifyUser "${HIGHLIGHTCOLOR}${PATH_TO_DDMS}LICENSE" 0 'dontClear'
+      sleep 4
+      clear && showBanner
+      notifyUser "This message will not show again unless the relevant .dsh_* cache file is deleted." 0 'dontClear'
+      sleep 4
+      showLoadingBar "Starting PhpUnit"
+    fi
+    "${PATH_TO_DDMS}vendor/phpunit/phpunit/phpunit" -c "${PATH_TO_DDMS}php.xml"
+    echo "License message already shown" >"${PATH_TO_DSH_DIR}/.dsh_license_notice_already_shown"
+}
+
+showHelpMsg() {
+    if [[ -z "${1}" || "${1}" == 'app' || "${1}" == 'apps' ]]; then
+        notifyUser "${HIGHLIGHTCOLOR}dsh${NOTIFYCOLOR} is a command line utility" 0 'dontClear'
+        notifyUser "that provides various utilities to aide in development with" 0 'dontClear'
+        notifyUser "the ${HIGHLIGHTCOLOR}Darling Data Management System${NOTIFYCOLOR}." 0 'dontClear'
+        notifyUser "The -s, or --start-app-server flag will start a local server" 0 'dontClear'
+        notifyUser "for you to use while in development." 0 'dontClear'
+        notifyUser "The -s flag expects one argument, a port number." 0 'dontClear'
+        notifyUser "    For example: ${HIGHLIGHTCOLOR}dsh -s 8080" 0 'dontClear'
+        notifyUser "    For example: ${HIGHLIGHTCOLOR}dsh --start-app-server 8888" 0 'dontClear'
+        notifyUser "The -t, or --test-ddms flag will run phpunit using ${HIGHLIGHTCOLOR}${PATH_TO_DDMS}php.xml${NOTIFYCOLOR} for configuration." 0 'dontClear'
+        notifyUser "    For example: ${HIGHLIGHTCOLOR}dsh -t" 0 'dontClear'
+        notifyUser "    For example: ${HIGHLIGHTCOLOR}dsh --test-ddms" 0 'dontClear'
+        exit 0
+   fi
+   notifyUser "${ERRORCOLOR}Invalid option supplied to -help flag." 0 'dontClear'
+   notifyUser "Options:" 0 'dontClear'
+   notifyUser "${HIGHLIGHTCOLOR}dsh --help${CLEAR_ALL_TEXT_STYLES}  ${HIGHLIGHTCOLOR}dsh --help app${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
+   notifyUser "${HIGHLIGHTCOLOR}dsh --help apps${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
+}
+
+startAppServer() {
+    newLine
+    notifyUser "Server is running @ ${HIGHLIGHTCOLOR}${BLACK_FG_COLOR}http://localhost:${1}${CLEAR_ALL_TEXT_STYLES}${NOTIFYCOLOR}, to stop use ${HIGHLIGHTCOLOR}${BLACK_FG_COLOR}CTRL-C${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
+    newLine
+    /usr/bin/php -S "localhost:${1}" -t "${PATH_TO_DDMS}" &> /dev/null & xdg-open "http://localhost:${1}" &>/dev/null & disown
+}
+
 disableCtrlC() {
     trap '' 2
     newLine
