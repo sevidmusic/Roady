548c00033c67dcffa3c084d7c68c9af851bc5d04
sevidmusic <sdmwwebsdm@gmail.com>
2020-10-26 19:21:21 -0400

    unitTests: Continued development of DynamicOutputComponent. Implemented testRuntimeExceptionIsThrownOnInstantiationIfAppDirectoryNamePropertyDoesNotMatchTheNameOfAnAppDirectroy(). All tests are passing.

--- Tests/Unit/interfaces/component/TestTraits/DynamicOutputComponentTestTrait.php
+++ Tests/Unit/interfaces/component/TestTraits/DynamicOutputComponentTestTrait.php
@@ -3,6 +3,7 @@
 namespace UnitTests\interfaces\component\TestTraits;
 
 use DarlingDataManagementSystem\interfaces\component\DynamicOutputComponent as DynamicOutputComponentInterface;
+use DarlingDataManagementSystem\classes\component\DynamicOutputComponent as CoreDynamicOutputComponent;
 use DarlingDataManagementSystem\classes\primary\Storable as CoreStorable;
 use DarlingDataManagementSystem\classes\primary\Switchable as CoreSwitchable;
 use DarlingDataManagementSystem\classes\primary\Positionable as CorePositionable;
@@ -38,13 +39,44 @@ trait DynamicOutputComponentTestTrait
                 'DynamicOutputComponentContainer'
             ),
             new CoreSwitchable(),
-            new CorePositionable()
+            new CorePositionable(),
+            'helloWorld'
         ];
+    }
+
+    private function expectedCurrentAppDirectoryPath(): string
+    {
+        return str_replace(
+            'Tests/Unit/interfaces/component/TestTraits',
+            'Apps/' . $this->getDynamicOutputComponent->export()['appDirectoryName'] . '/',
+            __DIR__
+        );
+    }
+
+    public function testRuntimeExceptionIsThrownOnInstantiationIfAppDirectoryNamePropertyDoesNotMatchTheNameOfAnAppDirectroy(): void
+    {
+        $randAppName = 'foo' . rand(100000,99999) . 'bar' . rand(100,999) . 'baz' . rand(10000000,99999999);
+        $this->expectException(RuntimeException::class);
+        $doc = new CoreDynamicOutputComponent(
+             new CoreStorable(
+                'DynamicOutputComponentName',
+                'DynamicOutputComponentLocation',
+                'DynamicOutputComponentContainer'
+            ),
+            new CoreSwitchable(),
+            new CorePositionable(),
+            $randAppName
+        );
     }
 
     private function expectedSharedDynamicOutputFileDirectoryPath(): string
     {
         return str_replace('Tests/Unit/interfaces/component/TestTraits', 'SharedDynamicOutput', __DIR__);
+    }
+
+    private function expectedCurrentAppsDynamicOutputFileDirectoryPath(): string
+    {
+        return str_replace('Tests/Unit/interfaces/component/TestTraits', 'Apps/' . $this->getDynamicOutputComponent()->export()['appDirectoryName'], __DIR__);
     }
 
     public function testGetSharedDynamicOutputFilesDirectoryPathReturnsExpectedPath(): void
