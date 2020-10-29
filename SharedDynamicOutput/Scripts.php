f4f49242a463de73cf7d3e2b0975debf498bf4c6
sevidmusic <sdmwwebsdm@gmail.com>
2020-10-29 13:16:22 -0400

    Apps: Continued development of helloWorld app.

--- SharedDynamicOutput/Scripts.php
+++ SharedDynamicOutput/Scripts.php
@@ -1,5 +1,5 @@
 <?php
-$path = str_replace('DynamicOutput', 'js', __DIR__);
+$path = str_replace('SharedDynamicOutput', 'Apps' . DIRECTORY_SEPARATOR . $this->appDirectoryName . DIRECTORY_SEPARATOR . 'js', __DIR__);
 $scripts = array_diff(scandir($path), array('.', '..'));
 
 foreach($scripts as $script)
