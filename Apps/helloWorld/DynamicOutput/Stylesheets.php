02b261b984aecc2bb0e32021ac6ebbeac220dc6d
sevidmusic <sdmwwebsdm@gmail.com>
2020-10-29 15:16:53 -0400

    Apps: Continued development of helloWorld app, and SharedDynamicOutput.

--- Apps/helloWorld/DynamicOutput/Stylesheets.php
+++ Apps/helloWorld/DynamicOutput/Stylesheets.php
@@ -1,5 +1,5 @@
 <?php
-$path = str_replace('SharedDynamicOutput', 'Apps' . DIRECTORY_SEPARATOR . $this->appDirectoryName . DIRECTORY_SEPARATOR . 'css', __DIR__);
+$path = str_replace('DynamicOutput', 'css', __DIR__);
 $stylesheets = array_diff(scandir($path), array('.', '..'));
 
 foreach($stylesheets as $stylesheet)
