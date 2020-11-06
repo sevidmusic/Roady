4f83ceb13772364e51bbc1c2888d5e780853a1b5
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-06 15:00:31 -0500

    Apps: Continued development of helloUniverse app. All tests are passing.

--- Apps/helloUniverse/DynamicOutput/Stylesheets.php
+++ Apps/helloUniverse/DynamicOutput/Stylesheets.php
@@ -1,3 +1,1 @@
-
-<link rel="stylesheet" href="' . $appComponentsFactory->getAppDomain()->getUrl() . '/Apps/helloWorld/css/styles.css">
-
+    <link rel="stylesheet" href="./Apps/helloWorld/css/styles.css">
