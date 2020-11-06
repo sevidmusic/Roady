a1d53f0615441005e36c4640f5214f377a388163
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-06 16:30:32 -0500

    Apps: Continued development of helloUniverse dev app. All tests are passing.

--- Apps/helloUniverse/Responses/Homepage.php
+++ Apps/helloUniverse/Responses/Homepage.php
@@ -6,7 +6,12 @@ $appComponentsFactory->buildResponse(
     $appComponentsFactory->buildRequest(
         'HomepageRequest',
         'Requests',
-        $appComponentsFactory->getApp()->getAppDomain()->getUrl() . '/index.php',
+        $appComponentsFactory->getApp()->getAppDomain()->getUrl() . '/', // @todo possibly create new issue: Response should match getUrl() and getUrl() . '/', i.e. with or without trailing slash, or getUrl() should include trailing slash, either way this is annoying because getUrl() and getUrl() . '/' are different...
+    ),
+    $appComponentsFactory->buildRequest(
+        'HomepageRequest',
+        'Requests',
+        $appComponentsFactory->getApp()->getAppDomain()->getUrl() . '/index.php', // @todo possibly create new issue: Response should match getUrl() and getUrl() . '/', i.e. with or without trailing slash, or getUrl() should include trailing slash, either way this is annoying because getUrl() and getUrl() . '/' are different...
     ),
     $appComponentsFactory->buildDynamicOutputComponent(
         'Welcome',
