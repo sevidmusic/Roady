232b28f58eb34f0e2e6617e0267327a9ddf547c6
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-08 18:12:34 -0500

    Apps: Continued development of helloUniverse dev app.  All tests are passing.

--- Apps/helloUniverse/DynamicOutput/MainMenu.php
+++ Apps/helloUniverse/DynamicOutput/MainMenu.php
@@ -1,6 +1,6 @@
 <div class="main-menu">
     <ul class="main-menu-links">
-        <li><a href="./">Home</a></li>
+        <li><a href="./index.php">Home</a></li>
         <li><a href="https://github.com/sevidmusic/DarlingDataManagementSystem">GitHub</a></li>
         <li><a href="https://www.youtube.com/channel/UCfR-CPqfSIvCFnYT1m4s66w">YouTube</a></li>
     </ul>
