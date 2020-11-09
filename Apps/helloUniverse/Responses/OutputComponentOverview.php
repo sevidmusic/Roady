c44f70511300a17911c0661a708de921d6476037
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-08 21:44:34 -0500

    Apps: Continued development of helloUniverse app. Implemented OutputComponentOverview. All tests are passing.

---
+++ Apps/helloUniverse/Responses/OutputComponentOverview.php
@@ -0,0 +1,19 @@
+<?php
+
+$appComponentsFactory->buildResponse(
+    'OutputComponentOverview',
+    2,
+    $appComponentsFactory->buildRequest(
+        'OutputComponentOverviewRequest',
+        'Requests',
+        $appComponentsFactory->getApp()->getAppDomain()->getUrl() . '/index.php?outputComponentOverview',
+    ),
+    $appComponentsFactory->buildDynamicOutputComponent(
+        'OutputComponentOverview',
+        'Output',
+        0,
+        'helloUniverse',
+        'OutputComponentOverview.php'
+    ),
+);
+
