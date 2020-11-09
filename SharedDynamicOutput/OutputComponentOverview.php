341b81d0f848d314de49cf4504f119271d138892
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-08 21:28:20 -0500

    Apps: Continued development of helloUniverse app. All tests are passing.

--- SharedDynamicOutput/OutputComponentOverview.php
+++ SharedDynamicOutput/OutputComponentOverview.php
@@ -27,10 +27,34 @@ $crud = new ComponentCrud(
     <div class="component-info font-audio-wide code-text-color">
         <h3>"Output Component Overview" Component's Info</h3>
         <p>Name: <?php echo $this->getName(); ?></p>
-        <p>Type: <?php echo $this->getType(); ?></p>
         <p>Unique Id: <?php echo substr($this->getUniqueId(), 0, 17); ?>...</p>
         <p>Storage Location: <?php echo $this->getLocation(); ?></p>
         <p>Storage Container: <?php echo $this->getContainer(); ?></p>
+        <p>Type: <?php echo $this->getType(); ?></p>
     </div>
+</div>
+<div class="output font-concert-one">
+<?php
+    $bgcolors = ['#000000', '#333333'];
+    $bgcolor = ['#000000'];
+    foreach($crud->readAll($this->getLocation(), 'Output') as $outputComponent) {
+        if($outputComponent->getName() !== 'OutputComponentOverview') {
+            $bgcolor = ($bgcolor === $bgcolors[0] ? $bgcolors[1] : $bgcolors[0]);
+            echo '
+    <div class="component-info font-audio-wide code-text-color" style="padding: .5em; background: ' . $bgcolor . ';">
+        <p>Name: ' . $outputComponent->getName() . '</p>
+        <p>Unique Id: ' . substr($outputComponent->getUniqueId(), 0, 17) . '...</p>
+        <p>Storage Location: ' . $outputComponent->getLocation() . '</p>
+        <p>Storage Container: ' . $outputComponent->getContainer() . '</p>
+        <p>Type: ' . $outputComponent->getType() . '</p>
+        <p>Output:</p>
+        <div style="background: red; color: balck;">
+        ' . str_replace(['<', '>'], ['&lt;', '&gt;'], $outputComponent->getOutput()) . '
+        </div>
+    </div>
+            ';
+        }
+    }
+?>
 </div>
 
