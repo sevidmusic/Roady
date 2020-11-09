2f3f546b365f90f1b0f57926b23f17157698f7e3
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-08 20:39:31 -0500

    Apps: Continued development of helloUniverse app. All tests are passing.

--- Apps/helloUniverse/Components.php
+++ Apps/helloUniverse/Components.php
@@ -21,8 +21,9 @@ $appComponentsFactory = new AppComponentsFactory(
 );
 
 require __DIR__ . DIRECTORY_SEPARATOR . 'Responses/OpeningHtml.php';
-require __DIR__ . DIRECTORY_SEPARATOR . 'Responses/Homepage.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'Responses/LogoMainMenu.php';
+require __DIR__ . DIRECTORY_SEPARATOR . 'Responses/Homepage.php';
+require __DIR__ . DIRECTORY_SEPARATOR . 'Responses/OutputComponentOverview.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'Responses/ClosingHtml.php';
 
 $appComponentsFactory->buildLog(
--- SharedDynamicOutput/OutputComponentOverview.php
+++ SharedDynamicOutput/OutputComponentOverview.php
@@ -1,18 +1,36 @@
+<?php
+
+use DarlingDataManagementSystem\classes\component\Crud\ComponentCrud;
+use DarlingDataManagementSystem\classes\component\Driver\Storage\FileSystem\JsonStorageDriver;
+use DarlingDataManagementSystem\classes\primary\Storable;
+use DarlingDataManagementSystem\classes\primary\Switchable;
+
+$crud = new ComponentCrud(
+    new Storable(
+        'OCOverviewCrud',
+        'Temp',
+        'Temp'
+    ),
+    new Switchable(),
+    new JsonStorageDriver(
+        new Storable(
+            'OCOverviewStorageDriver',
+            'Temp',
+            'Temp'
+        ),
+        new Switchable()
+    )
+);
+
+?>
 <div class="output font-concert-one">
-    <p>
-        Welcome to the Darling Data Management System,
-        a tool designed to aide in the development of
-        well organized PHP applications.
-    </p>
     <div class="component-info font-audio-wide code-text-color">
-        <h3>Output Component's Info</h3>
+        <h3>"Output Component Overview" Component's Info</h3>
         <p>Name: <?php echo $this->getName(); ?></p>
         <p>Type: <?php echo $this->getType(); ?></p>
         <p>Unique Id: <?php echo substr($this->getUniqueId(), 0, 17); ?>...</p>
         <p>Storage Location: <?php echo $this->getLocation(); ?></p>
         <p>Storage Container: <?php echo $this->getContainer(); ?></p>
     </div>
-    <p>View The Darling Data Management System  GitHub.com:</p>
-    <p><a href="https://github.com/sevidmusic/DarlingDataManagementSystem">https://github.com/sevidmusic/DarlingDataManagementSystem</a></p>
 </div>
 
