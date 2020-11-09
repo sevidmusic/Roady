7ba9e039ed7ea4a60c46661925e4772231c3fa8c
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-09 15:41:28 -0500

    Apps: Removed helloUniverse app. It has been replaced by the starterApp.

--- Apps/helloUniverse/DynamicOutput/Logo.php
+++
@@ -1,5 +0,0 @@
-<div class="logo animate-logo">
-    <h1>Darling Data Management System</h1>
-    <p class="logo-description highlight-text-color">Develop Well Organized PHP Applications</p>
-</div>
-
--- Apps/helloUniverse/DynamicOutput/MainMenu.php
+++
@@ -1,8 +0,0 @@
-<div class="main-menu">
-    <ul class="main-menu-links">
-        <li><a href="./index.php">Home</a></li>
-        <li><a href="./index.php?outputComponentOverview">Output</a></li>
-        <li><a href="https://github.com/sevidmusic/DarlingDataManagementSystem">GitHub</a></li>
-        <li><a href="https://www.youtube.com/channel/UCfR-CPqfSIvCFnYT1m4s66w">YouTube</a></li>
-    </ul>
-</div>
