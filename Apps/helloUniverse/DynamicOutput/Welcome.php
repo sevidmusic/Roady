8499103bb8b4a0b38b34f07553e495168b92fde8
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-08 19:39:50 -0500

    Apps: Continued development of helloUniverse app. All tests are passing.

--- Apps/helloUniverse/DynamicOutput/Welcome.php
+++ Apps/helloUniverse/DynamicOutput/Welcome.php
@@ -1,14 +1,21 @@
 <div class="output font-concert-one">
-    <p>Welcome to the Darling Data Management System, a tool designed to aide in the development of well organized PHP applications.</p>
-    <p>View The Darling Data Management System  GitHub.com:</p>
-    <p><a href="https://github.com/sevidmusic/DarlingDataManagementSystem">https://github.com/sevidmusic/DarlingDataManagementSystem</a></p>
-    <div class="component-info font-audio-wide code-text-color">
-        <h3>Output Component's Info</h3>
-        <p>Name: <?php echo $this->getName(); ?></p>
-        <p>Type: <?php echo $this->getType(); ?></p>
-        <p>Unique Id: <?php echo substr($this->getUniqueId(), 0, 17); ?>...</p>
-        <p>Storage Location: <?php echo $this->getLocation(); ?></p>
-        <p>Storage Container: <?php echo $this->getContainer(); ?></p>
-    </div>
+    <p>
+        Welcome to the Darling Data Management System,
+        a tool designed to aide in the development of
+        well organized PHP applications.
+    </p>
+    <p>
+        You are viewing the helloUniverse App which is
+        intended to serve as a demo, and can also be
+        used as a "starter" App.
+        To use this app as the base for a new app,
+        make a copy of Apps/helloUniverse with an
+        appropriate name, and place it in the Apps
+        directory.
+    </p>
+    <p>
+        You can then modify this new Apps source code
+        to suit your needs.
+    </p>
 </div>
 
