3ecdaf7855a97baab69af746ee5c5e6737b54f04
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-14 00:47:52 -0500

    CoreApps: Continued development of Documentation app. All tests are passing.

--- Apps/Documentation/DynamicOutput/Welcome.php
+++ Apps/Documentation/DynamicOutput/Welcome.php
@@ -4,33 +4,11 @@
         a tool designed to aide in the development of
         well organized PHP applications.
     </p>
-    <p>
-        You are viewing the Documentation App which is
-        intended to serve as a demo, and can also be
-        used as a base to develop a new App.
-    </p>
-    <p>
-        To use the Documentation app as the base for a
-        new app, make a copy of Apps/Documentation with an
-        appropriate name, and place it in the Apps
-        directory, i.e., Apps/NewAppName.
-    </p>
-    <p>
-        You can then modify this new App's source code
-        to suit your needs.
-    </p>
-    <p>
-        The benefit of using the Documentation as a base is you start
-        out with a working App with some defaults that can be used
-        as they are, or serve as a guide to help you figure out
-        how to build your new App.
-    </p>
-    <p>
-        To learn about how to develop with the Darling Data Management System,
-        take a look at the Documentation's source code, or use dsh -r Documentation
-        to start up the Documentation app which provides detailed information
-        on how the Darling Data Management System works, and how you can use
-        it to accomplish your goals.
-    </p>
+    <div class="landing-page-menu">
+        <ul class="landing-page-links">
+            <li><a href="./index.php?whatIsIt">What is the Darling Data Management System</a></li>
+            <li><a href="./index.php?howItWorks">How the Darling Data Management System Works</a></li>
+            <li><a href="./index.php?howToUseIt">How to use the Darling Data Management System</a></li>
+    </div>
 </div>
 
