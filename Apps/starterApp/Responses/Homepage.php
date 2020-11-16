177214980834979c5bee588bc458075af2245213
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-16 01:10:19 -0500

    CoreApps: Continued refactoring starterApp to use new ComponentCrud()->readByNameAndType() method to reference OutputComponents instead of using variables. All test are passing.

--- Apps/starterApp/Responses/Homepage.php
+++ Apps/starterApp/Responses/Homepage.php
@@ -1,10 +1,29 @@
 <?php
 
+use DarlingDataManagementSystem\classes\component\OutputComponent;
+use DarlingDataManagementSystem\classes\component\DynamicOutputComponent;
+use DarlingDataManagementSystem\classes\component\Web\Routing\Request;
+
 $appComponentsFactory->buildResponse(
     'Homepage',
     2,
-    $rootRequest,
-    $homepageRequest,
-    $welcome,
+    $appComponentsFactory->getComponentCrud()->readByNameAndType(
+        'RootRequest',
+        Request::class,
+        $appComponentsFactory->getLocation(),
+        'Requests'
+    ),
+    $appComponentsFactory->getComponentCrud()->readByNameAndType(
+        'HomepageRequest',
+        Request::class,
+        $appComponentsFactory->getLocation(),
+        'Requests'
+    ),
+    $appComponentsFactory->getComponentCrud()->readByNameAndType(
+        'Welcome',
+        DynamicOutputComponent::class,
+        $appComponentsFactory->getLocation(),
+        'Output'
+    ),
 );
 
--- Apps/starterApp/Responses/OutputComponentOverview.php
+++ Apps/starterApp/Responses/OutputComponentOverview.php
@@ -1,9 +1,23 @@
 <?php
 
+use DarlingDataManagementSystem\classes\component\OutputComponent;
+use DarlingDataManagementSystem\classes\component\DynamicOutputComponent;
+use DarlingDataManagementSystem\classes\component\Web\Routing\Request;
+
 $appComponentsFactory->buildResponse(
     'OutputComponentOverview',
     2,
-    $outputComponentOverviewRequest,
-    $outputComponentOverview
+    $appComponentsFactory->getComponentCrud()->readByNameAndType(
+        'OutputComponentOverviewRequest',
+        Request::class,
+        $appComponentsFactory->getLocation(),
+        'Requests'
+    ),
+    $appComponentsFactory->getComponentCrud()->readByNameAndType(
+        'OutputComponentOverview',
+        DynamicOutputComponent::class,
+        $appComponentsFactory->getLocation(),
+        'Output'
+    ),
 );
 
