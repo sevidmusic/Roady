661cce4c6a5d86326a7aea5be4fc73966ca0c472
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-17 00:44:28 -0500

    dsh: Replace dshtests.sh with dshUnitTests.sh. Began implementing new tests.

--- dsh/dshUnitTests.sh
+++ dsh/dshUnitTests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 set -o posix
+
+clear
+
+APP="${1}"
+TESTGROUP="${2:-all}"
+[[ -z "${APP}" ]] && printf "\nPlease specify the name of an existing App directory to run tests against.\n" && exit 1
 
 setupPaths() {
     # Determine real path to dsh directory
@@ -15,14 +20,55 @@ setupPaths() {
     PATH_TO_DSH_FUNCTIONS="${PATH_TO_DSH_DIR}/dshfunctions.sh"
     PATH_TO_DDMS="${PATH_TO_DSH_DIR/dsh/}"
 }
-
 loadLibrary() {
     [[ ! -f "${1}" ]] && printf "\n\n\e[33mError! Failed to load ${1}!\e[0m\n\n" && exit 1
     . "${1}"
 }
-
 setupPaths
-
+loadLibrary "${PATH_TO_DSH_FUNCTIONS}"
 loadLibrary "${PATH_TO_DSHUI}"
 
-showBanner "dsh Unit Tests"
+[[ ! -d "${PATH_TO_DDMS}/Apps/${APP}" ]] && printf "\nPlease specify the name of an existing App directory to run tests against.\n" && exit 1
+# dsh --help
+if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'help' ]]; then
+    $PATH_TO_DSH_DIR/dsh --help
+    $PATH_TO_DSH_DIR/dsh -h
+    $PATH_TO_DSH_DIR/dsh --help help
+    $PATH_TO_DSH_DIR/dsh -h h
+fi
+
+if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'start-app-server' ]]; then
+    $PATH_TO_DSH_DIR/dsh --help start-app-server
+    dsh --start-app-server 8080
+    $PATH_TO_DSH_DIR/dsh -h s
+    dsh -s 8080
+fi
+
+if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'test-ddms' ]]; then
+    $PATH_TO_DSH_DIR/dsh --help test-ddms
+    dsh --test-ddms
+    $PATH_TO_DSH_DIR/dsh -h t
+    dsh -t
+fi
+
+if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'build-app' ]]; then
+    $PATH_TO_DSH_DIR/dsh --help build-app
+    dsh --build-app "${APP}"
+    $PATH_TO_DSH_DIR/dsh -h b
+    dsh -b "${APP}"
+fi
+
+if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'run-app' ]]; then
+    $PATH_TO_DSH_DIR/dsh --help run-app
+    dsh --run-app "${APP}"
+    $PATH_TO_DSH_DIR/dsh -h r
+    dsh -r "${APP}"
+fi
+
+if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'active-development-servers' ]]; then
+    $PATH_TO_DSH_DIR/dsh --help active-development-servers
+    dsh --active-development-servers
+    $PATH_TO_DSH_DIR/dsh -h j
+    dsh -j
+fi
+
