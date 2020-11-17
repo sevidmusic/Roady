439973f95f58ca6764e03d920ace716ebf423d69
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-17 16:26:04 -0500

    dsh: Implemented use of assertSuccess in dshUnitTests ndoc test group. All tests are passing.

--- dsh/dshUnitTests.sh
+++ dsh/dshUnitTests.sh
@@ -4,7 +4,9 @@ set -o posix
 clear
 
 APP="${1}"
+
 TESTGROUP="${2:-all}"
+
 [[ -z "${APP}" ]] && printf "\nPlease specify the name of an existing App directory to run tests against.\n" && exit 1
 
 setupPaths() {
@@ -20,13 +22,33 @@ setupPaths() {
     PATH_TO_DSH_FUNCTIONS="${PATH_TO_DSH_DIR}/dshfunctions.sh"
     PATH_TO_DDMS="${PATH_TO_DSH_DIR/dsh/}"
 }
+
 loadLibrary() {
     [[ ! -f "${1}" ]] && printf "\n\n\e[33mError! Failed to load ${1}!\e[0m\n\n" && exit 1
     . "${1}"
 }
+
+#######
+
+assertSuccess() {
+    { ${1} &> /dev/null; } && notifyUser "${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR} ran without error : )" 0 'dontClear' && return
+    notifyUser "${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR}: ${ERRORCOLOR}An error occured, run ${CLEAR_ALL_TEXT_STYLES}${HIGHLIGHTCOLOR}${1}${CLEAR_ALL_TEXT_STYLES}${ERRORCOLOR} manually to see error messages." 0 'dontClear'
+}
+
+assertError() {
+    { ${1} &> /dev/null; } && notifyUser "${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR}: ${ERRORCOLOR}An error  did not occur, an error was expected, run ${CLEAR_ALL_TEXT_STYLES}${HIGHLIGHTCOLOR}${1}${CLEAR_ALL_TEXT_STYLES}${ERRORCOLOR} manually to see actual output." 0 'dontClear' && return
+    notifyUser "As expected, an error occured running ${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR}" 0 'dontClear'
+}
+
+######
+
+
 setupPaths
+
 loadLibrary "${PATH_TO_DSH_FUNCTIONS}"
+
 loadLibrary "${PATH_TO_DSHUI}"
+
 
 [[ ! -d "${PATH_TO_DDMS}/Apps/${APP}" ]] && printf "\nPlease specify the name of an existing App directory to run tests against.\n" && exit 1
 # dsh --help
@@ -79,34 +101,10 @@ if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'stop-all-development-servers
     dsh -k
 fi
 
-#######
-
-assertSuccess() {
-    { ${1} &> /dev/null; } && notifyUser "${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR} ran without error : )" 0 'dontClear' && return
-    notifyUser "${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR}: ${ERRORCOLOR}An error occured, run ${CLEAR_ALL_TEXT_STYLES}${HIGHLIGHTCOLOR}${1}${CLEAR_ALL_TEXT_STYLES}${ERRORCOLOR} manually to see error messages." 0 'dontClear'
-}
-
-assertError() {
-    { ${1} &> /dev/null; } && notifyUser "${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR}: ${ERRORCOLOR}An error  did not occur, an error was expected, run ${CLEAR_ALL_TEXT_STYLES}${HIGHLIGHTCOLOR}${1}${CLEAR_ALL_TEXT_STYLES}${ERRORCOLOR} manually to see actual output." 0 'dontClear' && return
-    notifyUser "As expected, an error occured running ${HIGHLIGHTCOLOR}${1}${NOTIFYCOLOR}" 0 'dontClear'
-}
-assertSuccess 'ls'
-assertSuccess 'ldfs'
-assertError 'ls'
-assertError 'ldfs'
-
-exit 0
-
-######
-
 if [[ "${TESTGROUP}" == 'all' || "${TESTGROUP}" == 'ndoc' ]]; then
-    $PATH_TO_DSH_DIR/dsh --help new DynamicOutputComponent
-    sleep 7
-    dsh --new DynamicOutputComponent starterApp TestDoc 4.2 Welcome.php
-    sleep 3
-    $PATH_TO_DSH_DIR/dsh -h n doc
-    sleep 7
-    dsh -n doc starterApp TestDoc 4.2 Welcome.php
-    sleep 3
+    assertSuccess "${PATH_TO_DSH_DIR}/dsh --help new DynamicOutputComponent"
+    assertSuccess "${PATH_TO_DSH_DIR}/dsh --new DynamicOutputComponent starterApp TestDoc 4.2 Welcome.php"
+    assertSuccess "$PATH_TO_DSH_DIR/dsh -h n doc"
+    assertSuccess "dsh -n doc starterApp TestDoc 4.2 Welcome.php"
 fi
 
