609ccbc0c1b8011916df9fe9d6296270110cc6ec
sevidmusic <sdmwwebsdm@gmail.com>
2020-11-19 17:49:58 -0500

    dsh: Code clean up in dshUnit and test files. DO NOT REVERT TO THIS COMMIT, IT IS UNSTABLE.

--- dsh/dshUnitTests/newMODE.sh
+++ dsh/dshUnitTests/newMODE.sh
@@ -5,7 +5,10 @@ clear
 # THIS LINE IS VERY IMPORTANT | IT IS THE ONLY LINE REQUIRED FOR ALL TEST FILES source dshUnit
 . "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd | sed 's/dshUnitTests//g')dshUnit"
 
-APP="TestApp${RANDOM}"
+setRandomTestAppName() {
+    APP="TestApp${RANDOM}"
+    [[ -z "${APP}" ]] && notifyUser "${ERRORCOLOR}A random App name could not be generated for testing." 0 'dontClear' && exit 1
+}
 
 TESTGROUP="${1:-all}"
 
@@ -38,8 +41,7 @@ tearDownTestAppDirectory() {
     rm -R "$(getTestAppDirectory)"
 }
 
-[[ -z "${APP}" ]] && notifyUser "${ERRORCOLOR}A random App name could not be generated for testing." 0 'dontClear' && exit 1
-
+setRandomTestAppName
 setUpTestAppDirectory
 
 [[ ! -d "$(getTestAppDirectory)" ]] && notifyUser "${ERRORCOLOR}A test App does not could not be created at $(getTestAppDirectory)." 0 'dontClear' && exit 1
