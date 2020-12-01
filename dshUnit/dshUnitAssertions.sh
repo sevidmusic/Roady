b6d8d846a15a45264bc5ac1a86402725af335ef9
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-01 07:59:09 -0500

    dshUnit: Added text styles to output of assertSuccess. All dshUnit tests are passing. This commit is related to issue #26.

--- dshUnit/dshUnitAssertions.sh
+++ dshUnit/dshUnitAssertions.sh
@@ -7,11 +7,11 @@ assertSuccess() {
     showLoadingBar "    Testing that an error does not occur running: ${HIGHLIGHTCOLOR}${1}" 'dontClear'
     error="$( ${1} 2>&1 1>/dev/null)"
     if [ $? -eq 0 ]; then
-        notifyUser "    ${2}${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
+        notifyUser "${CLEAR_ALL_TEXT_STYLES}    ${SUCCESS_COLOR}${2}${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
         ((PASSES++))
     else
-        notifyUser "    ${3}${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
-        notifyUser "    ${ERRORCOLOR}${error}" 0 'dontClear'
+        notifyUser "${CLEAR_ALL_TEXT_STYLES}    ${ERRORCOLOR}${3}${CLEAR_ALL_TEXT_STYLES}" 0 'dontClear'
+        notifyUser "${CLEAR_ALL_TEXT_STYLES}    ${ERRORCOLOR}${error}" 0 'dontClear'
         ((FAILS++))
     fi
 }
