a847f7842da2d5d67171eb6c805db430225a1901
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-02 01:42:45 -0500

    dshUnit: Refactored dshUnit, all dshUnit tess are passing again. This commit is related to issue #26.

--- dshUnit/dshUnitAssertions.sh
+++ dshUnit/dshUnitAssertions.sh
@@ -11,13 +11,13 @@ assertNoError() {
     showLoadingBar "    Testing: ${CLEAR_ALL_STYLES}${COLOR_19}assertNoError \"${CLEAR_ALL_STYLES}${COLOR_21}${1}${CLEAR_ALL_STYLES}${COLOR_19}\"" 'dontClear'
     error="$( ${1} 2>&1 1>/dev/null)"
     if [ $? -eq 0 ]; then
-        #((PASSES++))
+        ((PASSES++))
         [[ "${initial_passes}" -lt "${PASSES}" ]] && notifyUser "${CLEAR_ALL_STYLES}    ${SUCCESS_COLOR}Test Passed ${HIGHLIGHTCOLOR}:)" 0 'dontClear' && return
         notifyUser "    ${CLEAR_ALL_STYLES}${COLOR_17}Warning: The test passed, however, the number of PASSES was not increased!" 0 'dontClear'
     else
         notifyUser "${CLEAR_ALL_STYLES}    ${ERROR_COLOR}An error occurred!" 0 'dontClear'
         notifyUser "${CLEAR_ALL_STYLES}    Error Message: ${ERROR_COLOR}${error}" 0 'dontClear'
-        #((FAILS++))
+        ((FAILS++))
         [[ "${initial_fails}" -lt "${FAILS}" ]] &&  notifyUser ${CLEAR_ALL_STYLES}    "${ERROR_COLOR}Test Failed ${HIGHLIGHTCOLOR}:(" 0 'dontClear' && return
         notifyUser "    ${CLEAR_ALL_STYLES}${COLOR_17}Warning: The test failed, but the number of FAILS was not increased!" 0 'dontClear'
     fi
