bf9a3bdef30260241d58ddb7a15b0a77343fcefb
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-03 18:44:35 -0500

    dshUnit: Code clean up, UI clean up, implemented tracking of passing "Tests". Note: Not all dshUnit tests are passing in this commit on purpose. Some tests were refactored to fail to make sure tracking of passing and failing assertions and tests is working. All manual dshUnit tests are passing including manual refactors intended to test tracking of failing tests and assertions. This commit is related to issue #26.

--- dshUnit/AssertNoErrorTests.sh
+++ dshUnit/AssertNoErrorTests.sh
@@ -4,10 +4,15 @@
 set -o posix
 
 testAssertNoErrorIndicatesPassingTestForCommandsThatAreExpectedToPass() {
+    local initial_passes
+    initial_passes="${PASSING_ASSERTIONS}"
     showRunningTestMsg "testAssertNoErrorIndicatesPassingTestForCommandsThatAreExpectedToPass"
     assertNoError "ls" "assertNoError MUST run without error on system command ${HIGHLIGHTCOLOR}ls${NOTIFY_COLOR}."
     assertNoError "pwd" "assertNoError MUST run without error on system command ${HIGHLIGHTCOLOR}pwd${NOTIFY_COLOR}."
     assertNoError "assertNoError ls '${test_msg}'" "assertNoError MUST run without error on itself."
+    assertNoError 'asdfjkdfj'
+    [[ "$((initial_passes + 4))" == "${PASSING_ASSERTIONS}" ]] && increasePassingTests && return
+    increaseFailingTests
 }
 
 testAssertNoErrorIndicatesPassingTestForCommandsThatAreExpectedToPass
@@ -18,7 +23,7 @@ testAssertNoErrorIncreasesPASSING_ASSERTIONSOnPassingAssertion() {
     showRunningTestMsg "testAssertNoErrorIncreasesPASSING_ASSERTIONSOnPassingAssertion"
     assertNoError 'echo There should not be any errors and PASSING_ASSERTIONS MUST increase' "assertNoError MUST increase the number of PASSING_ASSERTIONS on passing assertion."
     notifyUser "${HIGHLIGHTCOLOR}Note: The previous call to assertNoError's results will not be tracked, it was just used to test that PASSING_ASSERTIONS are increased by assertNoError on a pssing assertion." 0 'dontClear'
-    [[ "${initial_passes}" -lt "${PASSING_ASSERTIONS}" ]] && showTestPassedMsg && PASSING_ASSERTIONS="${initial_passes}" && return
+    [[ "${initial_passes}" -lt "${PASSING_ASSERTIONS}" ]] && increasePassingTests && PASSING_ASSERTIONS="${initial_passes}" && return
     increaseFailingTests
 }
 
@@ -31,7 +36,7 @@ testAssertNoErrorIncreasesFAILING_ASSERTIONSOnFailingAssertion() {
     showRunningTestMsg "testAssertNoErrorIncreasesFAILING_ASSERTIONSOnFailingAssertion"
     assertNoError '${RANDOM}' "assertNoError MUST increase the number of FAILING_ASSERTIONS on failing assertion."
     notifyUser "${HIGHLIGHTCOLOR}Note: The previous call to assertNoError's results will not be tracked, it was just used to test that FAILING_ASSERTIONS are increased by assertNoError on a failing assertion." 0 'dontClear'
-    [[ "${initial_fails}" -lt "${FAILING_ASSERTIONS}" ]] && showTestPassedMsg && FAILING_ASSERTIONS="${initial_fails}" && return
+    [[ "${initial_fails}" -lt "${FAILING_ASSERTIONS}" ]] && increasePassingTests && FAILING_ASSERTIONS="${initial_fails}" && return
     increaseFailingTests
 }
 
