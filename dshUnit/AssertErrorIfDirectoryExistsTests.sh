3d013d0ca8e6794fc0645f0890ca17d7ac3d7a68
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-07 00:18:24 -0500

    dshUnit: Code clean up. Refactored tests, and dshUnitAssertions. All dshUnit tests are passing. This commit is related to issue #26.

--- dshUnit/AssertErrorIfDirectoryExistsTests.sh
+++ dshUnit/AssertErrorIfDirectoryExistsTests.sh
@@ -7,27 +7,25 @@ testAssertErrorIfDirectoryExistsDoesNotProduceAnErrorWhenFailingAssertionIsExpec
     local initial_fails
     initial_fails="${FAILING_ASSERTIONS}"
     showRunningTestMsg "testAssertErrorIfDirectoryExistsDoesNotProduceAnErrorWhenFailingAssertionIsExpected"
-    assertNoError "assertErrorIfDirectoryExists \"ls $(pwd)\" $(pwd) 'Test message'" "assertErrorIfDirectoryExists MUST run without error when failing assertion is expected."
+    assertNoError "assertErrorIfDirectoryExists \"ls $(determineDshUnitDirectoryPath)\" \"$(determineDshUnitDirectoryPath)\" 'Test message'" "assertErrorIfDirectoryExists MUST run without error when failing assertion is expected."
     [[ "${initial_fails}" == "${FAILING_ASSERTIONS}" ]] && increasePassingTests && return
     increaseFailingTests
 }
 
 testAssertErrorIfDirectoryExistsDoesNotProduceAnErrorWhenPassingAssertionIsExpected() {
-    local initial_fails random_directory_name
+    local initial_fails
     initial_fails="${FAILING_ASSERTIONS}"
     showRunningTestMsg "testAssertErrorIfDirectoryExistsDoesNotProduceAnErrorWhenPassingAssertionIsExpected"
-    random_directory_name="${RANDOM}${RANDOM}/${RANDOM}"
-    assertNoError "assertErrorIfDirectoryExists \"ls ${random_directory_name}\" ${random_directory_name} 'Test message'" "assertErrorIfDirectoryExists MUST run without error when passing assertion is expected."
+    assertNoError "assertErrorIfDirectoryExists \"mkdir $(determineDshUnitDirectoryPath)\" \"$(determineDshUnitDirectoryPath)\" 'Test message'" "assertErrorIfDirectoryExists MUST run without error when passing assertion is expected."
     [[ "${initial_fails}" == "${FAILING_ASSERTIONS}" ]] && increasePassingTests && return
     increasePassingTests
 }
 
 testAssertErrorIfDirectoryExistsIncreasesPASSING_ASSERTIONSOnPassingAssertion() {
-    local initial_passes random_directory_name
+    local initial_passes
     initial_passes="${PASSING_ASSERTIONS}"
-    random_directory_name="${RANDOM}${RANDOM}/${RANDOM}"
     showRunningTestMsg "testAssertErrorIfDirectoryExistsIncreasesPASSING_ASSERTIONSOnPassingAssertion"
-    assertErrorIfDirectoryExists "ls ${random_directory_name}" "${random_directory_name}" "assertErrorIfDirectoryExists MUST increase the number of PASSING_ASSERTIONS on passing assertion."
+    assertErrorIfDirectoryExists "mkdir $(determineDshUnitDirectoryPath)" "$(determineDshUnitDirectoryPath)" "assertErrorIfDirectoryExists MUST increase the number of PASSING_ASSERTIONS on passing assertion."
     [[ "${initial_passes}" -lt "${PASSING_ASSERTIONS}" ]] && increasePassingTests && return
     increaseFailingTests
 }
@@ -36,7 +34,7 @@ testAssertErrorIfDirectoryExistsIncreasesFAILING_ASSERTIONSOnFailingAssertion() 
     local initial_fails
     initial_fails="${FAILING_ASSERTIONS}"
     showRunningTestMsg "testAssertErrorIfDirectoryExistsIncreasesFAILING_ASSERTIONSOnFailingAssertion"
-    assertErrorIfDirectoryExists "ls $(pwd)" "$(pwd)" "assertErrorIfDirectoryExists MUST increase the number of FAILING_ASSERTIONS on failing assertion."
+    assertErrorIfDirectoryExists "ls $(determineDshUnitDirectoryPath)" "$(determineDshUnitDirectoryPath)" "assertErrorIfDirectoryExists MUST increase the number of FAILING_ASSERTIONS on failing assertion."
     [[ "${initial_fails}" -lt "${FAILING_ASSERTIONS}" ]] && increasePassingTests && return
     increaseFailingTests
 }
