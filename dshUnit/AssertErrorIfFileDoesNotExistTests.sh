cda17895b1520b79063cf91a983a6e03f2d9efb9
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-09 15:43:29 -0500

    dshUnit: Refactored remaining test files to use new dshUnit::runTest() function to run their tests. All dshUnit tests are passing. This commit is related to issue #26.

--- dshUnit/AssertErrorIfFileDoesNotExistTests.sh
+++ dshUnit/AssertErrorIfFileDoesNotExistTests.sh
@@ -4,48 +4,32 @@
 set -o posix
 
 testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenFailingAssertionIsExpected() {
-    local initial_faicat random_file_path
-    initial_fails="${FAILING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/${RANDOM}/Foo${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenFailingAssertionIsExpected"
     assertNoError "assertErrorIfFileDoesNotExist \"echo ${random_file_path}\" \"${random_file_path}\" 'Test message'" "assertErrorIfFileDoesNotExist MUST run without error when failing assertion is expected."
-    [[ "${initial_fails}" == "${FAILING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenFailingAssertionIsExpected" && return
-    increaseFailingTests"testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenFailingAssertionIsExpected"
 }
 
 testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenPassingAssertionIsExpected() {
-    local initial_faicat random_file_path
-    initial_fails="${FAILING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/${RANDOM}/Bar${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenPassingAssertionIsExpected"
     assertNoError "assertErrorIfFileDoesNotExist \"cat ${random_file_path}\" \"${random_file_path}\" 'Test message'" "assertErrorIfFileDoesNotExist MUST run without error when passing assertion is expected."
-    [[ "${initial_fails}" == "${FAILING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenPassingAssertionIsExpected" && return
-    increasePassingTests "testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenPassingAssertionIsExpected"
 }
 
 testAssertErrorIfFileDoesNotExistIncreasesPASSING_ASSERTIONSOnPassingAssertion() {
-    local initial_passes random_file_path
-    initial_passes="${PASSING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/Foo${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileDoesNotExistIncreasesPASSING_ASSERTIONSOnPassingAssertion"
     assertErrorIfFileDoesNotExist "cat ${random_file_path}" "${random_file_path}" "assertErrorIfFileDoesNotExist MUST increase the number of PASSING_ASSERTIONS on passing assertion."
-    [[ "${initial_passes}" -lt "${PASSING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileDoesNotExistIncreasesPASSING_ASSERTIONSOnPassingAssertion" && return
-    increaseFailingTests "testAssertErrorIfFileDoesNotExistIncreasesPASSING_ASSERTIONSOnPassingAssertion"
 }
 
 testAssertErrorIfFileDoesNotExistIncreasesFAILING_ASSERTIONSOnFailingAssertion() {
-    local initial_faicat random_file_path
-    initial_fails="${FAILING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/Bar${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileDoesNotExistIncreasesFAILING_ASSERTIONSOnFailingAssertion"
     assertErrorIfFileDoesNotExist "echo ${random_file_path}" "${random_file_path}" "assertErrorIfFileDoesNotExist MUST increase the number of FAILING_ASSERTIONS on failing assertion."
-    [[ "${initial_fails}" -lt "${FAILING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileDoesNotExistIncreasesFAILING_ASSERTIONSOnFailingAssertion" && return
-    increaseFailingTests "testAssertErrorIfFileDoesNotExistIncreasesFAILING_ASSERTIONSOnFailingAssertion"
 }
 
-testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenPassingAssertionIsExpected
-testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenFailingAssertionIsExpected
-testAssertErrorIfFileDoesNotExistIncreasesPASSING_ASSERTIONSOnPassingAssertion
-testAssertErrorIfFileDoesNotExistIncreasesFAILING_ASSERTIONSOnFailingAssertion
+runTest testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenPassingAssertionIsExpected
+runTest testAssertErrorIfFileDoesNotExistDoesNotProduceAnErrorWhenFailingAssertionIsExpected
+runTest testAssertErrorIfFileDoesNotExistIncreasesPASSING_ASSERTIONSOnPassingAssertion
+runTest testAssertErrorIfFileDoesNotExistIncreasesFAILING_ASSERTIONSOnFailingAssertion "1" "allAssertionsFail"
 
 
--- dshUnit/AssertErrorIfFileIsNotExecutable.sh
+++ dshUnit/AssertErrorIfFileIsNotExecutable.sh
@@ -4,48 +4,32 @@
 set -o posix
 
 testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenFailingAssertionIsExpected() {
-    local initial_faicat random_file_path
-    initial_fails="${FAILING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/${RANDOM}/Foo${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenFailingAssertionIsExpected"
     assertNoError "assertErrorIfFileIsNotExecutable \"echo ${random_file_path}\" \"${random_file_path}\" 'Test message'" "assertErrorIfFileIsNotExecutable MUST run without error when failing assertion is expected."
-    [[ "${initial_fails}" == "${FAILING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenFailingAssertionIsExpected" && return
-    increaseFailingTests "testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenFailingAssertionIsExpected"
 }
 
 testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected() {
-    local initial_faicat random_file_path
-    initial_fails="${FAILING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/${RANDOM}/Bar${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected"
     assertNoError "assertErrorIfFileIsNotExecutable \"cat ${random_file_path}\" \"${random_file_path}\" 'Test message'" "assertErrorIfFileIsNotExecutable MUST run without error when passing assertion is expected."
-    [[ "${initial_fails}" == "${FAILING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected" && return
-    increasePassingTests "testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected"
 }
 
 testAssertErrorIfFileIsNotExecutableIncreasesPASSING_ASSERTIONSOnPassingAssertion() {
-    local initial_passes random_file_path
-    initial_passes="${PASSING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/Foo${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileIsNotExecutableIncreasesPASSING_ASSERTIONSOnPassingAssertion"
     assertErrorIfFileIsNotExecutable "cat ${random_file_path}" "${random_file_path}" "assertErrorIfFileIsNotExecutable MUST increase the number of PASSING_ASSERTIONS on passing assertion."
-    [[ "${initial_passes}" -lt "${PASSING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileIsNotExecutableIncreasesPASSING_ASSERTIONSOnPassingAssertion" && return
-    increaseFailingTests "testAssertErrorIfFileIsNotExecutableIncreasesPASSING_ASSERTIONSOnPassingAssertion"
 }
 
 testAssertErrorIfFileIsNotExecutableIncreasesFAILING_ASSERTIONSOnFailingAssertion() {
-    local initial_faicat random_file_path
-    initial_fails="${FAILING_ASSERTIONS}"
+    local random_file_path
     random_file_path="${RANDOM}/Bar${RANDOM}"
-    showRunningTestMsg "testAssertErrorIfFileIsNotExecutableIncreasesFAILING_ASSERTIONSOnFailingAssertion"
     assertErrorIfFileIsNotExecutable "echo ${random_file_path}" "${random_file_path}" "assertErrorIfFileIsNotExecutable MUST increase the number of FAILING_ASSERTIONS on failing assertion."
-    [[ "${initial_fails}" -lt "${FAILING_ASSERTIONS}" ]] && increasePassingTests "testAssertErrorIfFileIsNotExecutableIncreasesFAILING_ASSERTIONSOnFailingAssertion" && return
-    increaseFailingTests "testAssertErrorIfFileIsNotExecutableIncreasesFAILING_ASSERTIONSOnFailingAssertion"
 }
 
-testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected
-testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenFailingAssertionIsExpected
-testAssertErrorIfFileIsNotExecutableIncreasesPASSING_ASSERTIONSOnPassingAssertion
-testAssertErrorIfFileIsNotExecutableIncreasesFAILING_ASSERTIONSOnFailingAssertion
+runTest testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected
+runTest testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenFailingAssertionIsExpected
+runTest testAssertErrorIfFileIsNotExecutableIncreasesPASSING_ASSERTIONSOnPassingAssertion
+runTest testAssertErrorIfFileIsNotExecutableIncreasesFAILING_ASSERTIONSOnFailingAssertion "1" "allAssertionsFail"
 
 
