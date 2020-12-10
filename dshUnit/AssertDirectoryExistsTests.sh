6b76695ffe2ff6edc803721aa434342a5aa63ab4
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-10 12:55:50 -0500

    dshUnit: Code clean up. All dshUnit tests are passing. This commit is related to issue #26.

--- dshUnit/AssertDirectoryExistsTests.sh
+++ dshUnit/AssertDirectoryExistsTests.sh
@@ -3,23 +3,23 @@
 
 set -o posix
 
+RANDOM_DIRECTORY_PATH="${RANDOM}/BarBazFoo${RANDOM}"
+EXISTING_DIRECTORY_PATH="$(determineDshUnitDirectoryPath)"
+
 testAssertDirectoryExistsDoesNotProduceAnErrorWhenFailingAssertionIsExpected() {
-    random_directory_path="${RANDOM}/${RANDOM}/FooBar${RANDOM}"
-    assertNoError "assertDirectoryExists \"ls ${random_directory_path}\" \"${random_directory_path}\" 'Test message'" "assertDirectoryExists MUST run without error when failing assertion is expected."
+    assertNoError "assertDirectoryExists \"ls ${RANDOM_DIRECTORY_PATH}\" \"${RANDOM_DIRECTORY_PATH}\" 'Test message'" "assertDirectoryExists MUST run without error when failing assertion is expected."
 }
 
 testAssertDirectoryExistsDoesNotProduceAnErrorWhenPassingAssertionIsExpected() {
-    assertNoError "assertDirectoryExists \"ls $(determineDshUnitDirectoryPath)\" \"$(determineDshUnitDirectoryPath)\" 'Test message'" "assertDirectoryExists MUST run without error when passing assertion is expected."
+    assertNoError "assertDirectoryExists \"ls ${EXISTING_DIRECTORY_PATH}\" \"${EXISTING_DIRECTORY_PATH}\" 'Test message'" "assertDirectoryExists MUST run without error when passing assertion is expected."
 }
 
 testAssertDirectoryExistsIncreasesPASSING_ASSERTIONSOnPassingAssertion() {
-    assertDirectoryExists "ls $(determineDshUnitDirectoryPath)" "$(determineDshUnitDirectoryPath)" "assertDirectoryExists MUST increase the number of PASSING_ASSERTIONS on passing assertion."
+    assertDirectoryExists "ls ${EXISTING_DIRECTORY_PATH}" "${EXISTING_DIRECTORY_PATH}" "assertDirectoryExists MUST increase the number of PASSING_ASSERTIONS on passing assertion."
 }
 
 testAssertDirectoryExistsIncreasesFAILING_ASSERTIONSOnFailingAssertion() {
-    local random_directory_path
-    random_directory_path="${RANDOM}/FooBar${RANDOM}"
-    assertDirectoryExists "ls ${random_directory_path}" "${random_directory_path}" "assertDirectoryExists MUST increase the number of FAILING_ASSERTIONS on failing assertion."
+    assertDirectoryExists "ls ${RANDOM_DIRECTORY_PATH}" "${RANDOM_DIRECTORY_PATH}" "assertDirectoryExists MUST increase the number of FAILING_ASSERTIONS on failing assertion."
 }
 
 runTest testAssertDirectoryExistsDoesNotProduceAnErrorWhenFailingAssertionIsExpected
--- dshUnit/AssertErrorIfFileIsNotExecutable.sh
+++ dshUnit/AssertErrorIfFileIsNotExecutable.sh
@@ -3,28 +3,22 @@
 
 set -o posix
 
+RANDOM_FILE_PATH="${RANDOM}/Bar${RANDOM}"
+
 testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenFailingAssertionIsExpected() {
-    local random_file_path
-    random_file_path="${RANDOM}/${RANDOM}/Foo${RANDOM}"
-    assertNoError "assertErrorIfFileIsNotExecutable \"echo ${random_file_path}\" \"${random_file_path}\" 'Test message'" "assertErrorIfFileIsNotExecutable MUST run without error when failing assertion is expected."
+    assertNoError "assertErrorIfFileIsNotExecutable \"echo ${RANDOM_FILE_PATH}\" \"${RANDOM_FILE_PATH}\" 'Test message'" "assertErrorIfFileIsNotExecutable MUST run without error when failing assertion is expected."
 }
 
 testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected() {
-    local random_file_path
-    random_file_path="${RANDOM}/${RANDOM}/Bar${RANDOM}"
-    assertNoError "assertErrorIfFileIsNotExecutable \"cat ${random_file_path}\" \"${random_file_path}\" 'Test message'" "assertErrorIfFileIsNotExecutable MUST run without error when passing assertion is expected."
+    assertNoError "assertErrorIfFileIsNotExecutable \"cat ${RANDOM_FILE_PATH}\" \"${RANDOM_FILE_PATH}\" 'Test message'" "assertErrorIfFileIsNotExecutable MUST run without error when passing assertion is expected."
 }
 
 testAssertErrorIfFileIsNotExecutableIncreasesPASSING_ASSERTIONSOnPassingAssertion() {
-    local random_file_path
-    random_file_path="${RANDOM}/Foo${RANDOM}"
-    assertErrorIfFileIsNotExecutable "cat ${random_file_path}" "${random_file_path}" "assertErrorIfFileIsNotExecutable MUST increase the number of PASSING_ASSERTIONS on passing assertion."
+    assertErrorIfFileIsNotExecutable "cat ${RANDOM_FILE_PATH}" "${RANDOM_FILE_PATH}" "assertErrorIfFileIsNotExecutable MUST increase the number of PASSING_ASSERTIONS on passing assertion."
 }
 
 testAssertErrorIfFileIsNotExecutableIncreasesFAILING_ASSERTIONSOnFailingAssertion() {
-    local random_file_path
-    random_file_path="${RANDOM}/Bar${RANDOM}"
-    assertErrorIfFileIsNotExecutable "echo ${random_file_path}" "${random_file_path}" "assertErrorIfFileIsNotExecutable MUST increase the number of FAILING_ASSERTIONS on failing assertion."
+    assertErrorIfFileIsNotExecutable "echo ${RANDOM_FILE_PATH}" "${RANDOM_FILE_PATH}" "assertErrorIfFileIsNotExecutable MUST increase the number of FAILING_ASSERTIONS on failing assertion."
 }
 
 runTest testAssertErrorIfFileIsNotExecutableDoesNotProduceAnErrorWhenPassingAssertionIsExpected
