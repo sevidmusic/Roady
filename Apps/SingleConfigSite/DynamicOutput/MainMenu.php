b3d3689b49de3b9a9b217d21981e299cb61123f7
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-12 18:13:20 -0500

    APPS: Added singleConfig app, which demonstrates an app that generates an entire website using a single config file to define all of it's components. All tests are passing. App was built and run successfully.

---
+++ Apps/SingleConfigSite/DynamicOutput/MainMenu.php
@@ -0,0 +1,9 @@
+<div class="main-menu">
+    <ul class="main-menu-links">
+        <li><a href="./index.php">Home</a></li>
+        <li><a href="./index.php?outputComponentOverview">Output</a></li>
+        <li><a href="./index.php?responseOverview">Responses</a></li>
+        <li><a href="https://github.com/sevidmusic/DarlingDataManagementSystem">GitHub</a></li>
+        <li><a href="https://www.youtube.com/channel/UCfR-CPqfSIvCFnYT1m4s66w">YouTube</a></li>
+    </ul>
+</div>
---
+++ Apps/SingleConfigSite/DynamicOutput/Welcome.php
@@ -0,0 +1,36 @@
+<div class="output font-concert-one">
+    <p>
+        Welcome to the Darling Data Management System,
+        a tool designed to aide in the development of
+        well organized PHP applications.
+    </p>
+    <p>
+        You are viewing the SingleConfigSite App which is
+        intended to serve as a demo, and can also be
+        used as a base to develop a new App.
+    </p>
+    <p>
+        To use the SingleConfigSite app as the base for a
+        new app, make a copy of Apps/SingleConfigSite with an
+        appropriate name, and place it in the Apps
+        directory, i.e., Apps/NewAppName.
+    </p>
+    <p>
+        You can then modify this new App's source code
+        to suit your needs.
+    </p>
+    <p>
+        The benefit of using the SingleConfigSite as a base is you start
+        out with a working App with some defaults that can be used
+        as they are, or serve as a guide to help you figure out
+        how to build your new App.
+    </p>
+    <p>
+        To learn about how to develop with the Darling Data Management System,
+        take a look at the SingleConfigSite's source code, or use dsh -r Documentation
+        to start up the Documentation app which provides detailed information
+        on how the Darling Data Management System works, and how you can use
+        it to accomplish your goals.
+    </p>
+</div>
+
