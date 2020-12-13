ab0fa6b892591b1062b1a84ea9c2080c580bcd7c
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-13 16:10:28 -0500

    APPS: Clened up starterApp, removed need for Component config loading files Output.php, Requests.php, and Responses.php. All tests are passing.

--- Apps/starterApp/Requests.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require __DIR__ . DIRECTORY_SEPARATOR . 'Requests/HomepageRequest.php';
-require __DIR__ . DIRECTORY_SEPARATOR . 'Requests/OutputComponentOverview.php';
-require __DIR__ . DIRECTORY_SEPARATOR . 'Requests/ResponseOverviewRequest.php';
-require __DIR__ . DIRECTORY_SEPARATOR . 'Requests/RootRequest.php';
