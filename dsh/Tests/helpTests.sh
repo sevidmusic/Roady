0c6c3c90cf280f161af9eed7abbe14153321672e
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-16 19:21:12 -0500

    DSH: Implemented `helpTests.sh::testDshHelpCreatesSystemManPageForDsh()`
    All dsh tests are passing. This commit is related to issue #27.
    
    Also: Ran `man dsh` after runnig dsh tests, the man page displayed as
    expected. `dsh --help` is now ready to be refactored to show a proper man
    page, and also create the system man page for dsh so man dsh can be used
    as well as `dsh --help`.

--- dsh/Tests/helpTests.sh
+++ dsh/Tests/helpTests.sh
@@ -7,4 +7,11 @@ testDshHelpRunsWithoutError() {
     assertNoError 'dsh --help' 'dsh --help MUST run without error.'
 }
 
+testDshHelpCreatesSystemManPageForDsh() {
+    local dsh_man_file_path
+    dsh_man_file_path="${HOME}/.local/share/man/man1/dsh.1"
+    assertFileExists "dsh --help" "${dsh_man_file_path}" "dsh --help MUST create a man page for dsh at ${dsh_man_file_path}. This may entail creating the parent directories."
+}
+
 runTest testDshHelpRunsWithoutError
+runTest testDshHelpCreatesSystemManPageForDsh
