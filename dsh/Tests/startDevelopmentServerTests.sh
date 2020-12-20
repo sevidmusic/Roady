7010b067c6949db263a0efef83f65360f7bf8c1e
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-20 18:04:40 -0500

    DSH: Continued development of dsh. Continued implementing:
        dsh --start-development-server <PORT>
    
    Implemented:
    
        testDshStartDevelopmentServerStartsDevelopmentServer()
    
    Ran dshUnit -c dsh/dshTestsConfig.sh -t TestDshStartDevelopmentServer
    
    Tests passed.
    
    This commit is related to issue #27.

--- dsh/Tests/startDevelopmentServerTests.sh
+++ dsh/Tests/startDevelopmentServerTests.sh
@@ -3,8 +3,25 @@
 
 set -o posix
 
+activeServers() {
+    printf "%s" "$(ps -aux | grep -Eo 'php -S localhost:[0-9][0-9][0-9][0-9]' | sed 's,php -S localhost,http://localhost,g')"
+}
+
+numberOfActiveServers() {
+    printf "%s" "$(activeServers | wc -w)"
+}
+
 testDshStartDevelopmentServerRunsWithoutError() {
     assertNoError "dsh --start-development-server" "dsh --start-development-server MUST run without error."
 }
 
+testDshStartDevelopmentServerStartsDevelopmentServer() {
+    killall php &> /dev/null
+    dsh --start-development-server
+    assertEquals "1" "$(numberOfActiveServers)" "dsh --start-development-server MUST start a development server."
+    killall php &> /dev/null
+}
+
 runTest testDshStartDevelopmentServerRunsWithoutError
+runTest testDshStartDevelopmentServerStartsDevelopmentServer
+
