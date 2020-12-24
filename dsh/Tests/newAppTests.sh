f24762c32e0d26f8c68575ce38b3e3a228c4cb70
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-23 23:49:18 -0500

    DSH: Continued development of dsh --new App. Implemented testDshNewAppCreatesNewAppsDirectory() in dsh/Tests/newAppTests.sh. All dsh --new App tests are passing. Also manually tested dsh --new App Bar, manual test passed. This commit is related to issue #27.

--- dsh/Tests/newAppTests.sh
+++ dsh/Tests/newAppTests.sh
@@ -3,8 +3,20 @@
 
 set -o posix
 
-testNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified() {
+determineAppsDirectoryPath() {
+    printf "%s" "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${1}"
+}
+
+testDshNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified() {
     assertError "dsh --new App" "dsh --new App <APP_NAME> <DOMAIN> MUST run with an error if the name to assign to the new App is not specified via the first parameter."
 }
 
-runTest testNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified
+testDshNewAppCreatesNewAppsDirectory() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new app at $(determineAppsDirectoryPath '<APP_NAME>')."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+runTest testDshNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified
+runTest testDshNewAppCreatesNewAppsDirectory
