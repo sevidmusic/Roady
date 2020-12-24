aa252427bea9f50b487bf8d6fd5c38b5c465d034
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-24 18:29:09 -0500

    DSH: Continued development of dsh --new App <APP_NAME> <DOMAIN>. Implemented testDshNewAppRunsWithErrorIfAnAppAlreadyExistsNamedAPP_NAME(), testDshNewAppCreatesNewAppsComponentsPhpFile(), and testDshNewAppCreatesNewAppsComponentsPhpFileWhoseContentMatchesComponentsPhpFileTemplate() in dsh/Tess/newAppTests.sh. All dsh --new App <APP_NAME> <DOMAIN> tests are passing. This commit is related to issue #27.

--- dsh/Tests/newAppTests.sh
+++ dsh/Tests/newAppTests.sh
@@ -11,6 +11,10 @@ testDshNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified() {
     assertError "dsh --new App" "dsh --new App <APP_NAME> <DOMAIN> MUST run with an error if the name to assign to the new App is not specified via the first parameter."
 }
 
+testDshNewAppRunsWithErrorIfAnAppAlreadyExistsNamedAPP_NAME() {
+    assertError "dsh --new App starterApp" "dsh --new App <APP_NAME> <DOMAIN> MUST run with an error if the name to assign to the new App is not specified via the first parameter."
+}
+
 testDshNewAppCreatesNewAppsDirectory() {
     local random_app_name
     random_app_name="Foo${RANDOM}"
@@ -60,7 +64,25 @@ testDshNewAppCreatesNewAppsJsDirectory() {
     [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
 }
 
+testDshNewAppCreatesNewAppsComponentsPhpFile() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertFileExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")/Components.php" "dsh --new App <APP_NAME> <DOMAIN> MUST create a Components.php file for the new App at $(determineAppsDirectoryPath '<APP_NAME>')/Components.php."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+testDshNewAppCreatesNewAppsComponentsPhpFileWhoseContentMatchesComponentsPhpFileTemplate() {
+    local random_app_name components_php_file_template_path created_components_php_file_path
+    random_app_name="Foo${RANDOM}"
+    components_php_file_template_path="$(determineDshUnitDirectoryPath | sed 's/dshUnit/dsh/g')/FileTemplates/Components.php"
+    created_components_php_file_path="$(determineAppsDirectoryPath "${random_app_name}")/Components.php"
+    assertNoError "dsh --new App ${random_app_name}" "Running dsh --new App ${random_app_name} to test that dsh --new App <APP_NAME> <DOMAIN> creates a Components.php file at Apps/<APP_NAME>/Components.php whose content matches the content of the Components.php file template at dsh/FileTemplates/Components.php"
+    assertEquals "$(cat ${components_php_file_template_path})" "$(cat ${created_components_php_file_path})" "dsh --new <APP_NAME> <DOMAIN> MUST create a Components.php file for the new App whose content matches the Components.php template file at ${components_php_file_template_path}."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
 runTest testDshNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified
+runTest testDshNewAppRunsWithErrorIfAnAppAlreadyExistsNamedAPP_NAME
 runTest testDshNewAppCreatesNewAppsDirectory
 runTest testDshNewAppCreatesNewAppsOutputComponentsDirectory
 runTest testDshNewAppCreatesNewAppsRequestsDirectory
@@ -68,13 +90,7 @@ runTest testDshNewAppCreatesNewAppsResponsesDirectory
 runTest testDshNewAppCreatesNewAppsDynamicOutputDirectory
 runTest testDshNewAppCreatesNewAppsCssDirectory
 runTest testDshNewAppCreatesNewAppsJsDirectory
+runTest testDshNewAppCreatesNewAppsComponentsPhpFile
+runTest testDshNewAppCreatesNewAppsComponentsPhpFileWhoseContentMatchesComponentsPhpFileTemplate 2
 
-
-# @todo
-# - Apps/<APP_NAME>/OutputComponents/
-# - Apps/<APP_NAME>/Requests/
-# - Apps/<APP_NAME>/Responses/
-# - Apps/<APP_NAME>DynamicOutput/
-# - Apps/<APP_NAME>/css/
-# - Apps/<APP_NAME>/js
 
