f9eb2145cc3abd651704988390933e4c115a2c64
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-24 00:50:48 -0500

    DSH: Continued development of dsh --new App <APP_NAME> <DOMAIN>. Implemented the following tests in dsh/Tests/newAppTests.sh:
    testDshNewAppCreatesNewAppsOutputComponentsDirectory()
    testDshNewAppCreatesNewAppsRequestsDirectory()
    testDshNewAppCreatesNewAppsResponsesDirectory()
    testDshNewAppCreatesNewAppsDynamicOutputDirectory()
    testDshNewAppCreatesNewAppsCssDirectory()
    testDshNewAppCreatesNewAppsJsDirectory()
    
    All dsh --new App tests are passing.
    
    This commit is related to issue #27.

--- dsh/Tests/newAppTests.sh
+++ dsh/Tests/newAppTests.sh
@@ -14,9 +14,67 @@ testDshNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified() {
 testDshNewAppCreatesNewAppsDirectory() {
     local random_app_name
     random_app_name="Foo${RANDOM}"
-    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new app at $(determineAppsDirectoryPath '<APP_NAME>')."
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new App at $(determineAppsDirectoryPath '<APP_NAME>')."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+testDshNewAppCreatesNewAppsOutputComponentsDirectory() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")/OutputComponents" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new App's OutputComponents at $(determineAppsDirectoryPath '<APP_NAME>')/OutputComponents."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+testDshNewAppCreatesNewAppsRequestsDirectory() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")/Requests" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new App's Requests at $(determineAppsDirectoryPath '<APP_NAME>')/Requests."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+testDshNewAppCreatesNewAppsResponsesDirectory() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")/Responses" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new App's Responses at $(determineAppsDirectoryPath '<APP_NAME>')/Responses."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+testDshNewAppCreatesNewAppsDynamicOutputDirectory() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")/DynamicOutput" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new App's DynamicOutput at $(determineAppsDirectoryPath '<APP_NAME>')/DynamicOutput."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+testDshNewAppCreatesNewAppsCssDirectory() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")/css" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new App's css at $(determineAppsDirectoryPath '<APP_NAME>')/css."
+    [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
+}
+
+testDshNewAppCreatesNewAppsJsDirectory() {
+    local random_app_name
+    random_app_name="Foo${RANDOM}"
+    assertDirectoryExists "dsh --new App "${random_app_name}"" "$(determineAppsDirectoryPath "${random_app_name}")/js" "dsh --new App <APP_NAME> <DOMAIN> MUST create a directory for the new App's js at $(determineAppsDirectoryPath '<APP_NAME>')/js."
     [[ -d "$(determineAppsDirectoryPath "${random_app_name}")" ]] && rm -R "$(determineAppsDirectoryPath "${random_app_name}")"
 }
 
 runTest testDshNewAppRunsWithErrorIfAPP_NAMEIsNotSpecified
 runTest testDshNewAppCreatesNewAppsDirectory
+runTest testDshNewAppCreatesNewAppsOutputComponentsDirectory
+runTest testDshNewAppCreatesNewAppsRequestsDirectory
+runTest testDshNewAppCreatesNewAppsResponsesDirectory
+runTest testDshNewAppCreatesNewAppsDynamicOutputDirectory
+runTest testDshNewAppCreatesNewAppsCssDirectory
+runTest testDshNewAppCreatesNewAppsJsDirectory
+
+
+# @todo
+# - Apps/<APP_NAME>/OutputComponents/
+# - Apps/<APP_NAME>/Requests/
+# - Apps/<APP_NAME>/Responses/
+# - Apps/<APP_NAME>DynamicOutput/
+# - Apps/<APP_NAME>/css/
+# - Apps/<APP_NAME>/js
+
