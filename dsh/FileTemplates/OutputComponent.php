70619ed8856a8bec391a7a624e36bf9f071f888f
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-25 18:13:35 -0500

    DSH: Continued development of dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT>. Implemented testNewOutputComponentDefinesSpecifiedOutputComponentForSpecifiedApp() in test file at dsh/Tests/newOutputComponentTests.sh. All dsh --new OutputComponent tests are passing. Also, defined template file for OutputComponent configuration files at dsh/FileTemplates/OutputComponent.php. Code clean up. This commit is related to issue #27.

---
+++ dsh/FileTemplates/OutputComponent.php
@@ -0,0 +1,10 @@
+<?php
+
+/** APP_NAME | OUTPUT_COMPONENT_NAME.php */
+
+ $appComponentsFactory->buildOutputComponent(
+    'OUTPUT_COMPONENT_NAME',
+    'OUTPUT_COMPONENT_CONTAINER',
+    'OUTPUT',
+    OUTPUT_COMPONENT_POSITION
+);
