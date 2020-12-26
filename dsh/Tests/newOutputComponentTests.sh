de79f9243dd5bc68e0a09e77af5e27a714e88fc3
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-25 22:21:19 -0500

    DSH: Ran all dsh --new OutputComponent tests. Refactored dsh/Tests/newOutputComponentTests.sh to no longer use the starterApp in it's tests, instead a test app is built for the tests and removed once tests are complete. Code clean up. All dsh --new OutputComponent tests are passing. This commit is related to issue #27.

--- dsh/Tests/newOutputComponentTests.sh
+++ dsh/Tests/newOutputComponentTests.sh
@@ -3,6 +3,10 @@
 
 set -o posix
 
+test_app_name="AppName${RANDOM}"
+showLoadingBar "Creating test App ${test_app_name} for use by tests defined in newOutputComponents.sh" 'dontClear'
+dsh -n App "${test_app_name}" &> /dev/null
+
 testNewOutputComponentRunsWithErrorIfAPP_NAMEIsNotSpecified() {
     assertError "dsh --new OutputComponent" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
 }
@@ -12,29 +16,31 @@ testNewOutputComponentRunsWithErrorIfSpecifiedAppDoesNotExist() {
 }
 
 testNewOutputComponentRunsWithErrorIfAnOutputComponentNamedOUTPUT_COMPONENT_NAMEAlreadyExists() {
-    assertError "dsh --new OutputComponent starterApp OCName OCContainer 4 Output" "--new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if an OutputComponent named <OUTPUT_COMPONENT> is already defined for the secified App."
+    showLoadingBar "Creating test OutputComponent OCName for test App ${test_app_name} to be used by testNewOutputComponentRunsWithErrorIfAnOutputComponentNamedOUTPUT_COMPONENT_NAMEAlreadyExists()" 'dontClear'
+    dsh --new OutputComponent ${test_app_name} OCName OCContainer 4 Output &> /dev/null
+    assertError "dsh --new OutputComponent ${test_app_name} OCName OCContainer 4 Output" "--new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if an OutputComponent named <OUTPUT_COMPONENT> is already defined for the secified App."
 }
 
 testNewOutputComponentRunsWithErrorIfOUTPUT_COMPONENT_NAMEIsNotSpecified() {
-    assertError "dsh --new OutputComponent starterApp" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
+    assertError "dsh --new OutputComponent ${test_app_name}" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
 }
 
 testNewOutputComponentRunsWithErrorIfOUTPUT_COMPONENT_CONTAINERIsNotSpecified() {
-    assertError "dsh --new OutputComponent starterApp AppName" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
+    assertError "dsh --new OutputComponent ${test_app_name} AppName" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
 }
 
 testNewOutputComponentRunsWithErrorIfOUTPUT_COMPONENT_POSITIONIsNotSpecified() {
-    assertError "dsh --new OutputComponent starterApp AppName OCName" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
+    assertError "dsh --new OutputComponent ${test_app_name} AppName OCName" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
 }
 
 testNewOutputComponentRunsWithErrorIfOUTPUTIsNotSpecified() {
-    assertError "dsh --new OutputComponent starterApp AppName OCName 2" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
+    assertError "dsh --new OutputComponent ${test_app_name} AppName OCName 2" "dsh --new OutputComponent <APP_NAME> <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST run with an error if <APP_NAME> is not specified."
 }
 
 testNewOutputComponentDefinesSpecifiedOutputComponentForSpecifiedApp() {
     local output_component_name
     output_component_name="OCName${RANDOM}"
-    assertFileExists "dsh --new OutputComponent starterApp ${output_component_name} OCContainer 4 Output" "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/starterApp/OutputComponents/${output_component_name}.php" "dsh --new OutputComponent <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST create a OutputComponent configuration file at Apps/<APP_NAME>/OutputComponents/<OUTPUT_COMPONENT_NAME>.php"
+    assertFileExists "dsh --new OutputComponent ${test_app_name} ${output_component_name} OCContainer 4 Output" "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${test_app_name}/OutputComponents/${output_component_name}.php" "dsh --new OutputComponent <OUTPUT_COMPONENT_NAME> <OUTPUT_COMPONENT_CONTAINER> <OUTPUT_COMPONENT_POSITION> <OUTPUT> MUST create a OutputComponent configuration file at Apps/<APP_NAME>/OutputComponents/<OUTPUT_COMPONENT_NAME>.php"
 }
 
 runTest testNewOutputComponentRunsWithErrorIfAPP_NAMEIsNotSpecified
@@ -45,3 +51,5 @@ runTest testNewOutputComponentRunsWithErrorIfOUTPUT_COMPONENT_CONTAINERIsNotSpec
 runTest testNewOutputComponentRunsWithErrorIfOUTPUT_COMPONENT_POSITIONIsNotSpecified
 runTest testNewOutputComponentRunsWithErrorIfOUTPUTIsNotSpecified
 runTest testNewOutputComponentDefinesSpecifiedOutputComponentForSpecifiedApp
+
+[[ -d "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${test_app_name}" ]] && rm -R "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${test_app_name}"
