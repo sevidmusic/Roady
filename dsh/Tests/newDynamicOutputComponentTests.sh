70fe78bf2f9d3b98b44541ce671ca39b239aa84b
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-26 14:45:14 -0500

    DSH: Continued development of dsh --new DynamicOutputComponent <APP_NAME> <DYNAMIC_OUTPUT_COMPONENT_NAME> <DYNAMIC_OUTPUT_COMPONENT_CONTAINER> <DYNAMIC_OUTPUT_COMPONENT_POSITION> <DYNAMIC_OUTPUT_FILE>
    Refactored dsh/Tests/newDynamicOutputComponentTests.sh
    
    - Renamed testNewDynamicOutputComponentDefinesSpecifiedDynamicOutputComponentForSpecifiedApp() to testNewDynamicOutputComponentCreatesNewOutputComponentConfigurationFileForSpecifiedApp()
    
    - Implemented testNewDynamicOutputComponentCreatesNewOutputComponentConfigurationFileForSpecifiedAppWhoseContentMatchesExpectedContent()
    
    All dsh --new DynamicOutputComponent tests are passing.
    
    This commit is related to issue #27.

--- dsh/Tests/newDynamicOutputComponentTests.sh
+++ dsh/Tests/newDynamicOutputComponentTests.sh
@@ -7,6 +7,24 @@ test_app_name="AppName${RANDOM}"
 showLoadingBar "Creating test App ${test_app_name} for use by tests defined in newOutputComponents.sh" 'dontClear'
 dsh -n App "${test_app_name}" &> /dev/null
 
+expectedDynamicOutputComponentFileContent() {
+    local expectedDOCTemplateFilePath
+    expectedDOCTemplateFilePath="$(determineDshUnitDirectoryPath | sed 's/dshUnit/dsh/g')/FileTemplates/DynamicOutputComponent.php"
+    expectedContent="$(cat "${expectedDOCTemplateFilePath}")"
+    [[ -z "${1}" ]] && logErrorMsgAndExit1 "Please specify the name of the App the new test DynamicOutputComponent will be defined for."
+    [[ -z "${2}" ]] && logErrorMsgAndExit1 "Please specify a name to assign to the new test DynamicOutputComponent."
+    [[ -z "${3}" ]] && logErrorMsgAndExit1 "Please specify a container to assign to the new test DynamicOutputComponent."
+    [[ -z "${4}" ]] && logErrorMsgAndExit1 "Please specify a position to assign to the new test DynamicOutputComponent."
+    [[ -z "${5}" ]] && logErrorMsgAndExit1 "Please specify the output to assignt to the new test DynamicOutputComponent."
+    [[ ! -f "${expectedDOCTemplateFilePath}" ]] && logErrorMsgAndExit1 "The DynamicOutputComponent.php file template does not exist at $(determineDshDirectoryPath)/FileTemplates/DynamicOutputComponent.php!"
+    expectedContent="$(echo "${expectedContent}" | sed "s/APP_NAME/${1}/g")"
+    expectedContent="$(echo "${expectedContent}" | sed "s/DYNAMIC_OUTPUT_COMPONENT_NAME/${2}/g")"
+    expectedContent="$(echo "${expectedContent}" | sed "s/DYNAMIC_OUTPUT_COMPONENT_CONTAINER/${3}/g")"
+    expectedContent="$(echo "${expectedContent}" | sed "s/DYNAMIC_OUTPUT_COMPONENT_POSITION/${4}/g")"
+    expectedContent="$(echo "${expectedContent}" | sed "s/DYNAMIC_OUTPUT_FILE/${5}/g")"
+    printf "%s" "${expectedContent}"
+}
+
 testNewDynamicOutputComponentRunsWithErrorIfAPP_NAMEIsNotSpecified() {
     assertError "dsh --new DynamicOutputComponent" "dsh --new DynamicOutputComponent <APP_NAME> <DYNAMIC_OUTPUT_COMPONENT_NAME> <DYNAMIC_OUTPUT_COMPONENT_CONTAINER> <DYNAMIC_OUTPUT_COMPONENT_POSITION> <DYNAMIC_OUTPUT_FILE> MUST run with an error if <APP_NAME> is not specified."
 }
@@ -37,12 +55,18 @@ testNewDynamicOutputComponentRunsWithErrorIfOUTPUTIsNotSpecified() {
     assertError "dsh --new DynamicOutputComponent ${test_app_name} DOCName DOCContainer 2" "dsh --new DynamicOutputComponent <APP_NAME> <DYNAMIC_OUTPUT_COMPONENT_NAME> <DYNAMIC_OUTPUT_COMPONENT_CONTAINER> <DYNAMIC_OUTPUT_COMPONENT_POSITION> <DYNAMIC_OUTPUT_FILE> MUST run with an error if <APP_NAME> is not specified."
 }
 
-testNewDynamicOutputComponentDefinesSpecifiedDynamicOutputComponentForSpecifiedApp() {
+testNewDynamicOutputComponentCreatesNewOutputComponentConfigurationFileForSpecifiedApp() {
     local output_component_name
     output_component_name="DOCName${RANDOM}"
     assertFileExists "dsh --new DynamicOutputComponent ${test_app_name} ${output_component_name} DOCContainer 4 DynamicOutputFileName.txt" "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${test_app_name}/OutputComponents/${output_component_name}.php" "dsh --new DynamicOutputComponent <DYNAMIC_OUTPUT_COMPONENT_NAME> <DYNAMIC_OUTPUT_COMPONENT_CONTAINER> <DYNAMIC_OUTPUT_COMPONENT_POSITION> <DYNAMIC_OUTPUT_FILE> MUST create a DynamicOutputComponent configuration file at Apps/<APP_NAME>/OutputComponents/<DYNAMIC_OUTPUT_COMPONENT_NAME>.php"
 }
 
+testNewDynamicOutputComponentCreatesNewOutputComponentConfigurationFileForSpecifiedAppWhoseContentMatchesExpectedContent() {
+    local output_component_name
+    output_component_name="DOCName${RANDOM}"
+    dsh --new DynamicOutputComponent ${test_app_name} ${output_component_name} DOCContainer 4 DynamicOutputFileName.txt
+    assertEquals "$(expectedDynamicOutputComponentFileContent ${test_app_name} ${output_component_name} DOCContainer 4 DynamicOutputFileName.txt)" "$(cat "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${test_app_name}/OutputComponents/${output_component_name}.php")"
+}
 runTest testNewDynamicOutputComponentRunsWithErrorIfAPP_NAMEIsNotSpecified
 runTest testNewDynamicOutputComponentRunsWithErrorIfSpecifiedAppDoesNotExist
 runTest testNewDynamicOutputComponentRunsWithErrorIfAnDynamicOutputComponentNamedDYNAMIC_OUTPUT_COMPONENT_NAMEAlreadyExists
@@ -50,6 +74,7 @@ runTest testNewDynamicOutputComponentRunsWithErrorIfDYNAMIC_OUTPUT_COMPONENT_NAM
 runTest testNewDynamicOutputComponentRunsWithErrorIfDYNAMIC_OUTPUT_COMPONENT_CONTAINERIsNotSpecified
 runTest testNewDynamicOutputComponentRunsWithErrorIfDYNAMIC_OUTPUT_COMPONENT_POSITIONIsNotSpecified
 runTest testNewDynamicOutputComponentRunsWithErrorIfOUTPUTIsNotSpecified
-runTest testNewDynamicOutputComponentDefinesSpecifiedDynamicOutputComponentForSpecifiedApp
+runTest testNewDynamicOutputComponentCreatesNewOutputComponentConfigurationFileForSpecifiedApp
+runTest testNewDynamicOutputComponentCreatesNewOutputComponentConfigurationFileForSpecifiedAppWhoseContentMatchesExpectedContent
 
 [[ -d "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${test_app_name}" ]] && rm -R "$(determineDshUnitDirectoryPath | sed 's/dshUnit/Apps/g')/${test_app_name}"
