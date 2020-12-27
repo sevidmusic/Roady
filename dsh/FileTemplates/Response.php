b5d57d79d665dad498bb397bbca32005294336bd
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-27 16:18:00 -0500

    DSH: Continued development of dsh --new Response.
    Defined new test file dsh/Tests/newResponseTests.sh
    
    Implemented the following tests:
    
    testNewResponseRunsWithErrorIfAPP_NAMEIsNotSpecified()
    testNewResponseRunsWithErrorIfSpecifiedAppDoesNotExist()
    testNewResponseRunsWithErrorIfAnResponseNamedRESPONSE_NAMEAlreadyExists()
    testNewResponseRunsWithErrorIfRESPONSE_NAMEIsNotSpecified()
    testNewResponseRunsWithErrorIfRESPONSE_POSITIONIsNotSpecified()
    testNewResponseCreatesNewResponseConfigurationFileForSpecifiedApp()
    testNewResponseCreatesNewResponseConfigurationFileForSpecifiedAppWhoseContentMatchesExpectedContent()
    
    All dsh --new Response tests are passing.
    
    This is related to issue #27.

---
+++ dsh/FileTemplates/Response.php
@@ -0,0 +1,13 @@
+<?php
+
+/** APP_NAME | RESPONSE_NAME.php */
+
+use DarlingDataManagementSystem\classes\component\OutputComponent;
+use DarlingDataManagementSystem\classes\component\DynamicOutputComponent;
+use DarlingDataManagementSystem\classes\component\Web\Routing\Request;
+
+$appComponentsFactory->buildResponse(
+    'RESPONSE_NAME',
+    RESPONSE_POSITION,
+);
+
