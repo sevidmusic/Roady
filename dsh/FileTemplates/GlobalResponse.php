23e43ef41ef4edf4fccb1c9f784fc7412e0e2dd0
sevidmusic <sdmwwebsdm@gmail.com>
2020-12-27 17:30:31 -0500

    DSH: Implemented dsh --new GlobalResponse
    Defined new test file at dsh/Tests/newGlobalResponseTests.sh
    
    Implemented the following tests:
    
    testNewGlobalResponseRunsWithErrorIfAPP_NAMEIsNotSpecified()
    testNewGlobalResponseRunsWithErrorIfSpecifiedAppDoesNotExist()
    testNewGlobalResponseRunsWithErrorIfAnGlobalResponseNamedGLOBAL_RESPONSE_NAMEAlreadyExists()
    testNewGlobalResponseRunsWithErrorIfGLOBAL_RESPONSE_NAMEIsNotSpecified()
    testNewGlobalResponseRunsWithErrorIfGLOBAL_RESPONSE_POSITIONIsNotSpecified()
    testNewGlobalResponseCreatesNewGlobalResponseConfigurationFileForSpecifiedApp()
    testNewGlobalResponseCreatesNewGlobalResponseConfigurationFileForSpecifiedAppWhoseContentMatchesExpectedContent()
    
    All dsh --new GlobalResponse tests are passing.
    
    This commit is related to issue #27.

---
+++ dsh/FileTemplates/GlobalResponse.php
@@ -0,0 +1,12 @@
+<?php
+
+/** APP_NAME | GLOBAL_RESPONSE_NAME.php */
+
+use DarlingDataManagementSystem\classes\component\OutputComponent;
+use DarlingDataManagementSystem\classes\component\DynamicOutputComponent;
+
+$appComponentsFactory->buildGlobalResponse(
+    'GLOBAL_RESPONSE_NAME',
+    GLOBAL_RESPONSE_POSITION,
+);
+
