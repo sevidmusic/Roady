03d074b58a271b177d2ec0b6d15de8ba9a36f90e
sevidmusic <sdmwwebsdm@gmail.com>
2021-01-18 16:18:12 -0500

    DSH: Continued development of dsh --locate-ddms-directory. Implemented testDshLocateDDMSDirectoryRunsWithoutError(), and testDshLocateDDMSDirectoryReturnsExpectedPath(). All dsh --locate-ddms-directory tests are passing.

--- dsh/Tests/locateDDMSDirectoryTests.sh
+++ dsh/Tests/locateDDMSDirectoryTests.sh
@@ -3,3 +3,15 @@
 
 set -o posix
 
+testDshLocateDDMSDirectoryRunsWithoutError() {
+    assertNoError "dsh --locate-ddms-directory"
+    assertNoError "dsh -l"
+}
+
+testDshLocateDDMSDirectoryReturnsExpectedPath() {
+    assertEquals "$(determineDshUnitDirectoryPath | sed 's/dshUnit//g')" "$(dsh --locate-ddms-directory)"
+    assertEquals "$(determineDshUnitDirectoryPath | sed 's/dshUnit//g')" "$(dsh -l)"
+}
+
+runTest testDshLocateDDMSDirectoryRunsWithoutError 2
+runTest testDshLocateDDMSDirectoryReturnsExpectedPath 2
