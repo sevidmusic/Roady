40c2c74f354a77fc2cd336491eae9b0d4ea5660b
sevidmusic <sdmwebsdm@gmail.com>
2021-02-25 13:35:51 -0500

    v1.0.0-beta: Resolved issue #129. Updated composer. Ran phpunit, all phpunit tests are passing.

--- core/abstractions/component/Web/App.php
+++ core/abstractions/component/Web/App.php
@@ -38,36 +38,6 @@ abstract class App extends SwitchableComponentBase implements AppInterface
         return (empty($nameLocation) === true ? 'DEFAULT' : $nameLocation);
     }
 
-    public static function getRequestedApp(RequestInterface $request, ComponentCrudInterface $componentCrud): AppInterface
-    {
-        $installedApps = $componentCrud->readAll(
-            App::deriveNameLocationFromRequest($request),
-            App::APP_CONTAINER
-        );
-        if (empty($installedApps)) {
-            throw new PHPRuntimeException('The requested app has not been installed, in fact, no apps have been installed. Please install the "' . App::deriveNameLocationFromRequest($request) . '" app.');
-        }
-        foreach ($installedApps as $storedApp) {
-            if (
-                $storedApp->getName() === self::deriveNameLocationFromRequest($request) &&
-                $storedApp->getLocation() === self::deriveNameLocationFromRequest($request) &&
-                $storedApp->getContainer() === self::APP_CONTAINER
-            ) {
-                $app = $storedApp;
-                break;
-            }
-            throw new PHPRuntimeException('The requested app has not been installed. Please install the "' . App::deriveNameLocationFromRequest($request) . '" app.');
-        }
-        $app = (empty($app) ? new \DarlingDataManagementSystem\classes\component\Web\App($request, new CoreSwitchable()) : $app);
-        if (self::isAnApp($app) === false) {
-            throw new PHPRuntimeException('The requested App is corrupted. Please remove and re-install the "' . App::deriveNameLocationFromRequest($request) . '" app.');
-        }
-        if ($app->getState() === false) {
-            throw new PHPRuntimeException('The requested app ' . $app->getName() . ' is not available at this time');
-        }
-        return $app;
-    }
-
     private static function isAnApp(ComponentInterface $component): bool
     {
         return (
