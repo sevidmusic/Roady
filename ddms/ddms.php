84af86523cd2e7b55db4679bfae488c363eb8a16
sevidmusic <sdmwebsdm@gmail.com>
2021-02-26 13:21:26 -0500

    issue127: Continued working out how ddms will work.

--- ddms/ddms.php
+++ ddms/ddms.php
@@ -5,22 +5,17 @@ require str_replace('ddms', '', __DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoloa
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
-class DDMS {
+interface DDMSCommandInterface
+{
+    public function run(array $argv): bool;
 
-    private function in_array_recursive(string $needle, array $haystack): bool
-    {
-        foreach($haystack as $value) {
-            if($value === $needle) {
-                return true;
-            }
-            if (is_array($value) && $this->in_array_recursive($needle, $value)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    public function prepareArguments(array $argv): array;
 
-    public function getArguments(array $argv): array
+}
+
+abstract class DDMSCommandBase implements DDMSCommandInterface
+{
+    public function prepareArguments(array $argv): array
     {
         $args = ['FLAGS' => [], 'OPTIONS' => []];
         foreach($argv as $position => $arg) {
@@ -39,10 +34,56 @@ class DDMS {
         }
         return $args;
     }
+
+    private function in_array_recursive(string $needle, array $haystack): bool
+    {
+        foreach($haystack as $value) {
+            if($value === $needle) {
+                return true;
+            }
+            if (is_array($value) && $this->in_array_recursive($needle, $value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    abstract public function run(array $argv): bool;
+
+}
+
+class DDMS extends DDMSCommandBase implements DDMSCommandInterface {
+
+    public function run(array $argv):bool {
+        var_dump($argv);
+        return true;
+    }
+
+}
+
+class DDMSDevCommand extends DDMSCommandBase implements DDMSCommandInterface {
+
+    public function run(array $argv):bool {
+        var_dump($this->prepareArguments($argv));
+        return true;
+    }
 }
 
 $ddms = new DDMS();
-var_dump($ddms->getArguments($argv));
+$ddms->run($argv);
+
+$ddmsDevCommand = new DDMSDevCommand();
+$ddmsDevCommand->run($argv);
+
+
+
+
+
+
+
+
+
+
 
 $process = new Process(['printf', '\n\n\e[0m\e[102m\e[30m%s\e[0m\n\n', 'The ddms command line utility is still in development.']);
 $process->run();
@@ -50,6 +91,6 @@ $process->run();
 if (!$process->isSuccessful()) {
     throw new ProcessFailedException($process);
 }
-echo $process->getOutput();
 
+        echo $process->getOutput();
 
