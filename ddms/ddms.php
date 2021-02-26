1234a0c03dcb9672b13ba14d2e0d7b342b33f50f
sevidmusic <sdmwebsdm@gmail.com>
2021-02-26 12:46:15 -0500

    issue127: Revised ddms dev drafts, still working out how ddms will work. A draft DDMS class has been implemented in ddms.php. This class is just for development, it will not be the actual implementation.

--- ddms/ddms.php
+++ ddms/ddms.php
@@ -5,34 +5,44 @@ require str_replace('ddms', '', __DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoloa
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
-function in_array_recursive(string $needle, array $haystack): bool {
-    foreach($haystack as $value) {
-        if($value === $needle) {
-            return true;
+class DDMS {
+
+    private function in_array_recursive(string $needle, array $haystack): bool
+    {
+        foreach($haystack as $value) {
+            if($value === $needle) {
+                return true;
+            }
+            if (is_array($value) && $this->in_array_recursive($needle, $value)) {
+                return true;
+            }
         }
-        if (is_array($value) && in_array_recursive($needle, $value)) {
-            return true;
-        }
+        return false;
     }
-    return false;
+
+    public function getArguments(array $argv): array
+    {
+        $args = ['FLAGS' => [], 'OPTIONS' => []];
+        foreach($argv as $position => $arg) {
+            if(substr($arg, 0, 2) === '--') {
+                $args['FLAGS'][str_replace('--' , '', $arg)] = [];
+                $nextItemKey = $position + 1;
+                while(isset($argv[$nextItemKey]) && substr($argv[$nextItemKey], 0, 2) !== '--') {
+                    $args['FLAGS'][str_replace('--' , '', $arg)][] = $argv[$nextItemKey];
+                    $nextItemKey++;
+                }
+                continue;
+            }
+            if (!$this->in_array_recursive($arg, $args['FLAGS'])) {
+                $args['OPTIONS'][$position] = $arg;
+            }
+        }
+        return $args;
+    }
 }
 
-$args = ['FLAGS' => [], 'OPTIONS' => []];
-foreach($argv as $position => $arg) {
-    if(substr($arg, 0, 2) === '--') {
-        $args['FLAGS'][str_replace('--' , '', $arg)] = [];
-        $nextItemKey = $position + 1;
-        while(isset($argv[$nextItemKey]) && substr($argv[$nextItemKey], 0, 2) !== '--') {
-            $args['FLAGS'][str_replace('--' , '', $arg)][] = $argv[$nextItemKey];
-            $nextItemKey++;
-        }
-        continue;
-    }
-    if (!in_array_recursive($arg, $args['FLAGS'])) {
-        $args['OPTIONS'][$position] = $arg;
-    }
-}
-var_dump($args);
+$ddms = new DDMS();
+var_dump($ddms->getArguments($argv));
 
 $process = new Process(['printf', '\n\n\e[0m\e[102m\e[30m%s\e[0m\n\n', 'The ddms command line utility is still in development.']);
 $process->run();
