c17d9331c13bda864727a20f47c6289d0b15e843
sevidmusic <sdmwebsdm@gmail.com>
2021-05-05 20:46:33 -0400

    More progress on development of mock AppBuilder class in Apps/HelloWorld/Components.php. The HelloWorld App is being used to mock new features, and will be deprecated once these new features are figured out.

--- Apps/HelloWorld/OutputComponents/HW.php
+++ Apps/HelloWorld/OutputComponents/HW.php
@@ -5,6 +5,6 @@
 $appComponentsFactory->buildOutputComponent(
     'HW',
     'OutputComponents',
-    'Hello World',
+    '<!DOCTYPE html><html lang="en"><head><title>Hello World</title><style> body { background: #020203; color: #03acef; } </style></head><body><h1>Hello World</h1></body></html>',
     0
 );
