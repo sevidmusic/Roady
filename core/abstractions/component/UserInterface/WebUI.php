57b5d97a09127ec3eb5deea01199b0edd8f7ad1d
sevidmusic <sdmwebsdm@gmail.com>
2021-07-03 17:21:54 -0400

    Housekeeping: Refactored abstract WebUI and abstract Router, cleaned up code. All phpunit and phpstan --level 8 tests are passing.

--- core/abstractions/component/UserInterface/WebUI.php
+++ core/abstractions/component/UserInterface/WebUI.php
@@ -3,71 +3,121 @@
 namespace DarlingDataManagementSystem\abstractions\component\UserInterface;
 
 use DarlingDataManagementSystem\abstractions\component\UserInterface\ResponseUI as ResponseUIInterface;
-use DarlingDataManagementSystem\interfaces\component\UserInterface\WebUI as WebUIInterface;
 use DarlingDataManagementSystem\classes\component\Web\App as CoreApp;
-use DarlingDataManagementSystem\interfaces\component\Web\Routing\Response as ResponseInterface;
 use DarlingDataManagementSystem\interfaces\component\OutputComponent as OutputComponentInterface;
+use DarlingDataManagementSystem\interfaces\component\UserInterface\WebUI as WebUIInterface;
+use DarlingDataManagementSystem\interfaces\component\Web\Routing\Response as ResponseInterface;
 use RuntimeException as PHPRuntimeException;
-
-
 
 abstract class WebUI extends ResponseUIInterface implements WebUIInterface
 {
 
-    private const DOCTYPE = '<!DOCTYPE html>' . PHP_EOL;
-    private const OPENHTML = '<html lang="en">' . PHP_EOL;
-    private const OPENHEAD = '<head>' . PHP_EOL;
-    private const CLOSEHEAD = '</head>' . PHP_EOL;
-    private const OPENBODY = '<body>' . PHP_EOL;
     private const CLOSEBODY = '</body>' . PHP_EOL;
+    private const CLOSEHEAD = '</head>' . PHP_EOL;
     private const CLOSEHTML = '</html>' . PHP_EOL;
+    private const DOCTYPE = '<!DOCTYPE html>' . PHP_EOL;
+    private const OPENBODY = '<body>' . PHP_EOL;
+    private const OPENHEAD = '<head>' . PHP_EOL;
+    private const OPENHTML = '<html lang="en">' . PHP_EOL;
 
-    private function buildOutputWithHtmlStructure(): string
+    /**
+     * @var string $webUIOutput The collective ouput of all Responses to the
+     * current request, with html structure added..
+     */
+    private string $webUIOutput = '';
+
+    /**
+     * @var string $collectiveResponseOutput
+     * The raw collective output of all Responses to the current Request.
+     */
+    private string $collectiveResponseOutput = '';
+
+    private function openHtml(): void
     {
-        $expectedOutput = self::DOCTYPE . self::OPENHTML . self::OPENHEAD;
-        $actualOutput = '';
-        $expectedResponses = $this->router->getResponses(
+        /** @devNote:
+         * Always reset the $this->webUIOutput when $this->openHtml() is called,
+         * i.e., use "=" not ".=" for assignment.
+         */
+        $this->webUIOutput = self::DOCTYPE . self::OPENHTML . self::OPENHEAD;
+    }
+
+    /**
+     * @return array<string, ResponseInterface> Sorted array of Responses to the current Request indexed by Response position.
+     */
+    private function getSortedResponsesToCurrentRequest(): array
+    {
+        $responsesToCurrentRequest = $this->router->getResponses(
             CoreApp::deriveAppLocationFromRequest($this->router->getRequest()),
             ResponseInterface::RESPONSE_CONTAINER
         );
-        $sortedResponses = $this->sortPositionables(...$expectedResponses);;
+        /** @var array <string, ResponseInterface> $sortedResponses */
+        $sortedResponses = $this->sortPositionables(...$responsesToCurrentRequest);;
+        return $sortedResponses;
+    }
+
+    private function closeHeadOpenBody(): void
+    {
+        $this->webUIOutput .= self::CLOSEHEAD . self::OPENBODY;
+    }
+
+    private function responsePositionIsLessThanZeroAndHeadWasNotClosedAndBodyNotOpened(ResponseInterface $response): bool
+    {
+        return ($response->getPosition() >= 0 && !str_contains($this->webUIOutput, self::CLOSEHEAD . self::OPENBODY));
+    }
+
+    private function buildOutputWithHtmlStructure(): string
+    {
+        /** @devNote: Always reset $this->collectiveResponseOutput when $this->buildOutputWithHtmlStructure() is called. */
+        $this->collectiveResponseOutput = '';
+        $this->openHtml();
         /**
          * @var ResponseInterface $response
          */
-        foreach($sortedResponses as $response)
+        foreach($this->getSortedResponsesToCurrentRequest() as $response)
         {
-            if($response->getPosition() >= 0 && !str_contains($expectedOutput, self::CLOSEHEAD . self::OPENBODY)) {
-                $expectedOutput .= self::CLOSEHEAD . self::OPENBODY;
+            if($this->responsePositionIsLessThanZeroAndHeadWasNotClosedAndBodyNotOpened($response)) {
+                $this->closeHeadOpenBody();
             }
-            $outputComponents = [];
-            foreach($response->getOutputComponentStorageInfo() as $storable)
-            {
-                $component = $this->getRoutersComponentCrud()->read($storable);
-                $classImplements = class_implements($component);
-                $isAnOutputComponent = (is_array($classImplements) ? in_array(OutputComponentInterface::class, $classImplements) : false);
-                if($isAnOutputComponent === true)
-                {
-                    /**
-                     * @var OutputComponentInterface $component
-                     */
-                    array_push($outputComponents, $component);
-                }
-            }
-            $sortedOutputComponents = $this->sortPositionables(...$outputComponents);
-            /**
-             * @var OutputComponentInterface $outputComponent
-             */
-            foreach($sortedOutputComponents as $outputComponent)
-            {
-                $expectedOutput .= $outputComponent->getOutput();
-                $actualOutput .= $outputComponent->getOutput();
-            }
+            $this->addResponseOutputToWebUIOutput($response);
         }
-        if(empty($actualOutput))
+        $this->closeBodyCloseHtml();
+        if(empty($this->collectiveResponseOutput))
         {
             throw new PHPRuntimeException('There is nothing to show for this request.');
         }
-        return $expectedOutput . self::CLOSEBODY . self::CLOSEHTML;
+        return $this->webUIOutput;
+    }
+
+    private function closeBodyCloseHtml(): void
+    {
+        $this->webUIOutput .= self::CLOSEBODY . self::CLOSEHTML;
+    }
+
+    private function addResponseOutputToWebUIOutput(ResponseInterface $response): void
+    {
+        $outputComponents = [];
+        foreach($response->getOutputComponentStorageInfo() as $storable)
+        {
+            $component = $this->getRoutersComponentCrud()->read($storable);
+            $classImplements = class_implements($component);
+            $isAnOutputComponent = (is_array($classImplements) ? in_array(OutputComponentInterface::class, $classImplements) : false);
+            if($isAnOutputComponent === true)
+            {
+                /**
+                * @var OutputComponentInterface $component
+                */
+                array_push($outputComponents, $component);
+            }
+        }
+        $sortedOutputComponents = $this->sortPositionables(...$outputComponents);
+        /**
+        * @var OutputComponentInterface $outputComponent
+        */
+        foreach($sortedOutputComponents as $outputComponent)
+        {
+            $this->webUIOutput .= $outputComponent->getOutput();
+            $this->collectiveResponseOutput .= $outputComponent->getOutput();
+        }
     }
 
     public function getOutput(): string
