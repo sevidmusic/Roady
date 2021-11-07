09091712dc1e1611166903c2239cf4d161edf272
sevidmusic <sdmwebsdm@gmail.com>
2021-11-07 11:54:40 -0500

    Working on addressing issue #260

--- core/interfaces/component/UserInterface/ResponseUI.php
+++ core/interfaces/component/UserInterface/ResponseUI.php
@@ -2,11 +2,35 @@
 
 namespace roady\interfaces\component\UserInterface;
 
-use roady\interfaces\component\OutputComponent as CoreOutputComponent;
-use roady\interfaces\component\Web\Routing\Router as RouterInterface;
+use roady\interfaces\component\OutputComponent; 
+use roady\interfaces\component\Web\Routing\Router; 
 
-interface ResponseUI extends CoreOutputComponent
+/**
+ * A ResponseUI is an OutputComponent whose output is generated
+ * from the collective output of all of the OutputComponents that
+ * are assigned to each of the Responses returned by a Router
+ * in response to a specific Request.
+ *
+ * Methods:
+ *
+ * public function getState(): bool;
+ * public function switchState(): bool;
+ * public function getType(): string;
+ * public function export(): array<string, mixed>;
+ * public function import(array<string, mixed> $export): bool;
+ * public function getName(): string;
+ * public function getUniqueId(): string;
+ * public function getLocation(): string;
+ * public function getContainer(): string;
+ * public function getOutput(): string;
+ * public function increasePosition(): bool;
+ * public function decreasePosition(): bool;
+ * public function getPosition(): float;
+ * public function getRouter(): Router;
+ *
+ */
+interface ResponseUI extends OutputComponent
 {
 
-    public function getRouter(): RouterInterface;
+    public function getRouter(): Router;
 }
