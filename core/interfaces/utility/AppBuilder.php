42c2e095ded6285f5c8f23df096e2b5b4c553361
sevidmusic <sdmwebsdm@gmail.com>
2021-11-08 14:51:59 -0500

    Continued addressing issue #266

--- core/interfaces/utility/AppBuilder.php
+++ core/interfaces/utility/AppBuilder.php
@@ -2,7 +2,7 @@
 
 namespace roady\interfaces\utility;
 
-use roady\interfaces\component\Factory\App\AppComponentsFactory as AppComponentsFactoryInterface;
+use roady\interfaces\component\Factory\App\AppComponentsFactory;
 
 /**
  * An AppBuilder is a utility that can be used to build a roady
@@ -23,18 +23,18 @@ interface AppBuilder {
      * @param string $domain The domain the AppComponentsFactory will build the
      *                       App's Components for. For example: http://localhost:8080
      *
-     * @return AppComponentsFactoryInterface The App's AppComponentsFactory.
+     * @return AppComponentsFactory The App's AppComponentsFactory.
      */
-    public static function getAppsAppComponentsFactory(string $appName, string $domain): AppComponentsFactoryInterface;
+    public static function getAppsAppComponentsFactory(string $appName, string $domain): AppComponentsFactory;
 
     /**
      * Build the App assigned to the provided AppComponentsFactory instance.
      *
-     * @param AppComponentsFactoryInterface $appComponentsFactory The AppComponentsFactory instance
+     * @param AppComponentsFactory $appComponentsFactory The AppComponentsFactory instance
      *                                                            to use to build the App's configured
      *                                                            Components.
      *
      */
-    public static function buildApp(AppComponentsFactoryInterface $appComponentsFactory): void;
+    public static function buildApp(AppComponentsFactory $appComponentsFactory): void;
 
 }
