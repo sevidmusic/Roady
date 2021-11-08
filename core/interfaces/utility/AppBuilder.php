68856c73a7b64676663c7f7b1507c9b708befb6e
sevidmusic <sdmwebsdm@gmail.com>
2021-11-08 15:02:22 -0500

    Working on addressing issue #266

--- core/interfaces/utility/AppBuilder.php
+++ core/interfaces/utility/AppBuilder.php
@@ -5,8 +5,14 @@ namespace roady\interfaces\utility;
 use roady\interfaces\component\Factory\App\AppComponentsFactory;
 
 /**
- * An AppBuilder is a utility that can be used to build a roady
- * App and it's configured Components for a domain.
+ * An AppBuilder is a utility that can be used to build a 
+ * roady App and it's configured Components for a domain via 
+ * a AppComponentsFactory.
+ *
+ * Methods:
+ *
+ * public static function getAppsAppComponentsFactory(string $appName, string $domain): AppComponentsFactory;
+ * public static function buildApp(AppComponentsFactory $appComponentsFactory): void;
  */
 interface AppBuilder {
 
@@ -17,22 +23,29 @@ interface AppBuilder {
      * instance can be used to build Components for the App for the 
      * specified domain.
      *
-     * @param string $appName The name of the App to return a AppComponentsFactory
-     *                        instance for.
+     * @param string $appName The name of the App to return a 
+     *                        AppComponentsFactory instance for.
      *
-     * @param string $domain The domain the AppComponentsFactory will build the
-     *                       App's Components for. For example: http://localhost:8080
+     * @param string $domain The domain the AppComponentsFactory 
+     *                       will build the App's Components for. 
+     *
+     *                       For example: 
+     *                           http://localhost:8080
      *
      * @return AppComponentsFactory The App's AppComponentsFactory.
      */
     public static function getAppsAppComponentsFactory(string $appName, string $domain): AppComponentsFactory;
 
     /**
-     * Build the App assigned to the provided AppComponentsFactory instance.
+     * Build the App assigned to the provided AppComponentsFactory 
+     * instance.
      *
-     * @param AppComponentsFactory $appComponentsFactory The AppComponentsFactory instance
-     *                                                            to use to build the App's configured
-     *                                                            Components.
+     * @param AppComponentsFactory $appComponentsFactory 
+     *
+     *                             The AppComponentsFactory 
+     *                             instance to use to build 
+     *                             the App's configured
+     *                             Components.
      *
      */
     public static function buildApp(AppComponentsFactory $appComponentsFactory): void;
