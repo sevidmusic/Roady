6d3d71fd894bd362671cdee49a6964fb9a5db99e
sevidmusic <sdmwebsdm@gmail.com>
2021-11-08 01:56:02 -0500

    Working on addressing issue #266

--- core/interfaces/component/Web/App.php
+++ core/interfaces/component/Web/App.php
@@ -2,17 +2,39 @@
 
 namespace roady\interfaces\component\Web;
 
-use roady\interfaces\component\Crud\ComponentCrud as ComponentCrudInterface;
-use roady\interfaces\component\SwitchableComponent as CoreSwitchableComponent;
-use roady\interfaces\component\Web\Routing\Request as RequestInterface;
+use roady\interfaces\component\Crud\ComponentCrud; 
+use roady\interfaces\component\SwitchableComponent;
+use roady\interfaces\component\Web\Routing\Request;
 
-interface App extends CoreSwitchableComponent
+/**
+ * An App is a SwitchableComponent
+ * 
+ * Constants:
+ *
+ * APP_CONTAINER: string 
+ *
+ * Methods:
+ *
+ * public function getState(): bool;
+ * public function switchState(): bool;
+ * public function getType(): string;
+ * public function export(): array<string, mixed>;
+ * public function import(array<string, mixed> $export): bool;
+ * public function getName(): string;
+ * public function getUniqueId(): string;
+ * public function getLocation(): string;
+ * public function getContainer(): string;
+ * public static function deriveAppLocationFromRequest(Request $request): string;
+ * public function getAppDomain(): Request;
+ *
+ */
+interface App extends SwitchableComponent
 {
 
     public const APP_CONTAINER = "APP";
 
-    public static function deriveAppLocationFromRequest(RequestInterface $request): string;
+    public static function deriveAppLocationFromRequest(Request $request): string;
 
-    public function getAppDomain(): RequestInterface;
+    public function getAppDomain(): Request;
 
 }
