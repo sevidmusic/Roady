59317b6cf5822496f797a9f30be225b4a86a1c82
sevidmusic <sevidmusic@gmail.com>
2022-07-22 01:43:00 -0400

    Added `tests\PhpUnitTest`, which simply tests that `phpunit` is setup properly.

---
+++ tests/PhpUnitTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Defines tests to make sure phpunit tests can be run.
+ *
+ * Methods:
+ *
+ * ```
+ * public function test_php_unit_tests_are_run(): void
+ *
+ * ```
+ *
+ */
+class PhpUnitTest extends TestCase
+{
+
+    public function test_php_unit_tests_are_run(): void
+    {
+        $this->assertTrue(
+            true,
+            'This test should run if phpunit is setup correctly'
+        );
+    }
+}
