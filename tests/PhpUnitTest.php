f30fea4a0f9f295b75e0f607892b0965c73a24fe
sevidmusic <sevidmusic@gmail.com>
2022-08-11 12:18:58 -0400

    Revised documentation in `tests\PhpUnitTest`.

--- tests/PhpUnitTest.php
+++ tests/PhpUnitTest.php
@@ -5,7 +5,7 @@ namespace tests;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Defines tests to make sure phpunit tests can be run.
+ * Defines tests to make sure PHPUnit tests can be run.
  *
  * Methods:
  *
@@ -15,14 +15,20 @@ use PHPUnit\Framework\TestCase;
  * ```
  *
  */
-class PhpUnitTest extends TestCase
+class PHPUnitTest extends TestCase
 {
 
+    /**
+     * Test that PHPUnit tests run.
+     *
+     * If this test does not run then PHPUnit is not set up properly.
+     *
+     */
     public function test_php_unit_tests_are_run(): void
     {
         $this->assertTrue(
             true,
-            'This test should run if phpunit is set up correctly'
+            'This test should run if PHPUnit is set up correctly'
         );
     }
 }
