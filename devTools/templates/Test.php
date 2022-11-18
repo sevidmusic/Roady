7220dcc07951fc5ea56ced798df971f43d4dc28c
sevidmusic <sevidmusic@gmail.com>
2022-11-18 01:26:46 -0500

    Added new development tool `devTools/NewRoadyClass.php` which can be used to generate source code for a new roady Class, including it's Interface, TestTrait, and Test class.

---
+++ devTools/templates/Test.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tests\classes\__SUB_NAMESPACE__;
+
+use roady\classes\__SUB_NAMESPACE__\__TARGET_CLASS_NAME__;
+use tests\RoadyTestCase;
+use tests\interfaces\__SUB_NAMESPACE__\__TARGET_CLASS_NAME__TestTrait;
+
+class __TARGET_CLASS_NAME__Test extends RoadyTestCase
+{
+
+    /**
+     * The __TARGET_CLASS_NAME__TestTrait defines
+     * common tests for implementations of the
+     * roady\interfaces\__SUB_NAMESPACE__\__TARGET_CLASS_NAME__
+     * interface.
+     *
+     * @see __TARGET_CLASS_NAME__TestTrait
+     *
+     */
+    use __TARGET_CLASS_NAME__TestTrait;
+
+}
+
