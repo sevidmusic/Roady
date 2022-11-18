d5325ba63d94eff55da9b1ae4b1e76e8d56ec3ee
sevidmusic <sevidmusic@gmail.com>
2022-11-18 11:31:54 -0500

    Defined the following new interface:
    ```
    roady\interfaces\strings\Text
    
    ```
    
    A class that implements the `roady\interfaces\strings\Text` interface
    represents a string, can be cast to the string it represents, and can
    provide information about the string it represents.
    
    Public methods defined by `roady\interfaces\strings\Text`:
    
    ```
    public function __toString(): string;
    public function contains(string|Stringable ...$strings): bool;
    public function length(): int;
    
    ```
    
    For example, assuming `$text` is an instance of a class that implements
    the `roady\interfaces\strings\Text`:
    
    ```
    echo $text;
    // example output: Foo
    
    echo (string->contains('F', 'oo', $text) ? 'true' : 'false');
    // example output: true
    
    echo (string->contains('f', 'oo', $text) ? 'true' : 'false');
    // example output: false
    
    echo strval($text->length());
    // example output: 3
    
    ```

---
+++ src/interfaces/strings/Text.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace roady\interfaces\strings;
+
+use \Stringable;
+
+/**
+ * Text represents a string, can be cast to the string it represents,
+ * and can provide information about the string it represents.
+ *
+ * @example
+ *
+ * ```
+ * echo $text;
+ * // example output: Foo
+ *
+ * echo (string->contains('F', 'oo', $text) ? 'true' : 'false');
+ * // example output: true
+ *
+ * echo (string->contains('f', 'oo', $text) ? 'true' : 'false');
+ * // example output: false
+ *
+ * echo strval($text->length());
+ * // example output: 3
+ *
+ * ```
+ *
+ * @see Stringable
+ *
+ */
+interface Text extends Stringable
+{
+
+    /**
+     * Return the original string.
+     *
+     * @return string
+     *
+     * @example
+     *
+     * ```
+     * echo $text->__toString();
+     * // example output: Foo Bar Baz
+     *
+     * ```
+     *
+     */
+    public function __toString(): string;
+
+    /**
+     * Return true if the Text contains all of the specified strings,
+     * false otherwise.
+     *
+     * @param string|Stringable $strings The strings to search for.
+     *
+     * @return bool
+     *
+     * @example
+     *
+     * ```
+     * echo $text;
+     * // example output: Foo
+     *
+     * echo (string->contains('F', 'oo', $text) ? 'true' : 'false');
+     * // example output: true
+     *
+     * echo (string->contains('f', 'oo', $text) ? 'true' : 'false');
+     * // example output: false
+     *
+     * ```
+     *
+     * @see Stringable
+     *
+     */
+    public function contains(string|Stringable ...$strings): bool;
+
+    /**
+     * Return the Text's length as an integer.
+     *
+     * @return int
+     *
+     * @example
+     *
+     * ```
+     * echo $text;
+     * // example output: Foo
+     *
+     * echo strval($text->length());
+     * // example output: 3
+     *
+     * ```
+     *
+     */
+    public function length(): int;
+
+}
+
