55781b72561e9dad30d378ecf7ed46742069963d
sevidmusic <sevidmusic@gmail.com>
2022-11-29 23:41:12 -0500

    Defined the following new interface:
    ```
    roady\interfaces\strings\Name
    
    ```
    
    The `roady\interfaces\strings\Name` interface extends the
    `roady\interfaces\strings\SafeText` interface.
    
    A class that implements the `roady\interfaces\strings\Name` interface
    is SafeText that begins with an alphanumeric character,
    is at least 1 character in length, is no more than 70 characters
    in length. Like SafeText, a Name will only contains the following
    characters:
    
    - Alphanumeric characters: A-Z, a-z, and 0-9
    - Underscores: _
    - Hyphens: -
    - Periods: .
    
    Public methods defined by the `roady\interfaces\strings\Name`
    interface:
    
    ```
    public function __toString(): string;
    
    ```
    
    For example:
    
    ```
    echo $name;
    // example output: Foo_Bar_Baz.Bin_Bar-Foo_Bazzer
    
    ```
    Began development of new class `roady\classes\strings\Name` which
    implements the `roady\interfaces\strings\Name` interface.
    
    Implemented the following methods defined by the
    `roady\interfaces\strings\Name` interface:
    
    ```
    public function __toString(): string;
    
    ```
    
    For example:
    
    ```
    $name = new \roady\classes\strings\Name(
        new \roady\classes\strings\Text(
            '!Foo bar Baz..bin!@#Bar--foo____%$#@#$%^&*Bazzer'
        )
    );
    
    echo $name;
    // example output: Foo_Bar_Baz.Bin_Bar-Foo_Bazzer
    
    ```
    
    Implemented the following tests in
    `tests\interfaces\strings\NameTestTrait`:
    
    ```
    public function test_Name_always_begins_with_an_alphanumeric_character(): void
    public function test_that_the_length_of_a_Name_is_always_at_least_1(): void
    public function test_that_the_length_of_a_Name_is_less_than_or_equal_to_70(): void
    
    ```

---
+++ tests/interfaces/strings/NameTestTrait.php
@@ -0,0 +1,282 @@
+<?php
+
+namespace tests\interfaces\strings;
+
+use roady\classes\strings\Text as TextToConvertToAName;
+use roady\interfaces\strings\Name;
+use roady\interfaces\strings\Text;
+use tests\interfaces\strings\SafeTextTestTrait;
+
+/**
+ * The NameTestTrait defines common tests for implementations of the
+ * Name interface.
+ *
+ * @see Name
+ *
+ */
+trait NameTestTrait
+{
+    /**
+     * The SafeTextTestTrait defines common tests for implementations
+     * of the roady\interfaces\strings\SafeText interface.
+     *
+     * @see roady\interfaces\strings\SafeText
+     */
+    use SafeTextTestTrait;
+
+    /**
+     * @var Name $name An instance of an implementation of the Name
+     *                 interface to test.
+     */
+    private Name $name;
+
+    /**
+     * Modify a string, insuring only the following characters
+     * exist in the resulting string:
+     *
+     * - Alphanumeric characters: A-Z, a-z, and 0-9
+     * - Underscores: _
+     * - Hyphens: -
+     * - Periods: .
+     *
+     * Unsafe characters will be replaced with underscores.
+     *
+     * A consecutive sequence of 2 or more unsafe characters will be
+     * replaced by a single underscore.
+     *
+     * A consecutive sequence of 2 or more underscores will be
+     * replaced by a single underscore.
+     *
+     * A consecutive sequence of 2 or more hyphens will be replaced
+     * by a single hyphen.
+     *
+     * A consecutive sequence of 2 or more periods will be replaced
+     * by a single period.
+     *
+     * If the original string is empty, then the modified string will
+     * be the numeric character: 0
+     *
+     * The resulting string will always start with an alphanumeric
+     * character.
+     *
+     * If the original string does not start with an alphanumeric
+     * character, then the first alphanumeric character in the
+     * original string will be the first character of the of the
+     * resulting string.
+     *
+     * If the original string does not contain any alphanumeric
+     * characters than the resulting string will be the numeric
+     * character: 0
+     *
+     * @return string
+     *
+     * @example
+     *
+     * ```
+     * $string = '!Foo bar Baz..bin!@#Bar--foo____%$#@#$%^&*Bazzer';
+     *
+     * echo $this->makeStringSafe($string);
+     * // example output: Foo_bar_Baz.bin_Bar-foo_Bazzer
+     *
+     * $string = '';
+     *
+     * echo $this->makeStringSafe($string);
+     * // example output: 0
+     *
+     * $string = '_';
+     *
+     * echo $this->makeStringSafe($string);
+     * // example output: 0
+     *
+     * $string = '-';
+     *
+     * echo $this->makeStringSafe($string);
+     * // example output: 0
+     *
+     * $string = '.';
+     *
+     * echo $this->makeStringSafe($string);
+     * // example output: 0
+     *
+     * $string = '(#$*%*';
+     *
+     * echo $this->makeStringSafe($string);
+     * // example output: 0
+     *
+     * ```
+     *
+     */
+    protected function makeStringSafe(string $string): string
+    {
+        $string = parent::makeStringSafe($string);
+        $string = substr(
+            $string,
+            $this->positionOfFirstAlphanumericCharacter($string),
+            70
+        );
+        return match(
+            empty($string) ||
+            $string === '_' ||
+            $string === '-' ||
+            $string === '.'
+        ) {
+            true => strval(0),
+            default => $string
+        };
+    }
+
+    /**
+     * Return the implementation of the Name interface to test.
+     *
+     * @return Name
+     *
+     * @see Name
+     *
+     * @example
+     *
+     * ```
+     * $this->nameTestInstance();
+     *
+     * ```
+     *
+     */
+    protected function nameTestInstance(): Name
+    {
+        return $this->name;
+    }
+
+    /**
+     * Determine the position of the first alphanumeric character
+     * in a string.
+     *
+     * @return int
+     *
+     * @example
+     *
+     * ```
+     * echo $this->positionOfFirstAlphanumericCharacter('_Foo');
+     * // example output: 1
+     *
+     * ```
+     */
+    protected function positionOfFirstAlphanumericCharacter(
+        string $string
+    ): int
+    {
+        $stringLength = mb_strlen($string);
+        for(
+            $firstAlphanumericCharacterIndex = 0;
+            $firstAlphanumericCharacterIndex < $stringLength;
+            $firstAlphanumericCharacterIndex++
+        ) {
+            if(
+                ctype_alnum(
+                    $string[$firstAlphanumericCharacterIndex]
+                )
+            ) {
+                break;
+            };
+        }
+        return $firstAlphanumericCharacterIndex;
+    }
+
+    /**
+     * Set an instance of an implementation of the Name interface
+     * to test.
+     *
+     * @return void
+     *
+     * @see Name
+     *
+     * @example
+     *
+     * ```
+     * $this->setNameTestInstance(
+     *     new \roady\classes\strings\Name(
+     *         new \roady\classes\strings\Text('Name')
+     *     )
+     * );
+     *
+     * ```
+     *
+     */
+    public function setNameTestInstance(Name $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Test that a Name always begins with an alphanumeric character.
+     *
+     * @return void
+     *
+     */
+    public function test_Name_always_begins_with_an_alphanumeric_character(): void
+    {
+        $text = new TextToConvertToAName(
+            $this->randomChars()
+        );
+        $this->setUpWithSpecificText($text);
+        $this->assertTrue(
+            ctype_alnum(substr($this->nameTestInstance(), 0, 1)),
+            $this->testFailedMessage(
+                $this->nameTestInstance(),
+                '',
+                'A Name must always start with an alphanumeric ' .
+                'character'
+            )
+        );
+    }
+
+    /**
+     * Test that the length of a Name less than or equal to 70.
+     *
+     * @return void
+     *
+     */
+    public function test_that_the_length_of_a_Name_is_less_than_or_equal_to_70(): void
+    {
+        $text = new TextToConvertToAName(
+            str_shuffle(
+                str_repeat('1234567890', 100) .
+                $this->randomChars()
+            )
+        );
+        $this->setUpWithSpecificText($text);
+        $this->assertLessThanOrEqual(
+            70,
+            $this->nameTestInstance()->length(),
+            $this->testFailedMessage(
+                $this->nameTestInstance(),
+                '',
+                'A Name\'s length must be less than or equal to 70'
+            )
+        );
+    }
+
+    /**
+     * Test that the length of a Name is always at least 1.
+     *
+     * @return void
+     *
+     */
+    public function test_that_the_length_of_a_Name_is_always_at_least_1(): void
+    {
+        $strings = ['', ' ', '.', '-', '_', $this->randomChars()];
+        $text = new TextToConvertToAName(
+            $strings[array_rand($strings)]
+        );
+        $this->setUpWithSpecificText($text);
+        $this->assertGreaterThanOrEqual(
+            1,
+            $this->nameTestInstance()->length(),
+            $this->testFailedMessage(
+                $this->nameTestInstance(),
+                '',
+                'A Name\'s length must be greater than or equal to 1'
+            )
+        );
+    }
+
+}
+
