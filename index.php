2605de14e68185296ed0c623998a54dd8441d4fe
sevidmusic <sevidmusic@gmail.com>
2024-01-25 11:41:32 -0500

    Working on mock index.php

--- index.php
+++ index.php
@@ -463,7 +463,6 @@ class RoadyUI
     ];
 
     private const ROADY_UI_LAYOUT_STRING = <<<'EOT'
-
 <!DOCTYPE html>
 
 <html>
@@ -534,11 +533,70 @@ EOT;
                     ||
                     $availableNamedPosition === 'roady-ui-js-script-tags-for-end-of-html'
                 ) {
-                    true => str_replace('<' . $availableNamedPosition . '></' . $availableNamedPosition . '>', implode(PHP_EOL, $renderedOutput[$availableNamedPosition]), $uiLayoutString),
-                    default => str_replace('<' . $availableNamedPosition . '></' . $availableNamedPosition . '>', '<div class="' . $availableNamedPosition . '">' . implode(PHP_EOL, $renderedOutput[$availableNamedPosition]) . '</div>', $uiLayoutString),
+                    true => str_replace(
+                        '<' . $availableNamedPosition . '></' . $availableNamedPosition . '>',
+                        implode(PHP_EOL, $renderedOutput[$availableNamedPosition]),
+                        $uiLayoutString
+                    ),
+                    default => str_replace(
+                        '<' . $availableNamedPosition . '></' . $availableNamedPosition . '>',
+                        PHP_EOL .
+                        '<!-- begin ' . $availableNamedPosition . ' -->' .
+                        PHP_EOL .
+                        '<div class="' . $availableNamedPosition . '">' .
+                            PHP_EOL .
+                            PHP_EOL .
+                            implode(
+                                PHP_EOL,
+                                $renderedOutput[$availableNamedPosition]
+                            ) .
+                            PHP_EOL .
+                        '</div>' .
+                        PHP_EOL .
+                        '<!-- end ' . $availableNamedPosition . ' -->',
+                        $uiLayoutString
+                    ),
                 };
             }
-            $uiLayoutString = str_replace('<' . $availableNamedPosition . '></' . $availableNamedPosition . '>', '', $uiLayoutString);
+            // Clean up unused/empty positions.
+            // css, js, and title should be removed if not used
+            // named positions a-g should be replaced with an empty div whose class attribute is assigned the postions's name
+            $uiLayoutString = match(
+                $availableNamedPosition === 'roady-ui-css-stylesheet-link-tags'
+                ||
+                $availableNamedPosition === 'roady-ui-js-script-tags-for-html-head'
+                ||
+                $availableNamedPosition === 'roady-ui-js-script-tags-for-end-of-html'
+                ||
+                $availableNamedPosition === 'roady-ui-page-title-placeholder'
+            ) {
+                true => str_replace(
+                    '<' . $availableNamedPosition . '></' . $availableNamedPosition . '>',
+                    '',
+                    $uiLayoutString
+                ),
+                default => str_replace(
+                    '<' . $availableNamedPosition . '></' . $availableNamedPosition . '>',
+                     PHP_EOL .
+                     '<!-- begin ' . $availableNamedPosition . ' -->' .
+                     PHP_EOL .
+                     '<div class="' . $availableNamedPosition . '"></div>' .
+                     PHP_EOL .
+                    '<!-- end ' . $availableNamedPosition . ' -->',
+                    $uiLayoutString
+                ),
+
+            };
+            $uiLayoutString = str_replace(
+                '<' . $availableNamedPosition . '></' . $availableNamedPosition . '>',
+                 PHP_EOL .
+                 '<!-- begin ' . $availableNamedPosition . ' -->' .
+                 PHP_EOL .
+                 '<div class="' . $availableNamedPosition . '"></div>' .
+                 PHP_EOL .
+                '<!-- end ' . $availableNamedPosition . ' -->',
+                $uiLayoutString
+            );
         }
         return $uiLayoutString;
     }
