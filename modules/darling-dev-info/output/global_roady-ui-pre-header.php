34126834a50568c0db8e48b3f90db54db8c8a115
Sevi Darling <sevidmusic@gmail.com>
2025-12-19 10:52:35 -0500

    Working on making collapsible logic available to all modules

--- modules/darling-dev-info/output/global_roady-ui-pre-header.php
+++ modules/darling-dev-info/output/global_roady-ui-pre-header.php
@@ -31,20 +31,3 @@ echo htmlspecialchars($json, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5, "UTF-8", f
         </tr>
     </table>
 </div>
-<script>
-var coll = document.getElementsByClassName("darling-collapse-content-trigger");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-      content.style.overflow = "auto";
-    }
-  });
-}
-</script>
